46b5a85b2fdeec94027c4203184dd3f3b1ce690c
jon borchardt <jonathanb@allenai.org>
2023-09-18 16:29:45 -0700

    uopdates to not replace context menu

--- src/components/ContextMenu.tsx
+++ src/components/ContextMenu.tsx
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
-import { Menu, MenuItem } from '@mui/material';
+import React, { useState, useEffect, useRef } from 'react';
+import { SpeedDial, SpeedDialAction, SpeedDialIcon } from '@mui/material';
+
+import SearchIcon from '@mui/icons-material/Search';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 
 import { useFeatureToggles } from '../FeatureToggleContext';
 
 export interface MenuOption {
     label: string;
+    icon: JSX.Element;
     action: (selectedText: string, mouseX?: number, mouseY?: number) => void;
 }
 
 // default implementation of searching the training datasets on the selected text
 export const SearchTrainingDatasetMenuOption: MenuOption = {
     label: 'Search Pretraining Data',
+    icon: <SearchIcon />,
     action: (selectedText: string) => {
         const params = new URLSearchParams();
         selectedText = selectedText.replace(/[/"/']/g, '\\"');
@@ -25,6 +30,7 @@ export const SearchTrainingDatasetMenuOption: MenuOption = {
 // default implementation of copy of the selected text
 export const CopyMenuOption: MenuOption = {
     label: 'Copy',
+    icon: <ContentCopyIcon />,
     action: (selectedText: string) => {
         navigator.clipboard.writeText(selectedText);
     },
@@ -45,63 +51,94 @@ export const ContextMenu = ({
         return <>{children}</>;
     }
 
-    const [contextMenu, setContextMenu] = useState<{
+    const [contextPos, setContextPos] = useState<{
         mouseX: number;
         mouseY: number;
-    } | null>(null);
+    }>({
+        mouseX: 0,
+        mouseY: 0,
+    });
 
-    const getSelectionText = () => {
-        let text = '';
-        if (window.getSelection) {
-            const sel = window.getSelection();
-            text = sel?.toString() || '';
+    const [showDial, setShowDial] = useState<boolean>(false);
+
+    const divRef = useRef<HTMLDivElement | null>(null);
+
+    const getSelectionText = (): string | undefined => {
+        if (!window.getSelection) {
+            return undefined;
         }
-        return text;
+        return window.getSelection()?.toString();
     };
 
-    const handleContextMenu = (event: React.MouseEvent) => {
-        event.preventDefault();
-        setContextMenu(
-            contextMenu === null
-                ? {
-                      mouseX: event.clientX + 2,
-                      mouseY: event.clientY - 6,
-                  }
-                : // repeated contextmenu when it is already open closes it with Chrome 84 on Ubuntu
-                  // Other native context menus might behave different.
-                  // With this behavior we prevent contextmenu from the backdrop to re-locale existing context menus.
-                  null
-        );
+    const handleOpen = (event: MouseEvent) => {
+        let clientX = event.clientX;
+        let clientY = event.clientY;
+        const elementOffset = divRef.current?.getBoundingClientRect();
+        if (elementOffset) {
+            clientX -= elementOffset.x;
+            clientY -= elementOffset.y;
+        }
+        setContextPos({
+            mouseX: clientX,
+            mouseY: clientY,
+        });
+        setShowDial(true);
     };
 
     const handleClose = () => {
-        setContextMenu(null);
+        setShowDial(false);
     };
 
+    const handleMouseUp = (event: MouseEvent) => {
+        const text = getSelectionText();
+        if (text) {
+            handleOpen(event);
+        } else {
+            handleClose();
+        }
+    };
+
+    const handleMouseDown = (_event: MouseEvent) => {
+        handleClose();
+    };
+
+    useEffect(() => {
+        window.addEventListener('mouseup', handleMouseUp);
+        window.addEventListener('mousedown', handleMouseDown);
+        return () => {
+            window.removeEventListener('mouseup', handleMouseUp);
+            window.removeEventListener('mousedown', handleMouseDown);
+        };
+    }, []);
+
     return (
-        // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-        <div onContextMenu={handleContextMenu} style={{ cursor: 'context-menu' }}>
+        <div ref={divRef}>
             {children}
-            <Menu
-                open={contextMenu !== null}
-                onClose={handleClose}
-                anchorReference="anchorPosition"
-                anchorPosition={
-                    contextMenu !== null
-                        ? { top: contextMenu.mouseY, left: contextMenu.mouseX }
-                        : undefined
-                }>
-                {menuOptions.map((mo) => (
-                    <MenuItem
-                        key={mo.label}
-                        onClick={() => {
-                            handleClose();
-                            mo.action(getSelectionText(), contextMenu?.mouseX, contextMenu?.mouseY);
-                        }}>
-                        {mo.label}
-                    </MenuItem>
+            <SpeedDial
+                ariaLabel="Options for selected text"
+                hidden={!showDial}
+                FabProps={{ size: 'small' }}
+                sx={{
+                    position: 'absolute',
+                    top: contextPos?.mouseY,
+                    left: contextPos?.mouseX,
+                }}
+                icon={<SpeedDialIcon />}>
+                {menuOptions.map((action) => (
+                    <SpeedDialAction
+                        key={action.label}
+                        icon={action.icon}
+                        tooltipTitle={action.label}
+                        onClick={() =>
+                            action.action(
+                                getSelectionText() || '',
+                                contextPos?.mouseX,
+                                contextPos?.mouseY
+                            )
+                        }
+                    />
                 ))}
-            </Menu>
+            </SpeedDial>
         </div>
     );
 };
