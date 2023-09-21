7581ec69532a8b5c19cdbcd65534962bfc15c9e3
jon borchardt <jonathanb@allenai.org>
2023-09-21 15:21:15 -0700

    cleanup

--- src/components/draft/CopyRawButton.tsx
+++ src/components/draft/CopyRawButton.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
-import { Dialog, DialogTitle, Tooltip } from '@mui/material';
+import { Tooltip } from '@mui/material';
 import { ToolbarChildrenProps } from '@draft-js-plugins/inline-toolbar/lib/components/Toolbar';
 import { convertToRaw } from 'draft-js';
 
@@ -8,49 +7,27 @@ import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
 
 import { CustomButton, CustomButtonWrapper } from './Toolbar';
 
-// custom button added to toolbar to search the pretraining data
 export const LogRawButton = (props: ToolbarChildrenProps) => {
-    const [rawModalOpen, setMetadataModalOpen] = React.useState(false);
-    const handleModalOpen = () => setMetadataModalOpen(true);
-    const handleModalClose = () => setMetadataModalOpen(false);
-
-    const RawModal = () => {
-        return (
-            <Dialog disableRestoreFocus onClose={handleModalClose} open={rawModalOpen}>
-                <Metadata>
-                    {JSON.stringify(
-                        convertToRaw(props.getEditorState().getCurrentContent()),
-                        null,
-                        4
-                    )}
-                </Metadata>
-            </Dialog>
-        );
-    };
-
     // When using a click event inside overridden content, mouse down
     // events needs to be prevented so the focus stays in the editor
     // and the toolbar remains visible
-    const onMouseDown = (event: React.MouseEvent) => event.preventDefault();
+    const onMouseDown = (event: React.MouseEvent) => {
+        event.preventDefault();
+    };
 
     const onClick = () => {
-        handleModalOpen();
+        navigator.clipboard.writeText(
+            JSON.stringify(convertToRaw(props.getEditorState().getCurrentContent()), null, 4)
+        );
     };
 
     return (
-        <>
-            <CustomButtonWrapper onMouseDown={onMouseDown}>
-                <CustomButton onClick={onClick}>
-                    <Tooltip title="Copy Raw Data">
-                        <LibraryBooksIcon />
-                    </Tooltip>
-                </CustomButton>
-            </CustomButtonWrapper>
-            <RawModal />
-        </>
+        <CustomButtonWrapper onMouseDown={onMouseDown}>
+            <CustomButton onClick={onClick}>
+                <Tooltip title="Copy raw data to clipboard">
+                    <LibraryBooksIcon />
+                </Tooltip>
+            </CustomButton>
+        </CustomButtonWrapper>
     );
 };
-
-const Metadata = styled(DialogTitle)`
-    white-space: pre;
-`;
