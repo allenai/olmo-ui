bedc15bfa940420b2f9950ed968d6be02b377423
arnavic <arnavic@allenai.org>
2023-09-26 16:33:49 -0700

    fixing broken border radius

--- src/components/shared.tsx
+++ src/components/shared.tsx
@@ -5,7 +5,7 @@ export const Content = styled(Stack).attrs({ spacing: 3 })``;
 
 export const SearchResultsContainer = styled(Box)`
     background: white;
-    border-radius: 2;
+    border-radius: 10px;
     ${({ theme }) => theme.breakpoints.up('md')} {
         padding: ${({ theme }) => `${theme.spacing(5)} ${theme.spacing(6)}`};
     }
