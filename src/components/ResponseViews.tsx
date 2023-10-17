2e328d0e6fcc9e28dbb96d48ede85ed4c7561af0
arnavic <arnavic@allenai.org>
2023-10-17 16:28:50 -0700

    initial round of changes for on-hover context and branch menus

---
+++ src/components/ResponseViews.tsx
@@ -0,0 +1,136 @@
+import React, { useState } from 'react';
+import { Stack, Typography } from '@mui/material';
+import styled from 'styled-components';
+import { Marked } from 'marked';
+import { markedHighlight } from 'marked-highlight';
+import hljs from 'highlight.js';
+import DOMPurify from 'dompurify';
+
+import 'highlight.js/styles/github-dark.css';
+import { RobotAvatar } from './avatars/RobotAvatar';
+import { UserAvatar } from './avatars/UserAvatar';
+
+interface WrapperProps {
+    children: JSX.Element;
+    setHover: (value: boolean) => void;
+}
+
+const Wrapper = ({ children, setHover }: WrapperProps) => {
+    return (
+        <div
+            role="presentation" // TODO: need a better a11y keyboard-only story pre-release
+            onMouseEnter={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            onFocus={() => setHover(true)}
+            onBlur={() => setHover(true)}>
+            {children}
+        </div>
+    );
+};
+
+interface AgentResponseProps {
+    msgId: string;
+    response: string;
+    contextMenu?: JSX.Element;
+    branchMenu?: JSX.Element;
+    isEditedResponse?: boolean;
+}
+
+export const LLMResponseView = ({
+    response,
+    msgId,
+    contextMenu,
+    branchMenu,
+    isEditedResponse = false,
+}: AgentResponseProps) => {
+    const [hover, setHover] = useState(false);
+
+    const marked = new Marked(
+        markedHighlight({
+            langPrefix: 'hljs language-',
+            highlight(code, lang) {
+                const language = hljs.getLanguage(lang) ? lang : 'plaintext';
+                return hljs.highlight(code, { language }).value;
+            },
+        })
+    );
+    // turning off features as they pop dom warnings
+    marked.use({
+        mangle: false,
+        headerIds: false,
+    });
+    const html = DOMPurify.sanitize(marked.parse(response));
+    return (
+        <Wrapper setHover={setHover}>
+            <Stack direction="row">
+                {isEditedResponse ? (
+                    <Stack direction="column" spacing={-1}>
+                        <RobotAvatar />
+                        <UserAvatar />
+                    </Stack>
+                ) : (
+                    <RobotAvatar />
+                )}
+                <LLMResponseContainer id={msgId}>
+                    <Stack direction="row" justifyContent="space-between">
+                        <div
+                            dangerouslySetInnerHTML={{ __html: html }}
+                            style={{ background: 'transparent' }}
+                        />
+                        <Stack
+                            direction="row"
+                            spacing={1}
+                            style={{ visibility: hover ? 'visible' : 'hidden' }}>
+                            {contextMenu || null}
+                            {branchMenu || null}
+                        </Stack>
+                    </Stack>
+                </LLMResponseContainer>
+            </Stack>
+        </Wrapper>
+    );
+};
+
+export const UserResponseView = ({
+    response,
+    msgId,
+    contextMenu,
+    branchMenu,
+}: AgentResponseProps) => {
+    const [hover, setHover] = useState(false);
+
+    return (
+        <Wrapper setHover={setHover}>
+            <Stack direction="row" justifyContent="space-between">
+                <Stack direction="row">
+                    <UserAvatar />
+                    <UserResponseContainer id={msgId}>
+                        <TitleTypography sx={{ fontWeight: 'bold' }}>{response}</TitleTypography>
+                    </UserResponseContainer>
+                </Stack>
+                <Stack direction="row" spacing={1}>
+                    {contextMenu && hover ? contextMenu : null}
+                    {branchMenu && hover ? branchMenu : null}
+                </Stack>
+            </Stack>
+        </Wrapper>
+    );
+};
+
+const UserResponseContainer = styled.div`
+    padding-top: ${({ theme }) => theme.spacing(1)};
+    padding-bottom: ${({ theme }) => theme.spacing(2)};
+    margin-left: ${({ theme }) => theme.spacing(1)};
+`;
+
+const LLMResponseContainer = styled.div`
+    background-color: ${({ theme }) => theme.color2.N1};
+    border-radius: ${({ theme }) => theme.shape.borderRadius};
+    padding: ${({ theme }) => theme.spacing(2)};
+    margin-left: ${({ theme }) => theme.spacing(1)};
+    width: 100%;
+`;
+
+const TitleTypography = styled(Typography)`
+    color: ${({ theme }) => theme.color2.B5};
+`;
--- src/components/ThreadBodyView.tsx
+++ src/components/ThreadBodyView.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import {
     Box,
-    Chip,
+    Button,
     Grid,
     IconButton,
     LinearProgress,
+    ListItemIcon,
+    ListItemText,
     Menu,
     MenuItem,
     Stack,
@@ -12,12 +14,9 @@ import {
     Typography,
 } from '@mui/material';
 import styled from 'styled-components';
-import { Marked } from 'marked';
-import { markedHighlight } from 'marked-highlight';
-import hljs from 'highlight.js';
-import DOMPurify from 'dompurify';
 
 import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import EditIcon from '@mui/icons-material/Edit';
 import CheckIcon from '@mui/icons-material/Check';
 import ClearIcon from '@mui/icons-material/Clear';
@@ -26,10 +25,7 @@ import { Message, MessagePost } from '../api/Message';
 import { Role } from '../api/Role';
 import { BarOnRightContainer } from './BarOnRightContainer';
 import { useAppContext } from '../AppContext';
-
-import 'highlight.js/styles/github-dark.css';
-import { RobotAvatar } from './avatars/RobotAvatar';
-import { UserAvatar } from './avatars/UserAvatar';
+import { LLMResponseView, UserResponseView } from './ResponseViews';
 
 interface ThreadBodyProps {
     parent?: Message;
@@ -38,55 +34,8 @@ interface ThreadBodyProps {
     disabledActions?: boolean;
 }
 
-interface AgentResponseProps {
-    msgId: string;
-    response: string;
-    isEditedResponse?: boolean;
-}
-
-const LLMResponseView = ({ response, msgId, isEditedResponse = false }: AgentResponseProps) => {
-    const marked = new Marked(
-        markedHighlight({
-            langPrefix: 'hljs language-',
-            highlight(code, lang) {
-                const language = hljs.getLanguage(lang) ? lang : 'plaintext';
-                return hljs.highlight(code, { language }).value;
-            },
-        })
-    );
-    // turning off features as they pop dom warnings
-    marked.use({
-        mangle: false,
-        headerIds: false,
-    });
-    const html = DOMPurify.sanitize(marked.parse(response));
-    return (
-        <Stack direction="row">
-            {isEditedResponse ? (
-                <Stack direction="column" spacing={-1}>
-                    <RobotAvatar />
-                    <UserAvatar />
-                </Stack>
-            ) : (
-                <RobotAvatar />
-            )}
-            <LLMResponseContainer id={msgId} dangerouslySetInnerHTML={{ __html: html }} />
-        </Stack>
-    );
-};
-
-const UserResponseView = ({ response, msgId }: AgentResponseProps) => {
-    return (
-        <Stack direction="row">
-            <UserAvatar />
-            <UserResponseContainer id={msgId}>
-                <TitleTypography sx={{ fontWeight: 'bold' }}>{response}</TitleTypography>
-            </UserResponseContainer>
-        </Stack>
-    );
-};
-
 const MENU_MAX_HEIGHT = 48 * 4.5;
+const MENU_WIDTH = '20ch';
 
 export const ThreadBodyView = ({
     parent,
@@ -103,25 +52,20 @@ export const ThreadBodyView = ({
     const [isLoading, setIsLoading] = useState(false);
     const [editMessageContent, setEditMessageContent] = useState('');
     const [curMessageIndex, setCurMessageIndex] = React.useState(0);
-    // the anchor element anchors the expanded dropdown menu to the dropdown menu button element
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const open = Boolean(anchorEl);
+    // the anchor elements anchors the relevant dropdown menu to the dropdown menu button element (contextmenu, branchmenu)
+    const [branchMenuAnchorEl, setBranchMenuAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [contextMenuAnchorEl, setContextMenuAnchorEl] = React.useState<null | HTMLElement>(null);
+
+    const handleBranchMenuSelect = (index: number) => {
+        setCurMessageIndex(index);
+        setBranchMenuAnchorEl(null);
+    };
+
+    const branchMenuOpen = Boolean(branchMenuAnchorEl);
+    const contextMenuOpen = Boolean(contextMenuAnchorEl);
 
     const [isSubmitting, setIsSubmitting] = useState(false);
     const [followUpPrompt, setFollowUpPrompt] = useState<string>();
-
-    const handleMenuClick = (event: React.MouseEvent<HTMLElement>) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleSelect = (index: number) => {
-        setCurMessageIndex(index);
-        setAnchorEl(null);
-    };
-
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
 
     const branchCount = messages.length;
     const curMessage = messages[curMessageIndex];
@@ -148,16 +92,78 @@ export const ThreadBodyView = ({
             role: curMessageRole,
             original: curMessage.id,
         };
-        handleSelect(0); // 0 because the new message is unshifted
+        handleBranchMenuSelect(0); // 0 because the new message is unshifted
         const postMessageInfo = await postMessage(payload, parent);
         if (!postMessageInfo.loading && postMessageInfo.data && !postMessageInfo.error) {
             setIsLoading(false);
         }
     };
 
-    // we add mouseover functionality via the BarOnRightContainer to enable a right side border on hover.
-    // we do this using mouseover and mouseout (and onblur/onfocus for a11y) because the :hover css
-    // pseudo-class does not preserve the border if you click into the menu, so this is more robust.
+    const menuPaperStyle = {
+        maxHeight: MENU_MAX_HEIGHT,
+        width: MENU_WIDTH,
+    };
+
+    const contextMenu = (
+        <MenuWrapperContainer>
+            <ResponseMenuButton
+                variant="outlined"
+                disabled={isLoading || disabledActions}
+                onClick={(e) => setContextMenuAnchorEl(e.currentTarget)}>
+                <MoreHorizIcon />
+            </ResponseMenuButton>
+            <Menu
+                anchorEl={contextMenuAnchorEl}
+                open={contextMenuOpen}
+                onClose={() => setContextMenuAnchorEl(null)}
+                PaperProps={{
+                    style: menuPaperStyle,
+                }}>
+                <MenuItem
+                    key={'edit'}
+                    onClick={() => {
+                        setIsEditing(true);
+                        setContextMenuAnchorEl(null);
+                    }}>
+                    <ListItemIcon>
+                        <EditIcon fontSize="small" />
+                    </ListItemIcon>
+                    <ListItemText>Edit</ListItemText>
+                </MenuItem>
+            </Menu>
+        </MenuWrapperContainer>
+    );
+
+    const branchesMenu = (
+        <MenuWrapperContainer>
+            <ResponseMenuButton
+                startIcon={<KeyboardArrowDown />}
+                variant="outlined"
+                onClick={(e) => setBranchMenuAnchorEl(e.currentTarget)}>
+                <Typography noWrap>View {branchCount} branches</Typography>
+            </ResponseMenuButton>
+            <Menu
+                anchorEl={branchMenuAnchorEl}
+                open={branchMenuOpen}
+                onClose={() => setBranchMenuAnchorEl(null)}
+                PaperProps={{
+                    style: menuPaperStyle,
+                }}>
+                {messages.map((msg, i) => (
+                    <MenuItem
+                        key={i}
+                        onClick={() => handleBranchMenuSelect(i)}
+                        selected={i === curMessageIndex}
+                        title={msg.content}>
+                        <Typography variant="inherit" noWrap>
+                            {msg.content}
+                        </Typography>
+                    </MenuItem>
+                ))}
+            </Menu>
+        </MenuWrapperContainer>
+    );
+
     return (
         <BarOnRightContainer displayBar={branchCount > 1}>
             <>
@@ -179,7 +185,6 @@ export const ThreadBodyView = ({
                                             <OutlinedIconButton
                                                 sx={{ border: 1, borderRadius: 0, p: 0 }}
                                                 size="small"
-                                                // color="primary"
                                                 disabled={!editMessageContent?.length}
                                                 onClick={editMessage}>
                                                 <CheckIcon />
@@ -191,7 +196,6 @@ export const ThreadBodyView = ({
                                             <OutlinedIconButton
                                                 sx={{ border: 1, borderRadius: 0, p: 0 }}
                                                 size="small"
-                                                // color="primary"
                                                 onClick={() => setIsEditing(false)}>
                                                 <ClearIcon />
                                             </OutlinedIconButton>
@@ -204,6 +208,8 @@ export const ThreadBodyView = ({
                                         <UserResponseView
                                             response={curMessage.content}
                                             msgId={curMessage.id}
+                                            contextMenu={!isEditing ? contextMenu : undefined}
+                                            branchMenu={branchCount > 1 ? branchesMenu : undefined}
                                         />
                                     ) : (
                                         <LLMResponseView
@@ -213,52 +219,13 @@ export const ThreadBodyView = ({
                                                 curMessage.original !== undefined &&
                                                 curMessage.original?.length > 0
                                             }
+                                            contextMenu={!isEditing ? contextMenu : undefined}
+                                            branchMenu={branchCount > 1 ? branchesMenu : undefined}
                                         />
                                     )}
                                 </>
                             )}
                         </Grid>
-                        {!isEditing && (
-                            <EditButton
-                                disabled={isLoading || disabledActions}
-                                onClick={() => setIsEditing(true)}
-                            />
-                        )}
-                        {branchCount > 1 && (
-                            <Grid item>
-                                <MenuWrapperContainer>
-                                    <Chip
-                                        icon={<KeyboardArrowDown />}
-                                        label={'View ' + branchCount + ' branches'}
-                                        variant="outlined"
-                                        clickable
-                                        onClick={handleMenuClick}
-                                    />
-                                    <Menu
-                                        anchorEl={anchorEl}
-                                        open={open}
-                                        onClose={handleClose}
-                                        PaperProps={{
-                                            style: {
-                                                maxHeight: MENU_MAX_HEIGHT,
-                                                width: '20ch',
-                                            },
-                                        }}>
-                                        {messages.map((msg, i) => (
-                                            <MenuItem
-                                                key={i}
-                                                onClick={() => handleSelect(i)}
-                                                selected={i === curMessageIndex}
-                                                title={msg.content}>
-                                                <Typography variant="inherit" noWrap>
-                                                    {msg.content}
-                                                </Typography>
-                                            </MenuItem>
-                                        ))}
-                                    </Menu>
-                                </MenuWrapperContainer>
-                            </Grid>
-                        )}
                     </Stack>
                 </Box>
                 {curMessage.children ? (
@@ -292,20 +259,16 @@ export const ThreadBodyView = ({
     );
 };
 
-const EditButton = ({ disabled, onClick }: { disabled: boolean; onClick: () => void }) => (
-    <Grid item>
-        <MenuWrapperContainer>
-            <Chip
-                icon={<EditIcon sx={{ fontSize: 16 }} />}
-                label={'Edit'}
-                disabled={disabled}
-                variant="outlined"
-                clickable
-                onClick={onClick}
-            />
-        </MenuWrapperContainer>
-    </Grid>
-);
+const ResponseMenuButton = styled(Button)`
+    &&& {
+        padding: ${({ theme }) => theme.spacing(0.75)};
+        min-width: 20px;
+        height: 25px;
+        color: ${({ theme }) => theme.color2.N5};
+        border-color: ${({ theme }) => theme.color2.N5};
+        font-size: 12px;
+    }
+`;
 
 const FollowUpContainer = styled.div`
     padding-left: ${({ theme }) => theme.spacing(2)};
@@ -323,22 +286,4 @@ const OutlinedIconButton = styled(IconButton)`
 
 const MenuWrapperContainer = styled.div`
     padding-top: ${({ theme }) => theme.spacing(1)};
-`;
-
-const UserResponseContainer = styled.div`
-    padding-top: ${({ theme }) => theme.spacing(1)};
-    padding-bottom: ${({ theme }) => theme.spacing(2)};
-    margin-left: ${({ theme }) => theme.spacing(1)};
-`;
-
-const LLMResponseContainer = styled.div`
-    background-color: ${({ theme }) => theme.color2.N1};
-    border-radius: ${({ theme }) => theme.shape.borderRadius};
-    padding: ${({ theme }) => theme.spacing(2)};
-    margin-left: ${({ theme }) => theme.spacing(1)};
-    width: 100%;
-`;
-
-const TitleTypography = styled(Typography)`
-    color: ${({ theme }) => theme.color2.B5};
 `;
