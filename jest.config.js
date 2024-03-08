11cef0698ead79bd3d76cf24aa7f6af393b637ea
Taylor Blanton <matthewtblanton@gmail.com>
2024-03-08 12:05:11 -0600

    fix vitest config

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  collectCoverage: true,
-  collectCoverageFrom: ["src/**/*.ts,tsx}"],
-  coverageDirectory: "coverage",
-  preset: "ts-jest/presets/default-esm",
-  roots: ["<rootDir>"],
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
-};
