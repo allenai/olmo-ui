7f31062abdbccad6e6d90808fc4846fe1595ae9f
Taylor Blanton <matthewtblanton@gmail.com>
2024-03-12 17:00:09 -0500

    move to vitest globals, add vitest and jest-dom eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,28 @@ module.exports = {
     rules: {
         '@typescript-eslint/no-use-before-define': 0,
     },
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            rules: {
+                // Typescript handles no undefined for us already. We can safely disable this in TS files
+                'no-undef': 'off',
+            },
+        },
+        {
+            files: ['**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+            plugins: ['jest-dom', 'vitest'],
+            extends: ['plugin:jest-dom/recommended', 'plugin:vitest/recommended'],
+            rules: {
+                'no-restricted-imports': [
+                    'warn',
+                    {
+                        name: '@testing-library/react',
+                        message:
+                            'Import from @test-utils instead. It provides a VarnishApp wrapper for our themed components.',
+                    },
+                ],
+            },
+        },
+    ],
 };
