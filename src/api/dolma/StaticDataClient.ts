9add23fcea0f961e0beb7dc315e00c9ecc75c709
Taylor Blanton <matthewtblanton@gmail.com>
2024-03-14 17:34:07 -0500

    upgrade node types, update yarn lock, make StaticDataClient.getData private

--- src/api/dolma/StaticDataClient.ts
+++ src/api/dolma/StaticDataClient.ts
@@ -4,29 +4,29 @@ import { staticData } from './staticData';
 export class StaticDataClient extends ClientBase {
     staticFilePath = `${process.env.DOLMA_API_URL}/api/static`;
 
-    getData = async <TResponse>(filePath: string): Promise<TResponse> => {
+    #getData = async <TResponse>(filePath: string): Promise<TResponse> => {
         const url = `${this.staticFilePath}/${filePath}`;
         const response = await fetch(url);
         return this.unpack<TResponse>(response);
     };
 
     getSources = async (): Promise<staticData.Sources> => {
-        return this.getData('/sources.json');
+        return this.#getData('/sources.json');
     };
 
     getWords = async (): Promise<staticData.BinnedBySource> => {
-        return this.getData('/words/data.json');
+        return this.#getData('/words/data.json');
     };
 
     getCreated = async (): Promise<staticData.BinnedBySource> => {
-        return this.getData('/created/data.json');
+        return this.#getData('/created/data.json');
     };
 
     getDomains = async (): Promise<staticData.DomainsBySource> => {
-        return this.getData('/domains/data.json');
+        return this.#getData('/domains/data.json');
     };
 
     getSourceCounts = async (): Promise<staticData.SourceCounts> => {
-        return this.getData('/source_counts/data.json');
+        return this.#getData('/source_counts/data.json');
     };
 }
