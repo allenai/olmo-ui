748c4ef9e3ecb6466aacad51b51fef72dce8af37
huytr1995 <huyt@allenai.org>
2024-03-20 15:22:14 -0700

    Split ThreadBodyView's inline components into separate components

---
+++ src/components/ChatResponseView.tsx
@@ -0,0 +1,71 @@
+import { Role } from '../api/Role';
+import { Message } from '../api/Message';
+import { BranchMenu } from './BranchMenu';
+import { UserResponseView } from './ResponseView/UserResponseView';
+import { LLMResponseView } from './ResponseView/LLMResponseView';
+
+interface ChatResponseViewProps {
+    messages: Message[];
+    setBranchMenuAnchorEl: (anchor: null | HTMLElement) => void;
+    handleBranchMenuSelect: (index: number) => void;
+    branchMenuAnchorEl: HTMLElement | null;
+    curMessageIndex: number;
+    isEditing: boolean;
+    contextMenu: JSX.Element;
+}
+
+export const ChatResponseView = ({
+    messages,
+    setBranchMenuAnchorEl,
+    handleBranchMenuSelect,
+    curMessageIndex,
+    branchMenuAnchorEl,
+    isEditing,
+    contextMenu,
+}: ChatResponseViewProps) => {
+    const curMessage = messages[curMessageIndex];
+    const branchCount = messages.length;
+
+    if (curMessage.role === Role.User) {
+        return (
+            <UserResponseView
+                response={curMessage.content}
+                msgId={curMessage.id}
+                contextMenu={!isEditing ? contextMenu : undefined}
+                branchMenu={
+                    branchCount > 1 ? (
+                        <BranchMenu
+                            setBranchMenuAnchorEl={setBranchMenuAnchorEl}
+                            branchMenuAnchorEl={branchMenuAnchorEl}
+                            messages={messages}
+                            curMessageIndex={curMessageIndex}
+                            handleBranchMenuSelect={handleBranchMenuSelect}
+                        />
+                    ) : undefined
+                }
+                displayBranchIcon={branchCount > 1}
+            />
+        );
+    }
+
+    return (
+        <LLMResponseView
+            response={curMessage.content}
+            msgId={curMessage.id}
+            isEditedResponse={curMessage.original !== undefined && curMessage.original?.length > 0}
+            contextMenu={!isEditing ? contextMenu : undefined}
+            branchMenu={
+                branchCount > 1 ? (
+                    <BranchMenu
+                        setBranchMenuAnchorEl={setBranchMenuAnchorEl}
+                        branchMenuAnchorEl={branchMenuAnchorEl}
+                        messages={messages}
+                        curMessageIndex={curMessageIndex}
+                        handleBranchMenuSelect={handleBranchMenuSelect}
+                    />
+                ) : undefined
+            }
+            displayBranchIcon={branchCount > 1}
+        />
+    );
+};
