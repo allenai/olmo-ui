1921352fed4fd962d44dde5f756892576b57baa9
Taylor Blanton <matthewtblanton@gmail.com>
2024-03-21 16:20:31 -0500

    try not using the compose

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
-        baseURL: 'http://localhost:8080',
+        baseURL: 'http://localhost:3000',
 
         /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
         // trace: 'on-first-retry',
@@ -72,8 +72,8 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: 'docker compose up --build',
-        url: 'http://localhost:8080',
-        reuseExistingServer: true,
+        command: 'yarn start',
+        url: 'http://localhost:3000',
+        reuseExistingServer: !process.env.CI,
     },
 });
