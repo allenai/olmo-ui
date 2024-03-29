212d98c85396905b805302bd18ce3e33542d750a
huytr1995 <huyt@allenai.org>
2024-03-29 09:15:16 -0700

    Add History Button

---
+++ src/components/HistoryButton.tsx
@@ -0,0 +1,145 @@
+import { Button, Divider, IconButton, List, Stack } from '@mui/material';
+import HistoryIcon from '@mui/icons-material/History';
+import CloseIcon from '@mui/icons-material/Close';
+import { useEffect, useState } from 'react';
+
+import { useLocation } from 'react-router-dom';
+
+import { ResponsiveDrawer } from './ResponsiveDrawer';
+import { useAppContext } from '../AppContext';
+import { NavigationHeading } from './OlmoAppBar/NavigationHeading';
+import { ThreadLink } from '../components/ThreadLink';
+import { Message } from '../api/Message';
+
+export const HistoryButton = () => {
+    const userInfo = useAppContext((state) => state.userInfo);
+    const getAllThreads = useAppContext((state) => state.getAllThreads);
+    const allThreadInfo = useAppContext((state) => state.allThreadInfo);
+    const loc = useLocation();
+    const qs = new URLSearchParams(loc.search);
+    const p = parseInt(qs.get('page') ?? '');
+    const page = isNaN(p) ? 1 : p;
+
+    const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+    const handleDrawerClose = () => {
+        setIsDrawerOpen(false);
+    };
+
+    const size = 10;
+
+    useEffect(() => {
+        const creator = userInfo?.data?.client;
+        const offset = (page - 1) * size;
+        getAllThreads(offset, creator);
+    }, [userInfo, page, size]);
+
+    const [todayThread, setTodayThread] = useState<Message[]>([]);
+    const [pastSevenDaysThread, setPastSevenDaysThread] = useState<Message[]>([]);
+    const [pastThirtyDaysThread, setPastThirtyDaysThread] = useState<Message[]>([]);
+
+    useEffect(() => {
+        if (!allThreadInfo.loading && !allThreadInfo.error && allThreadInfo.data) {
+            const tempToday: Message[] = [];
+            const tempPastSevenDays: Message[] = [];
+            const tempPastThirtyDays: Message[] = [];
+            allThreadInfo.data.messages.forEach((m) => {
+                const createdDay = m.created;
+                if (createdDay.toDateString() === new Date().toDateString()) {
+                    tempToday.push(m);
+                } else if (
+                    new Date().getDate() - createdDay.getDate() > 7 &&
+                    new Date().getDate() - createdDay.getDate() <= 30
+                ) {
+                    tempPastSevenDays.push(m);
+                } else {
+                    tempPastThirtyDays.push(m);
+                }
+            });
+            setTodayThread(tempToday);
+            setPastSevenDaysThread(tempPastSevenDays);
+            setPastThirtyDaysThread(tempPastThirtyDays);
+        }
+    }, [allThreadInfo]);
+
+    const toggleDrawer = () => {
+        setIsDrawerOpen(!isDrawerOpen);
+    };
+
+    return (
+        <>
+            <Button
+                component="label"
+                variant="contained"
+                tabIndex={-1}
+                startIcon={<HistoryIcon />}
+                onClick={toggleDrawer}
+                disabled={allThreadInfo.loading || allThreadInfo.error}>
+                History
+            </Button>
+            {!allThreadInfo.loading && !allThreadInfo.error && allThreadInfo.data ? (
+                <ResponsiveDrawer
+                    onClose={handleDrawerClose}
+                    open={isDrawerOpen}
+                    anchor="right"
+                    desktopDrawerVariant="persistent"
+                    desktopHeading={
+                        <Stack justifyContent="space-between" direction="row" gap={2}>
+                            <NavigationHeading>History</NavigationHeading>
+                            <IconButton
+                                onClick={handleDrawerClose}
+                                sx={{ verticalAlign: 'middle', display: 'inline-flex' }}>
+                                <CloseIcon />
+                            </IconButton>
+                        </Stack>
+                    }
+                    desktopDrawerSx={{ gridArea: 'side-drawer' }}>
+                    <Stack
+                        component="nav"
+                        direction="column"
+                        justifyContent="space-between"
+                        height="1">
+                        <List>
+                            <NavigationHeading>Today</NavigationHeading>
+                            {todayThread.map((t, index) => (
+                                <ThreadLink
+                                    href={`/thread/${t.id}`}
+                                    content={t.content}
+                                    timeStamp={t.created}
+                                    key={index}
+                                    threadId={t.id}
+                                />
+                            ))}
+                        </List>
+                        <Divider />
+                        <List>
+                            <NavigationHeading>Previous 7 Days</NavigationHeading>
+                            {pastSevenDaysThread.map((pS, index) => (
+                                <ThreadLink
+                                    href={`/thread/${pS.id}`}
+                                    content={pS.content}
+                                    timeStamp={pS.created}
+                                    key={index}
+                                    threadId={pS.id}
+                                />
+                            ))}
+                        </List>
+                        <Divider />
+                        <List>
+                            <NavigationHeading>Previous 30 Days</NavigationHeading>
+                            {pastThirtyDaysThread.map((pT, index) => (
+                                <ThreadLink
+                                    href={`/thread/${pT.id}`}
+                                    content={pT.content}
+                                    timeStamp={pT.created}
+                                    key={index}
+                                    threadId={pT.id}
+                                />
+                            ))}
+                        </List>
+                    </Stack>
+                </ResponsiveDrawer>
+            ) : null}
+        </>
+    );
+};
---
+++ src/components/ThreadLink.tsx
@@ -0,0 +1,91 @@
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { Link, ListItem, ListItemButton, ListItemText, Stack } from '@mui/material';
+import { useParams } from 'react-router-dom';
+
+const todayDateFormatter = new Intl.DateTimeFormat(undefined, {
+    hour: 'numeric',
+    minute: 'numeric',
+});
+const pastWeekDateFormatter = new Intl.DateTimeFormat(undefined, { weekday: 'long' });
+const pastMonthDateFormatter = new Intl.DateTimeFormat(undefined, {
+    month: 'numeric',
+    day: 'numeric',
+    year: 'numeric',
+});
+
+const isCurrentDay = (date: Date): boolean => {
+    const dateClone = new Date(date);
+
+    return dateClone.setHours(0, 0, 0, 0) === new Date().setHours(0, 0, 0, 0);
+};
+
+const isPastWeek = (date: Date): boolean => {
+    return new Date().getDate() - date.getDate() > 7 && new Date().getDate() - date.getDate() <= 30;
+};
+interface ThreadLinkProps {
+    content: string;
+    timeStamp: Date;
+    href: string;
+    threadId: string;
+}
+export const ThreadLink = ({ content, timeStamp, href, threadId }: ThreadLinkProps) => {
+    const { id } = useParams();
+
+    const isSelected = id === threadId;
+
+    const displayTime = (): string => {
+        if (isCurrentDay(timeStamp)) {
+            return todayDateFormatter.format(timeStamp);
+        } else if (isPastWeek(timeStamp)) {
+            return pastWeekDateFormatter.format(timeStamp);
+        }
+        return pastMonthDateFormatter.format(timeStamp);
+    };
+
+    return (
+        <ListItem disableGutters>
+            <ListItemButton
+                alignItems="center"
+                selected={isSelected}
+                sx={{
+                    gap: (theme) => theme.spacing(1),
+                    '&.Mui-selected': {
+                        backgroundColor: (theme) => theme.palette.primary.main,
+                        color: (theme) => theme.palette.primary.contrastText,
+
+                        '&:focus-visible,&:hover': {
+                            backgroundColor: (theme) => theme.palette.primary.dark,
+                        },
+                    },
+                }}
+                component={Link}
+                href={href}>
+                <ListItemText
+                    primaryTypographyProps={{
+                        variant: 'caption',
+                        color: 'inherit',
+                        sx: {
+                            margin: 0,
+                            textOverflow: 'ellipsis',
+                            whiteSpace: 'nowrap',
+                            overflow: 'hidden',
+                        },
+                    }}>
+                    {content}
+                </ListItemText>
+
+                <Stack direction="row" alignItems="center" gap={1}>
+                    <ListItemText
+                        primaryTypographyProps={{
+                            variant: 'caption',
+                            color: 'inherit',
+                            sx: { margin: 0 },
+                        }}>
+                        {displayTime()}
+                    </ListItemText>
+                    <ChevronRightIcon sx={{ marginInlineStart: 'auto' }} />
+                </Stack>
+            </ListItemButton>
+        </ListItem>
+    );
+};
