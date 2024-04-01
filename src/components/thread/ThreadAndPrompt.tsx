c01453ab78546b5faa5c319d1bad875372dcd0ee
Taylor Blanton <matthewtblanton@gmail.com>
2024-04-01 17:27:46 -0500

    pull out ChatMessage

--- src/components/thread/ThreadAndPrompt.tsx
+++ src/components/thread/ThreadAndPrompt.tsx
@@ -1,49 +1,11 @@
-import { Box, Paper, Stack, Typography } from '@mui/material';
-import { PropsWithChildren, useEffect } from 'react';
+import { Stack } from '@mui/material';
+import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import { useAppContext } from '../../AppContext';
 
-import { RobotAvatar } from '../avatars/RobotAvatar';
 import { Message } from '@/api/Message';
-import { Role } from '@/api/Role';
-
-const UserMessage = ({ children }: PropsWithChildren): JSX.Element => {
-    return <Typography fontWeight="bold">{children}</Typography>;
-};
-
-const LLMMessage = ({ children }: PropsWithChildren): JSX.Element => {
-    return (
-        <Paper
-            variant="outlined"
-            elevation={1}
-            sx={{
-                border: 'none',
-                backgroundColor: (theme) => theme.palette.background.paper,
-                padding: 2,
-            }}>
-            <Typography>{children}</Typography>
-        </Paper>
-    );
-};
-
-interface ChatMessageProps extends PropsWithChildren {
-    variant: 'user' | 'llm';
-}
-
-const ChatMessage = ({ variant, children }: ChatMessageProps): JSX.Element => {
-    const MessageComponent = variant === 'user' ? UserMessage : LLMMessage;
-    const icon = variant === 'user' ? null : <RobotAvatar />;
-
-    return (
-        <Stack direction="row" gap={1} alignItems="start">
-            <Box id="icon" width={28} height={28}>
-                {icon}
-            </Box>
-            <MessageComponent>{children}</MessageComponent>
-        </Stack>
-    );
-};
+import { ChatMessage } from './ChatMessage';
 
 interface MessageViewProps {
     message?: Message;
@@ -58,7 +20,7 @@ const MessageView = ({ message }: MessageViewProps) => {
 
     return (
         <>
-            <ChatMessage variant={role === Role.User ? 'user' : 'llm'}>{content}</ChatMessage>
+            <ChatMessage role={role}>{content}</ChatMessage>
             {/* TODO: add thread handling */}
             <MessageView message={children?.[0]} />
         </>
