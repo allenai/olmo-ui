ca05844d2b19733831acafaa807e5a55a5b7c3c6
Taylor Blanton <matthewtblanton@gmail.com>
2024-04-01 10:42:10 -0500

    link refactoring

--- src/Links.ts
+++ src/Links.ts
@@ -4,4 +4,9 @@ export const links = {
     datasetExplorer: '/dolma',
     ourDatasets: '/dolma/datasets/about',
     thread: (threadId: string) => `/thread/${threadId}`,
-};
+    faqs: '/faqs',
+    dataPolicy: '/data-policy',
+    logOut: '/log-out',
+    feedbackForm:
+        'https://docs.google.com/forms/d/e/1FAIpQLSfmPUnxBss08X8aq7Aiy17YSPhH-OqHzHMIzXg4zsIhAbvqxg/viewform?usp=sf_link',
+} as const;
