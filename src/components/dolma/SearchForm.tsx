215023ddbd661bade847902966a82ae0af593bb4
Eric Marsh <108751850+ericmarsh995@users.noreply.github.com>
2024-04-09 13:36:27 -0700

    apply Figma styles to SearchForm component (#288)
    ## Description
    
    Closes https://github.com/allenai/olmo-ui/issues/278 
    
    - Use
    [figma](https://www.figma.com/file/HzdNG5hmhXtRYV5f7Ed7G3/OLMo-Platform-V0-(2024)?type=design&node-id=44-19521&mode=design&t=G8s8EVz3h2wRQIKh-0)
    styles to style the search form in the DataSet explorer, and the search
    results
    - Prevent search submit if the input is empty
    - The Info icon button has a tooltip that says "FAQs: How does the
    Dataset Explorer work?" and works as a link to the Faqs page
    
    EDIT: Changed the screenshots to show off the latest changes
    
    
    ![Screen Shot 2024-04-09 at 11 32 06
    AM](https://github.com/allenai/olmo-ui/assets/108751850/3bf798e5-44dc-4c1c-9458-f7e636c35198)
    
     
    
    
    https://github.com/allenai/olmo-ui/assets/108751850/40b68860-1bc8-4d27-97ee-b4eb70e749ed
    
    
    
    
    ## A11y
    
    Input, submit button, and tooltip are navigable via keyboard. Voiceover
    reads tooltip message

--- src/components/dolma/SearchForm.tsx
+++ src/components/dolma/SearchForm.tsx
@@ -1,10 +1,23 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, Stack } from '@mui/material';
+import { Button, Card, IconButton, Stack, TextField, Tooltip, styled } from '@mui/material';
 
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+
+import { DESKTOP_LAYOUT_BREAKPOINT } from '@/constants';
 import { search } from '../../api/dolma/search';
-import { PartialWidthTextField, Section } from '../../components/dolma/shared';
 import { useAppContext } from '../../AppContext';
+import { links } from '../../Links';
+
+const SearchTextField = styled(TextField)`
+    background-color: ${({ theme }) => theme.palette.background.default};
+    fieldset {
+        border-color: ${({ theme }) => theme.color.N5.hex};
+    }
+    input::placeholder {
+        opacity: 1;
+    }
+`;
 
 export const SearchForm = ({
     defaultValue,
@@ -26,26 +39,54 @@ export const SearchForm = ({
 
     const nav = useNavigate();
     const submitSearch = (e?: React.KeyboardEvent) => {
-        if (e && e.key !== 'Enter') {
+        if ((e && e.key !== 'Enter') || queryText.trim().length === 0) {
             return;
         }
         nav(`/search?${search.toQueryString({ query: queryText })}`);
     };
-
     return (
-        <Section>
-            <Stack direction={'row'} spacing={2}>
-                <PartialWidthTextField
+        <Card
+            variant="elevation"
+            elevation={1}
+            sx={{
+                padding: (theme) => theme.spacing(2.25),
+                backgroundColor: (theme) => theme.palette.background.default,
+            }}>
+            <Stack gap={1.5} alignItems="flex-start">
+                <SearchTextField
+                    fullWidth
+                    multiline
                     value={queryText}
                     placeholder={placeholder}
                     onChange={(e) => setQueryText(e.currentTarget.value)}
                     onKeyDown={(e) => submitSearch(e)}
                     disabled={disabled}
                 />
-                <Button variant="contained" onClick={() => submitSearch()} disabled={disabled}>
-                    Search
-                </Button>
+                <Stack direction="row" justifyContent="space-between" width="100%">
+                    <Button
+                        variant="contained"
+                        onClick={() => submitSearch()}
+                        sx={{ height: 'fit-content', paddingBlock: 1, paddingInline: 2 }}
+                        disabled={disabled}>
+                        Submit
+                    </Button>
+                    <Tooltip
+                        title="About Dataset Explorer"
+                        sx={{ color: (theme) => theme.palette.text.primary }}>
+                        <IconButton
+                            aria-label="About Dataset Explorer"
+                            size="large"
+                            href={links.faqs}
+                            sx={{
+                                color: (theme) => theme.color.N9.hex,
+                                padding: 0,
+                                display: { xs: 'none', [DESKTOP_LAYOUT_BREAKPOINT]: 'block' },
+                            }}>
+                            <InfoOutlinedIcon />
+                        </IconButton>
+                    </Tooltip>
+                </Stack>
             </Stack>
-        </Section>
+        </Card>
     );
 };
