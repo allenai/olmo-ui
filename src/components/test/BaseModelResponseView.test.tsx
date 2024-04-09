75d6c5b3ddf1a4b3909bab762df2c530411d82ec
Taylor Blanton <matthewtblanton@gmail.com>
2024-04-09 14:51:54 -0500

    remove debug

--- src/components/test/BaseModelResponseView.test.tsx
+++ src/components/test/BaseModelResponseView.test.tsx
@@ -1,10 +1,8 @@
 import { render, screen } from '@test-utils';
-import { debug } from 'vitest-preview';
 
 import { BaseModelResponseView } from '../ResponseView/BaseModelResponseView';
 
 test('LLMResponseView', () => {
     render(<BaseModelResponseView response="This is a response" msgId="testId" />);
-    debug();
     expect(screen.getByText('This is a response')).toBeVisible();
 });
