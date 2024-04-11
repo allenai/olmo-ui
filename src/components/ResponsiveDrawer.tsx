f4c4f6172eac3c72e70ddd0c79526f22686ec5e8
huytr1995 <huyt@allenai.org>
2024-04-11 12:33:35 -0700

    Add MediaQuery to render Drawer based on Desktop or Mobile

--- src/components/ResponsiveDrawer.tsx
+++ src/components/ResponsiveDrawer.tsx
@@ -1,4 +1,13 @@
-import { Breakpoint, Drawer, DrawerProps, GlobalStyles, SxProps, Theme } from '@mui/material';
+import {
+    Breakpoint,
+    Drawer,
+    DrawerProps,
+    GlobalStyles,
+    SxProps,
+    Theme,
+    useMediaQuery,
+    useTheme,
+} from '@mui/material';
 import { ReactNode } from 'react';
 
 import { DESKTOP_LAYOUT_BREAKPOINT } from '../constants';
@@ -38,54 +47,60 @@ export const ResponsiveDrawer = ({
     anchor = 'left',
     desktopDrawerVariant = 'permanent',
 }: ResponsiveDrawerProps): JSX.Element => {
-    const isPersistantDrawerClosed = !open && desktopDrawerVariant === 'persistent';
+    const isPersistentDrawerClosed = !open && desktopDrawerVariant === 'persistent';
+    const theme = useTheme();
+    const isDesktopOrUp = useMediaQuery(theme.breakpoints.up(DESKTOP_LAYOUT_BREAKPOINT));
 
     return (
         <>
-            <Drawer
-                variant="temporary"
-                anchor={anchor}
-                open={open}
-                onClose={onClose}
-                disableScrollLock={false}
-                PaperProps={{
-                    sx: {
-                        // This is intentionally not following the breakpoint. It looks nicer this way
-                        width: { xs: '100vw', sm: '40vw' },
-                    },
-                }}
-                sx={{
-                    display: { xs: 'flex', [drawerBreakpoint]: 'none' },
-                    ...mobileDrawerSx,
-                }}>
-                {mobileHeading ?? heading}
-                {children}
-            </Drawer>
-            <Drawer
-                variant={desktopDrawerVariant}
-                open={open}
-                anchor={anchor}
-                onClose={onClose}
-                sx={{
-                    width: 'auto',
-                    display: { xs: 'none', [drawerBreakpoint]: 'flex' },
-                    overflow: isPersistantDrawerClosed ? 'hidden' : 'visible',
+            {isDesktopOrUp ? (
+                <Drawer
+                    variant={desktopDrawerVariant}
+                    open={open}
+                    anchor={anchor}
+                    onClose={onClose}
+                    sx={{
+                        width: 'auto',
+                        display: { xs: 'none', [drawerBreakpoint]: 'flex' },
+                        overflow: isPersistentDrawerClosed ? 'hidden' : 'visible',
 
-                    ...desktopDrawerSx,
-                }}
-                PaperProps={{
-                    elevation: 2,
-                    sx: {
-                        maxWidth: (theme) => theme.spacing(50),
-                        position: 'unset',
-                        backgroundColor: (theme) => theme.palette.background.default,
-                        borderRight: 'none',
-                    },
-                }}>
-                {heading}
-                {children}
-            </Drawer>
-            {isPersistantDrawerClosed && <GlobalStyle />}
+                        ...desktopDrawerSx,
+                    }}
+                    PaperProps={{
+                        elevation: 2,
+                        sx: {
+                            maxWidth: (theme) => theme.spacing(50),
+                            position: 'unset',
+                            backgroundColor: (theme) => theme.palette.background.default,
+                            borderRight: 'none',
+                        },
+                    }}>
+                    {heading}
+                    {children}
+                </Drawer>
+            ) : (
+                <Drawer
+                    variant="temporary"
+                    anchor={anchor}
+                    open={open}
+                    onClose={onClose}
+                    disableScrollLock={false}
+                    PaperProps={{
+                        sx: {
+                            // This is intentionally not following the breakpoint. It looks nicer this way
+                            width: { xs: '100vw', sm: '40vw' },
+                        },
+                    }}
+                    sx={{
+                        display: { xs: 'flex', [drawerBreakpoint]: 'none' },
+                        ...mobileDrawerSx,
+                    }}>
+                    {mobileHeading ?? heading}
+                    {children}
+                </Drawer>
+            )}
+
+            {isPersistentDrawerClosed && <GlobalStyle />}
         </>
     );
 };
