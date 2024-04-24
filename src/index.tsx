5d0a5f1d4af906c6ee3954324f4b0fc69731f99f
Taylor Blanton <matthewtblanton@gmail.com>
2024-04-24 16:53:20 -0500

    pull router and varnishedapp out of index so we can test with them

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,9 @@
-import { VarnishApp } from '@allenai/varnish2/components';
-import { getTheme } from '@allenai/varnish2/theme';
-import { getRouterOverriddenTheme } from '@allenai/varnish2/utils';
-import { LinearProgress, ThemeOptions } from '@mui/material';
-import { PropsWithChildren } from 'react';
+import { LinearProgress } from '@mui/material';
 import { createRoot } from 'react-dom/client';
-import { Link, Navigate, RouteObject, RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { ThemeProvider, createGlobalStyle } from 'styled-components';
+import { RouterProvider } from 'react-router-dom';
 
-import { App } from './App';
-import { FeatureToggleProvider } from './FeatureToggleContext';
-import { ScrollToTopOnPageChange } from './components/ScrollToTopOnPageChange';
-import { olmoTheme, uiRefreshOlmoTheme } from './olmoTheme';
-import { Home } from './pages/Home';
-import { Admin } from './pages/Admin';
-import { DolmaExplorer } from './pages/DolmaExplorer';
-import { ErrorPage } from './pages/ErrorPage';
-import { NotFound } from './pages/NotFound';
-import { PromptTemplates } from './pages/PromptTemplates';
-import { Search, searchPageLoader } from './pages/Search';
-import { Thread } from './pages/Thread';
-import { Document } from './pages/Document';
-import { NewApp } from './components/NewApp';
-import { UIRefreshThreadPage } from './pages/UIRefreshThreadPage';
-import { ThreadDisplay, selectedThreadLoader } from './components/thread/ThreadDisplay';
-import { links } from './Links';
-
-const GlobalStyle = createGlobalStyle`
-    html {
-        background: transparent;
-
-        // force chip selector to be on top
-        #typeahead-menu {
-            z-index: 999;
-        }
-    }
-`;
+import { router } from './router';
+import { VarnishedApp } from './VarnishedApp';
 
 const enableMocking = async () => {
     if (process.env.NODE_ENV !== 'development' || process.env.ENABLE_MOCKING !== 'true') {
@@ -46,171 +15,11 @@ const enableMocking = async () => {
     return worker.start();
 };
 
-interface VarnishedAppProps extends PropsWithChildren {
-    theme?: ThemeOptions;
-}
-
-const VarnishedApp = ({ children, theme = olmoTheme }: VarnishedAppProps) => {
-    const combinedTheme = getTheme(getRouterOverriddenTheme(Link, theme));
-
-    return (
-        <FeatureToggleProvider>
-            <ScrollToTopOnPageChange />
-            <ThemeProvider theme={combinedTheme}>
-                <VarnishApp layout="left-aligned" theme={combinedTheme}>
-                    <GlobalStyle />
-                    {children}
-                </VarnishApp>
-            </ThemeProvider>
-        </FeatureToggleProvider>
-    );
-};
-
-const routes = [
-    {
-        path: '/',
-        element: (
-            <VarnishedApp>
-                <App />
-            </VarnishedApp>
-        ),
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: '/',
-                element: <Home />,
-            },
-            {
-                path: '/document/:id',
-                element: <Document />,
-                errorElement: <ErrorPage />,
-            },
-            {
-                path: '/dolma',
-                element: <DolmaExplorer />,
-                errorElement: <ErrorPage />,
-            },
-            {
-                path: '/search',
-                element: <Search />,
-                errorElement: <ErrorPage />,
-                loader: searchPageLoader,
-            },
-            {
-                path: '/thread/:id',
-                element: <Thread />,
-            },
-            {
-                path: '/prompttemplates',
-                element: <PromptTemplates />,
-            },
-            {
-                path: '/prompt-templates',
-                element: <PromptTemplates />,
-            },
-            {
-                path: '/admin',
-                element: <Admin />,
-            },
-            {
-                path: '/*',
-                element: (
-                    <VarnishedApp>
-                        <NotFound />
-                    </VarnishedApp>
-                ),
-            },
-        ],
-    },
-];
-
-const uiRefreshRoutes: RouteObject[] = [
-    {
-        path: '/',
-        element: (
-            <VarnishedApp theme={uiRefreshOlmoTheme}>
-                <NewApp />
-            </VarnishedApp>
-        ),
-        errorElement: <ErrorPage />,
-        children: [
-            {
-                path: links.playground,
-                element: <UIRefreshThreadPage />,
-                children: [
-                    {
-                        path: '/thread',
-                        // We don't have anything at /thread but it would make sense for it to exist since we have things at /thread/:id
-                        // We just redirect to the playground to make sure people going to /thread get what they want
-                        element: <Navigate to={links.playground} />,
-                    },
-                    {
-                        path: links.thread(':id'),
-                        element: <ThreadDisplay />,
-                        handle: {
-                            title: 'Playground',
-                        },
-                        loader: selectedThreadLoader,
-                    },
-                ],
-                handle: {
-                    title: 'Playground',
-                },
-            },
-            {
-                path: links.document(':id'),
-                element: <Document />,
-                handle: {
-                    title: 'Dataset Explorer',
-                },
-            },
-            {
-                path: links.datasetExplorer,
-                element: <DolmaExplorer />,
-                handle: {
-                    title: 'Dataset Explorer',
-                },
-            },
-            {
-                path: links.search,
-                element: <Search />,
-                handle: {
-                    title: 'Dataset Explorer',
-                },
-                loader: searchPageLoader,
-            },
-            {
-                path: links.promptTemplates,
-                element: <PromptTemplates />,
-                handle: {
-                    title: 'Prompt Templates',
-                },
-            },
-            {
-                path: '/*',
-                element: (
-                    <VarnishedApp>
-                        <NotFound />
-                    </VarnishedApp>
-                ),
-                handle: {
-                    title: '',
-                },
-            },
-        ],
-    },
-];
-
-const searchParams = new URL(window.location.href).searchParams;
-const isUIRefreshEnabled =
-    searchParams.get('isUIRefreshEnabled') === 'true' ||
-    process.env.IS_UI_REFRESH_ENABLED === 'true';
-const router = createBrowserRouter(isUIRefreshEnabled ? uiRefreshRoutes : routes);
-
 const container = document.getElementById('root');
 if (!container) {
     throw new Error("No element with an id of 'root' was found.");
 }
+
 const root = createRoot(container);
 enableMocking().then(() => {
     root.render(
