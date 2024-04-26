fde4ada868ece1038ef19a021fa4b04e78eec3c5
huytr1995 <huyt@allenai.org>
2024-04-26 11:51:28 -0700

    Add Dolma E2E test

--- e2e/olmo.spec.ts
+++ e2e/olmo.spec.ts
@@ -65,3 +65,25 @@ test('can send prompt in Olmo Playground', async ({ page }) => {
     await expect(page.getByText('This is the first response.')).toBeVisible();
     await expect(page.getByText('This is the second response.')).toBeVisible();
 });
+
+test('can search pretraining documents in DataSet Explorer', async ({ page }) => {
+    await page.goto('/?isUIRefreshEnabled=true');
+    await page.waitForLoadState('networkidle');
+    await page.getByTestId('MenuIcon').click();
+
+    await page.getByRole('link', { name: 'Dataset Explorer' }).click();
+    await page.waitForLoadState('networkidle');
+    await page.getByLabel('Close navigation drawer').click();
+
+    await page.getByLabel('Search Term').focus();
+    await page.getByLabel('Search Term').fill('Seattle');
+    await page.getByRole('button', { name: 'Submit' }).click();
+    await page.waitForLoadState('networkidle');
+
+    await expect(
+        page.getByRole('link', {
+            name: 'Gnishimura/We_Eat: data/restaurant_aliases.txt (bff1b112a1b0f6cb411e8b9f43792cc42412765b)',
+        })
+    ).toBeVisible();
+    await expect(page.getByText('About These Results')).toBeVisible();
+});
