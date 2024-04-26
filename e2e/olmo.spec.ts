fc4b7019ac7fb961d77b7f2a7eb2cb1d226756d4
huytr1995 <huyt@allenai.org>
2024-04-26 13:51:46 -0700

    Change test

--- e2e/olmo.spec.ts
+++ e2e/olmo.spec.ts
@@ -80,10 +80,5 @@ test('can search pretraining documents in DataSet Explorer', async ({ page }) =>
     await page.getByRole('button', { name: 'Submit' }).click();
     await page.waitForLoadState('networkidle');
 
-    await expect(
-        page.getByLabel(
-            'Gnishimura/We_Eat: data/restaurant_aliases.txt (bff1b112a1b0f6cb411e8b9f43792cc42412765b)'
-        )
-    ).toBeVisible();
-    await expect(page.getByText('About These Results')).toBeVisible();
+    await expect(page.getByText('b1277a5bbd120574d3abea306025f274da483a47')).toBeVisible();
 });
