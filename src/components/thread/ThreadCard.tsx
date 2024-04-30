46b7a5ff297b3a37edebc2e925f1cb3ea62ef219
Taylor Blanton <matthewtblanton@gmail.com>
2024-04-30 17:30:56 -0500

    make the main container query break on md, tinker with ThreadCardStyles

--- src/components/thread/ThreadCard.tsx
+++ src/components/thread/ThreadCard.tsx
@@ -1,27 +1,28 @@
 import { Card, Stack } from '@mui/material';
 import { PropsWithChildren } from 'react';
 
-import { biggerContainerQuery, smallerContainerQuery } from '@/utils/container-query-utils';
+import { DESKTOP_LAYOUT_BREAKPOINT } from '@/constants';
 import { useDesktopOrUp } from '../dolma/shared';
 
 interface ThreadPageCardProps extends PropsWithChildren {}
 
 export const ThreadCard = ({ children }: ThreadPageCardProps): JSX.Element => {
+    const isDesktopOrUp = useDesktopOrUp();
+
     return (
         <Card
-            variant={useDesktopOrUp() ? 'elevation' : 'outlined'}
+            variant={isDesktopOrUp ? 'elevation' : 'outlined'}
             sx={(theme) => ({
-                padding: 0,
-                [biggerContainerQuery(theme)]: {
+                // This component uses screen size rather than container queries
+                // We want it to always be a card on desktop and always be flat on mobile
+                [theme.breakpoints.up(DESKTOP_LAYOUT_BREAKPOINT)]: {
                     padding: 2,
                 },
 
                 backgroundColor: (theme) => theme.palette.background.default,
 
+                // This removes the border from the outlined version of the component. Doesn't affect the elevated version.
                 border: 0,
-                [smallerContainerQuery(theme)]: {
-                    borderRadius: 0,
-                },
             })}>
             <Stack
                 component={Stack}
