c8398dd0c8e271dabca6789796526be804785820
huytr1995 <huyt@allenai.org>
2024-05-07 11:50:47 -0700

    Remove unused import

--- src/utils/useClosingDrawerOnNavigation-utils.ts
+++ src/utils/useClosingDrawerOnNavigation-utils.ts
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { useLocation, useNavigation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 import { links } from '@/Links';
 
