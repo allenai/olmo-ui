79e185f9251b891a8e76edb60453356d108842c7
Huy Tran <84343285+huytr1995@users.noreply.github.com>
2024-05-09 10:51:41 -0700

    Add Alt for Robot Avatar (#394)
    ## Description
    
    closes:
    https://github.com/orgs/allenai/projects/149/views/1?pane=issue&itemId=61822076
    Add Alt text for Robot Icon
    
    ## Testing Plan
    Verify UI works if CI passes we are good.
    
    ## Output / Screenshots
    
    
    ![image](https://github.com/allenai/olmo-ui/assets/84343285/97d1e923-89c1-4a1e-a6b7-de1cacd60ec2)
    
    ### A11y
    
    N/A

--- src/components/avatars/RobotAvatar.tsx
+++ src/components/avatars/RobotAvatar.tsx
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import robotAvatarURL from '../assets/robot.svg';
 
 export const RobotAvatar = () => {
-    return <RobotStyledAvatar src={robotAvatarURL} />;
+    return <RobotStyledAvatar src={robotAvatarURL} alt="A blue robot icon representing the LLM" />;
 };
 
 const RobotStyledAvatar = styled(Avatar)`
