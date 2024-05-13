e0d8b9c5974d634ebf2398192f293bab93a7f5e4
Taylor Blanton <matthewtblanton@gmail.com>
2024-05-13 17:35:16 -0500

    add the faq content

--- src/pages/FAQsPage.tsx
+++ src/pages/FAQsPage.tsx
@@ -1,3 +1,4 @@
+import { faqs } from '@/assets/faq-list';
 import { PageContentWrapper } from '@/components/dolma/PageContentWrapper';
 import { FAQ } from '@/components/faq/FAQ';
 import { FAQCategory } from '@/components/faq/FAQCategory';
@@ -5,16 +6,15 @@ import { FAQCategory } from '@/components/faq/FAQCategory';
 export const FAQsPage = () => {
     return (
         <PageContentWrapper>
-            <FAQCategory categoryName="Example">
-                <FAQ summary="How should an FAQ look?">This is an example FAQ</FAQ>
-                <FAQ summary="What about a second FAQ...">Example 2</FAQ>
-            </FAQCategory>
-            <FAQCategory categoryName="Example2">
-                <FAQ summary="How should an FAQ look in the second category?">
-                    This is an example FAQ
-                </FAQ>
-                <FAQ summary="What about a second FAQ in the second category...">Example 2</FAQ>
-            </FAQCategory>
+            {faqs.map((faqCategory) => (
+                <FAQCategory categoryName={faqCategory.category} key={faqCategory.category}>
+                    {faqCategory.questions.map((question) => (
+                        <FAQ summary={question.summary} key={question.summary}>
+                            {question.content}
+                        </FAQ>
+                    ))}
+                </FAQCategory>
+            ))}
         </PageContentWrapper>
     );
 };
