74290519196b29050dcb79dd2b0a8f6fd22d285c
Taylor Blanton <matthewtblanton@gmail.com>
2024-05-14 13:13:18 -0500

    initial search setup done

--- src/pages/FAQsPage.tsx
+++ src/pages/FAQsPage.tsx
@@ -1,10 +1,13 @@
-import { Link, Typography } from '@mui/material';
+import { Button, Link, Stack, TextField, Typography } from '@mui/material';
+import { useMemo } from 'react';
 import Markdown from 'react-markdown';
+import { Form, useActionData, useNavigation, useSearchParams } from 'react-router-dom';
 
 import { faqs } from '@/assets/faq-list';
 import { PageContentWrapper } from '@/components/dolma/PageContentWrapper';
 import { FAQ } from '@/components/faq/FAQ';
 import { FAQCategory } from '@/components/faq/FAQCategory';
+import { links } from '@/Links';
 
 const FAQMarkdown = ({ children }: { children: string }) => {
     return (
@@ -19,10 +22,42 @@ const FAQMarkdown = ({ children }: { children: string }) => {
     );
 };
 
+const SEARCH_FIELD_NAME = 'search';
+
 export const FAQsPage = () => {
+    const [searchParams] = useSearchParams();
+    const search = searchParams.get(SEARCH_FIELD_NAME);
+
+    const filteredFAQs = useMemo(() => {
+        if (search == null) {
+            return faqs;
+        }
+
+        return faqs.map((category) => ({
+            category: category.category,
+            questions: category.questions.filter(
+                (question) => question.answer.includes(search) || question.question.includes(search)
+            ),
+        }));
+    }, [search]);
+
     return (
         <PageContentWrapper>
-            {faqs.map((faqCategory) => (
+            <Form action={links.faqs}>
+                <Stack direction="row" gap={2} marginBlockEnd={3}>
+                    <TextField
+                        type="search"
+                        label="Search FAQs"
+                        name={SEARCH_FIELD_NAME}
+                        fullWidth
+                        defaultValue={search}
+                    />
+                    <Button type="submit" variant="contained">
+                        Search
+                    </Button>
+                </Stack>
+            </Form>
+            {filteredFAQs.map((faqCategory) => (
                 <FAQCategory categoryName={faqCategory.category} key={faqCategory.category}>
                     {faqCategory.questions.map((question) => (
                         <FAQ question={question.question} key={question.question}>
