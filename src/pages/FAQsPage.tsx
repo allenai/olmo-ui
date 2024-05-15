e9cc444430b704aa4269f22986f9465bad883fb6
huytr1995 <huyt@allenai.org>
2024-05-15 16:03:09 -0700

    Update

--- src/pages/FAQsPage.tsx
+++ src/pages/FAQsPage.tsx
@@ -1,17 +1,20 @@
-import { Button, Grid, Link, Stack, TextField, Typography } from '@mui/material';
+import { Button, Grid, Stack, TextField } from '@mui/material';
 import { useEffect, useMemo } from 'react';
-import Markdown from 'react-markdown';
 import { Form, useSearchParams } from 'react-router-dom';
 
 import { faqs } from '@/assets/faq-list';
 import { PageContentWrapper } from '@/components/dolma/PageContentWrapper';
+import { useDesktopOrUp } from '@/components/dolma/shared';
+import { FAQButton } from '@/components/faq/FAQButton';
 import { FAQCategory } from '@/components/faq/FAQCategory';
+import { FAQCategoryLinks } from '@/components/faq/FAQCategoryLinks';
 import { NoResults } from '@/components/NoResults';
 
 const SEARCH_FIELD_NAME = 'search';
 
 export const FAQsPage = (): JSX.Element => {
     const [searchParams, setSearchParams] = useSearchParams();
+    const isDesktop = useDesktopOrUp();
     const search = searchParams.get(SEARCH_FIELD_NAME);
 
     useEffect(() => {
@@ -44,33 +47,41 @@ export const FAQsPage = (): JSX.Element => {
     );
 
     return (
-        <PageContentWrapper>
-            <Form>
-                <Stack direction="row" gap={2} marginBlockEnd={3}>
-                    <TextField
-                        type="search"
-                        label="Search FAQs"
-                        name={SEARCH_FIELD_NAME}
-                        fullWidth
-                        defaultValue={search}
-                        size="small"
-                    />
-                    <Button type="submit" variant="contained">
-                        Search
-                    </Button>
-                </Stack>
-            </Form>
-            {hasNoQuestionsToDisplay && search != null ? (
-                <NoResults request={search} resultsType="FAQ" />
-            ) : (
-                filteredFAQs.map((faqCategory) => (
-                    <FAQCategory
-                        categoryName={faqCategory.category}
-                        questions={faqCategory.questions}
-                        key={faqCategory.category}
-                    />
-                ))
-            )}
-        </PageContentWrapper>
+        <Grid container spacing={2}>
+            <Grid item xs={10}>
+                <PageContentWrapper>
+                    <Form>
+                        <Stack direction="row" gap={2} marginBlockEnd={3}>
+                            <TextField
+                                type="search"
+                                label="Search FAQs"
+                                name={SEARCH_FIELD_NAME}
+                                fullWidth
+                                defaultValue={search}
+                                size="small"
+                            />
+                            <Button type="submit" variant="contained">
+                                Search
+                            </Button>
+                        </Stack>
+                    </Form>
+                    {!isDesktop && <FAQButton />}
+                    {hasNoQuestionsToDisplay && search != null ? (
+                        <NoResults request={search} resultsType="FAQ" />
+                    ) : (
+                        filteredFAQs.map((faqCategory) => (
+                            <FAQCategory
+                                categoryName={faqCategory.category}
+                                questions={faqCategory.questions}
+                                key={faqCategory.category}
+                            />
+                        ))
+                    )}
+                </PageContentWrapper>
+            </Grid>
+            <Grid item xs={2}>
+                {isDesktop && <FAQCategoryLinks />}
+            </Grid>
+        </Grid>
     );
 };
