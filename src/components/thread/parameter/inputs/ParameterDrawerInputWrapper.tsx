a4ac9ca1c7980ae13acb330a9446fcc8c8505137
Taylor Blanton <matthewtblanton@gmail.com>
2024-05-15 10:54:21 -0500

    put the info button inside the standardized parameters input

--- src/components/thread/parameter/inputs/ParameterDrawerInputWrapper.tsx
+++ src/components/thread/parameter/inputs/ParameterDrawerInputWrapper.tsx
@@ -1,5 +1,8 @@
-import { Box, Typography } from '@mui/material';
-import { ComponentProps, ReactNode, useRef } from 'react';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import { Box, IconButton, Typography } from '@mui/material';
+import { ComponentProps, ReactNode, useRef, useState } from 'react';
+
+import { ResponsiveTooltip } from '@/components/thread/ResponsiveTooltip';
 
 import { ParameterInfoButton } from './ParameterInfoButton';
 
@@ -27,6 +30,16 @@ export const ParameterDrawerInputWrapper = ({
 
     const shouldShowInfoButton = tooltipTitle != null && tooltipContent != null;
 
+    const [isTooltipOpen, setIsTooltipOpen] = useState(false);
+
+    const toggleTooltipOpen = () => {
+        setIsTooltipOpen(!isTooltipOpen);
+    };
+
+    const handleTooltipClose = () => {
+        setIsTooltipOpen(false);
+    };
+
     return (
         <Box
             // MUI's Grid component had some weird stuff going on with top padding so we're using CSS grid here instead
@@ -44,12 +57,30 @@ export const ParameterDrawerInputWrapper = ({
                 {label}
             </Typography>
             {shouldShowInfoButton && (
-                <ParameterInfoButton
-                    anchorElement={containerRef.current}
-                    tooltipTitle={tooltipTitle}
-                    tooltipContent={tooltipContent}
-                    tooltipIdSuffix={`${inputId}-description`}
-                />
+                <ResponsiveTooltip
+                    anchorEl={containerRef.current}
+                    dialogTitle={tooltipTitle}
+                    dialogContent={tooltipContent}
+                    isTooltipOpen={isTooltipOpen}
+                    onTooltipClose={handleTooltipClose}
+                    tooltipIdSuffix={`${inputId}-description`}>
+                    <IconButton
+                        tabIndex={0}
+                        aria-expanded={isTooltipOpen}
+                        sx={{ color: 'inherit', justifySelf: 'start' }}
+                        onClick={toggleTooltipOpen}
+                        onKeyDown={(event) => {
+                            // This is currently used in the parameters drawer that has ESC handling as well
+                            // we need to stop propagation so the drawer doesn't close when we  just want to close the tooltip
+                            event.stopPropagation();
+
+                            if (event.key === 'Escape') {
+                                handleTooltipClose();
+                            }
+                        }}>
+                        <InfoOutlinedIcon />
+                    </IconButton>
+                </ResponsiveTooltip>
             )}
             <Box sx={{ gridArea: 'input' }}>
                 {children instanceof Function ? children({ inputLabelId }) : children}
