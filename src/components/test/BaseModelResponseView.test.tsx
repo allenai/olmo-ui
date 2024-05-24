0a3ab560b378b81ba4aaea498cda0248b147adaa
Huy Tran <84343285+huytr1995@users.noreply.github.com>
2024-05-24 10:58:48 -0700

    Remove old Olmo UI (#440)
    ## Description
    
    Closes: https://github.com/allenai/olmo-ui/issues/437
    
    ## Reviewer Instructions
    
    I tried to find all the instances & components associated with old Olmo
    including test. Please let me know if i miss any of them
    
    ## Testing Plan
    
    Verify we cant see the old Olmo any more. Also Olmo Playground still
    function. If CI passes we are good.
    
    ## Output / Screenshots
    
    
    
    
    https://github.com/allenai/olmo-ui/assets/84343285/ecf1240d-43f4-43b2-9e1a-9188bf07d9d1
    
    
    
    ### A11y
    
    N/A

--- src/components/test/BaseModelResponseView.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@test-utils';
-
-import { BaseModelResponseView } from '../ResponseView/BaseModelResponseView';
-
-test('LLMResponseView', () => {
-    render(<BaseModelResponseView response="This is a response" msgId="testId" />);
-    expect(screen.getByText('This is a response')).toBeVisible();
-});
--- src/components/test/UserResponseView.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@test-utils';
-
-import { UserResponseView } from '../ResponseView/UserResponseView';
-
-test('UserResponseView', () => {
-    render(<UserResponseView response="Hello! How are you?" msgId="testId" />);
-    expect(screen.getByText('Hello! How are you?')).toBeVisible();
-});
