12cb6a538038e698dadb0d438ef2ed72ca470b73
huytr1995 <huyt@allenai.org>
2024-06-06 14:44:41 -0700

    Separate codeblock out

---
+++ src/components/thread/CodeBlock.tsx
@@ -0,0 +1,22 @@
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+interface CodeBlockProps {
+    inline?: boolean;
+    className?: string;
+    children?: React.ReactNode;
+}
+
+export const CodeBlock = ({ inline, className, children, ...props }: CodeBlockProps) => {
+    const match = /language-(\w+)/.exec(className || '');
+
+    return !inline && match ? (
+        <SyntaxHighlighter style={dracula} PreTag="div" language={match[1]} {...props}>
+            {String(children).replace(/\n$/, '')}
+        </SyntaxHighlighter>
+    ) : (
+        <code className={className} {...props}>
+            {children}
+        </code>
+    );
+};
--- src/components/thread/MarkDownWithSyntax.tsx
+++ src/components/thread/MarkDownWithSyntax.tsx
@@ -1,8 +1,8 @@
 import Markdown from 'react-markdown';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import rehypeRaw from 'rehype-raw';
 import remarkGfm from 'remark-gfm';
+
+import { CodeBlock } from './CodeBlock';
 
 interface MarkdownRendererProps {
     children: string;
@@ -14,25 +14,7 @@ export function MarkdownRenderer({ children: markdown }: MarkdownRendererProps) 
             remarkPlugins={[remarkGfm]}
             rehypePlugins={[rehypeRaw]}
             components={{
-                // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                code({ node, inline, className, children, ...props }: any) {
-                    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-                    const match = /language-(\w+)/.exec(className || '');
-
-                    return !inline && match ? (
-                        <SyntaxHighlighter
-                            style={dracula}
-                            PreTag="div"
-                            language={match[1]}
-                            {...props}>
-                            {String(children).replace(/\n$/, '')}
-                        </SyntaxHighlighter>
-                    ) : (
-                        <code className={className} {...props}>
-                            {children}
-                        </code>
-                    );
-                },
+                code: CodeBlock,
             }}>
             {markdown}
         </Markdown>
