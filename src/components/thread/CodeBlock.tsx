8383b00e73c06f1a6911aafd8d7ab64f722ddb27
huytr1995 <huyt@allenai.org>
2024-06-06 14:59:09 -0700

    Change to PrsimAsyncLight to help with bundle size

--- src/components/thread/CodeBlock.tsx
+++ src/components/thread/CodeBlock.tsx
@@ -1,4 +1,4 @@
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 interface CodeBlockProps {
