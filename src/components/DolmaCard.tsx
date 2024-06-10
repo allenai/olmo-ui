d0f47f4bfded928e34c8129483d18e5b21e902b2
huytr1995 <huyt@allenai.org>
2024-06-10 09:16:10 -0700

    Address PR comments

--- src/components/DolmaCard.tsx
+++ src/components/DolmaCard.tsx
@@ -4,6 +4,11 @@ import { links } from '@/Links';
 
 import { useDesktopOrUp } from './dolma/shared';
 
+const DOLMA_TITLE = 'Dolma is proven, trusted, and fully open.';
+
+const DOLMA_CONTENT =
+    'Dolma 1.7-7B is a dataset of 2.05 trillion tokens from a diverse mix of web content, academic publications, code, books, and encyclopedic materials. It is openly available for download on';
+
 export const DolmaCard = () => {
     const isDesktop = useDesktopOrUp();
 
@@ -12,18 +17,15 @@ export const DolmaCard = () => {
             {isDesktop ? (
                 <Card
                     sx={{
-                        padding: '32px, 16px, 32px, 16px',
+                        padding: (theme) => theme.spacing(4, 2, 4, 2),
                         marginBottom: (theme) => theme.spacing(2),
                     }}>
                     <CardContent>
                         <Typography variant="h1" align="center">
-                            Dolma is proven, trusted, and fully open.
+                            {DOLMA_TITLE}
                         </Typography>
                         <Typography variant="h5" align="center">
-                            Dolma 1.7-7B is a dataset of 2.05 trillion tokens from a diverse mix of
-                            web content, academic publications, code, books, and encyclopedic
-                            materials. It is openly available for download on{' '}
-                            <Link href={links.ourDatasets}>Hugging Face </Link>under the ODC-By
+                            {DOLMA_CONTENT} <Link href={links.ourDatasets}>Hugging Face </Link>under
                             license.
                         </Typography>
                     </CardContent>
@@ -31,17 +33,15 @@ export const DolmaCard = () => {
             ) : (
                 <Box
                     sx={{
-                        padding: '32px, 16px, 32px, 16px',
+                        padding: (theme) => theme.spacing(4, 2, 4, 2),
                         marginBottom: (theme) => theme.spacing(2),
                     }}>
                     <Typography variant="h3" align="left">
-                        Dolma is proven, trusted, and fully open.
+                        {DOLMA_TITLE}
                     </Typography>
                     <Typography variant="body1" align="left">
-                        Dolma 1.7-7B is a dataset of 2.05 trillion tokens from a diverse mix of web
-                        content, academic publications, code, books, and encyclopedic materials. It
-                        is openly available for download on{' '}
-                        <Link href={links.ourDatasets}>Hugging Face </Link>under the ODC-By license.
+                        {DOLMA_CONTENT} <Link href={links.ourDatasets}>Hugging Face </Link>under
+                        license.
                     </Typography>
                 </Box>
             )}
