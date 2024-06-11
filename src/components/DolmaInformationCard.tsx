3debc212dbe810a31824380b2ee7c7fdbaa895da
huytr1995 <huyt@allenai.org>
2024-06-11 11:28:53 -0700

    WIP

---
+++ src/components/DolmaInformationCard.tsx
@@ -0,0 +1,75 @@
+import { Button, Card, CardActions, CardContent, Link, Typography } from '@mui/material';
+
+import { SMALL_LAYOUT_BREAKPOINT } from '@/constants';
+
+import { useDesktopOrUp } from './dolma/shared';
+
+interface DolmaInformationCardProps {
+    linkText: string;
+    title: string;
+    buttonText: string;
+}
+
+export const DolmaInformationCard = ({
+    linkText,
+    title,
+    buttonText,
+}: DolmaInformationCardProps) => {
+    const isDesktopOrUp = useDesktopOrUp();
+
+    return (
+        <Card
+            sx={(theme) => ({
+                background: (theme) => theme.palette.primary.dark,
+                padding: (theme) => theme.spacing(4),
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                textAlign: 'left',
+                flex: 1,
+                justifyContent: 'space-between',
+                [theme.breakpoints.down(SMALL_LAYOUT_BREAKPOINT)]: {
+                    padding: 3,
+                    borderRadius: '0px',
+                    border: '0px',
+                },
+            })}
+            variant={isDesktopOrUp ? 'elevation' : 'outlined'}>
+            <CardContent
+                sx={{
+                    flex: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    alignItems: 'flex-start',
+                    padding: 0,
+                }}>
+                <CardActions
+                    sx={{
+                        padding: 0,
+                    }}>
+                    <Link href="#" sx={{ color: (theme) => theme.palette.primary.light }}>
+                        {linkText}
+                    </Link>
+                </CardActions>
+                <Typography
+                    variant="h4"
+                    component="div"
+                    sx={{
+                        color: (theme) => theme.palette.primary.contrastText,
+                        marginY: 0,
+                    }}>
+                    {title}
+                </Typography>
+                <CardActions
+                    sx={{
+                        padding: 0,
+                    }}>
+                    <Button size="small" variant="contained">
+                        {buttonText}
+                    </Button>
+                </CardActions>
+            </CardContent>
+        </Card>
+    );
+};
--- src/pages/DolmaExplorer.tsx
+++ src/pages/DolmaExplorer.tsx
@@ -1,16 +1,52 @@
-import { Stack } from '@mui/material';
+import { Grid, Stack } from '@mui/material';
 
+import { useDesktopOrUp } from '@/components/dolma/shared';
 import { DolmaCard } from '@/components/DolmaCard';
+import { DolmaInformationCard } from '@/components/DolmaInformationCard';
 
 import { NewSearchPlaceholder } from '../components/dolma/NewSearchPlaceholder';
 import { SearchForm } from '../components/dolma/SearchForm';
 
-export const DolmaExplorer = () => (
-    <>
-        <Stack sx={{ textAlign: 'center' }} spacing={2}>
-            <DolmaCard />
-            <SearchForm />
-            <NewSearchPlaceholder />
-        </Stack>
-    </>
-);
+export const DolmaExplorer = () => {
+    const isDesktopOrUp = useDesktopOrUp();
+    return (
+        <>
+            <Stack sx={{ textAlign: 'center' }} spacing={2}>
+                <DolmaCard />
+                {isDesktopOrUp ? (
+                    <Grid container spacing={2} sx={{ display: 'flex' }}>
+                        <Grid item xs={6} sx={{ display: 'flex' }}>
+                            <DolmaInformationCard
+                                linkText="Learn More"
+                                title="How was Dolma Created?"
+                                buttonText="Read the Blog"
+                            />
+                        </Grid>
+                        <Grid item xs={6} sx={{ display: 'flex' }}>
+                            <DolmaInformationCard
+                                linkText="Research"
+                                title="Access the Dataset on Hugging Face"
+                                buttonText="Get Started"
+                            />
+                        </Grid>
+                    </Grid>
+                ) : (
+                    <Stack spacing={2}>
+                        <DolmaInformationCard
+                            linkText="Learn More"
+                            title="How was Dolma Created?"
+                            buttonText="Read the Blog"
+                        />
+                        <DolmaInformationCard
+                            linkText="Research"
+                            title="Access the Dataset on Hugging Face"
+                            buttonText="Get Started"
+                        />
+                    </Stack>
+                )}
+                <SearchForm />
+                <NewSearchPlaceholder />
+            </Stack>
+        </>
+    );
+};
