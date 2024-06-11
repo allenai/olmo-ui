0b0c62734e0672b9b0384fbd2dc68f898d5d1a70
huytr1995 <huyt@allenai.org>
2024-06-11 11:30:40 -0700

    Remove const

--- src/components/DolmaCard.tsx
+++ src/components/DolmaCard.tsx
@@ -4,21 +4,18 @@ import { links } from '@/Links';
 
 import { ResponsiveCard } from './ResponsiveCard';
 
-const DOLMA_TITLE = 'Dolma is proven, trusted, and fully open.';
-
-const DOLMA_CONTENT =
-    'Dolma 1.7-7B is a dataset of 2.05 trillion tokens from a diverse mix of web content, academic publications, code, books, and encyclopedic materials. It is openly available for download on';
-
 export const DolmaCard = () => {
     return (
         <ResponsiveCard>
             <CardContent sx={{ padding: (theme) => theme.spacing(4, 2) }}>
                 <Typography variant="h1" align="center">
-                    {DOLMA_TITLE}
+                    Dolma is proven, trusted, and fully open.
                 </Typography>
                 <Typography variant="h5" align="center">
-                    {DOLMA_CONTENT} <Link href={links.ourDatasets}>Hugging Face </Link>under
-                    license.
+                    Dolma 1.7-7B is a dataset of 2.05 trillion tokens from a diverse mix of web
+                    content, academic publications, code, books, and encyclopedic materials. It is
+                    openly available for download on{' '}
+                    <Link href={links.ourDatasets}>Hugging Face </Link>under license.
                 </Typography>
             </CardContent>
         </ResponsiveCard>
