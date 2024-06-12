5110366c95dc45ba3cfc87d283c31909b10eb54c
huytr1995 <huyt@allenai.org>
2024-06-12 09:31:47 -0700

    Revert change

--- src/pages/DolmaExplorer.tsx
+++ src/pages/DolmaExplorer.tsx
@@ -5,14 +5,12 @@ import { DolmaCard } from '@/components/DolmaCard';
 import { NewSearchPlaceholder } from '../components/dolma/NewSearchPlaceholder';
 import { SearchForm } from '../components/dolma/SearchForm';
 
-export const DolmaExplorer = () => {
-    return (
-        <>
-            <Stack sx={{ textAlign: 'center' }} spacing={2}>
-                <DolmaCard />
-                <SearchForm />
-                <NewSearchPlaceholder />
-            </Stack>
-        </>
-    );
-};
+export const DolmaExplorer = () => (
+    <>
+        <Stack sx={{ textAlign: 'center' }} spacing={2}>
+            <DolmaCard />
+            <SearchForm />
+            <NewSearchPlaceholder />
+        </Stack>
+    </>
+);
