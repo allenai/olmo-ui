8c14df17963770a31afb861128dc83610e3bf54c
Huy Tran <84343285+huytr1995@users.noreply.github.com>
2024-06-13 10:20:59 -0700

    Create blue informational cards for dataset explorer (#490)
    ## Description
    
    Closes: https://github.com/allenai/olmo-ui/issues/482
    Figma:
    Desktop:
    https://www.figma.com/design/HzdNG5hmhXtRYV5f7Ed7G3/AI2-Playground-(2024)?node-id=1439-8704&t=IGGiGOzLV5bROmFd-4
    Mobile:
    https://www.figma.com/design/HzdNG5hmhXtRYV5f7Ed7G3/AI2-Playground-(2024)?node-id=1439-7577&t=ZjxRMh7Z4qHLkC3J-0
    
    ## Reviewer Instructions
    
    I asked for the links in the Figma design but Cassidy is still waiting
    for the answer. I will be able to add it quickly once she has response.
    
    ## Testing Plan
    
    Verify UI matches with design of both Figma. If CI passes we are good.
    
    ## Output / Screenshots
    
    ### Desktop
    
    ![image](https://github.com/allenai/olmo-ui/assets/84343285/306d01dd-de02-4a97-a1ad-7d5a7630d97d)
    
    ### Mobile
    
    ![image](https://github.com/allenai/olmo-ui/assets/84343285/1b4418ab-8528-4582-82d8-ff9b7b23aaf6)
    
    
    ### A11y
    
    N/A

--- src/components/DolmaCard.tsx
+++ src/components/DolmaCard.tsx
@@ -1,23 +1,68 @@
-import { CardContent, Link, Typography } from '@mui/material';
+import { CardContent, Grid, Link, Stack, Typography } from '@mui/material';
 
 import { links } from '@/Links';
 
+import { useDesktopOrUp } from './dolma/shared';
+import { DolmaInformationCard } from './DolmaInformationCard';
 import { ResponsiveCard } from './ResponsiveCard';
 
 export const DolmaCard = () => {
+    const isDesktopOrUp = useDesktopOrUp();
+
     return (
-        <ResponsiveCard>
-            <CardContent sx={{ padding: (theme) => theme.spacing(4, 2) }}>
-                <Typography variant="h1" align="center">
-                    Dolma is proven, trusted, and fully open.
-                </Typography>
-                <Typography variant="h5" align="center">
-                    Dolma 1.7-7B is a dataset of 2.05 trillion tokens from a diverse mix of web
-                    content, academic publications, code, books, and encyclopedic materials. It is
-                    openly available for download on{' '}
-                    <Link href={links.ourDatasets}>Hugging Face </Link>under license.
-                </Typography>
-            </CardContent>
-        </ResponsiveCard>
+        <>
+            <ResponsiveCard>
+                <CardContent sx={{ padding: (theme) => theme.spacing(4, 2) }}>
+                    <Typography variant="h1" align="center">
+                        Dolma is proven, trusted, and fully open.
+                    </Typography>
+                    <Typography variant="h5" align="center">
+                        Dolma 1.7-7B is a dataset of 2.05 trillion tokens from a diverse mix of web
+                        content, academic publications, code, books, and encyclopedic materials. It
+                        is openly available for download on{' '}
+                        <Link href={links.ourDatasets}>Hugging Face </Link>under license.
+                    </Typography>
+                </CardContent>
+            </ResponsiveCard>
+            {isDesktopOrUp ? (
+                <Grid container spacing={2} sx={{ display: 'flex' }}>
+                    <Grid item xs={6} sx={{ display: 'flex' }}>
+                        <DolmaInformationCard
+                            linkText="Learn More"
+                            linkUrl="#"
+                            title="How was Dolma Created?"
+                            buttonText="Read the Blog"
+                            buttonUrl={links.ourDatasets}
+                        />
+                    </Grid>
+                    <Grid item xs={6} sx={{ display: 'flex' }}>
+                        <DolmaInformationCard
+                            linkText="Research"
+                            linkUrl="#"
+                            title="Access the Dataset on Hugging Face"
+                            buttonText="Get Started"
+                            buttonUrl={links.ourDatasets}
+                        />
+                    </Grid>
+                </Grid>
+            ) : (
+                <Stack spacing={2}>
+                    <DolmaInformationCard
+                        linkText="Learn More"
+                        linkUrl="#"
+                        title="How was Dolma Created?"
+                        buttonText="Read the Blog"
+                        buttonUrl={links.ourDatasets}
+                    />
+                    <DolmaInformationCard
+                        linkText="Research"
+                        linkUrl="#"
+                        title="Access the Dataset on Hugging Face"
+                        buttonText="Get Started"
+                        buttonUrl={links.ourDatasets}
+                    />
+                </Stack>
+            )}
+        </>
     );
 };
---
+++ src/components/DolmaInformationCard.tsx
@@ -0,0 +1,87 @@
+import { Button, Card, CardActions, CardContent, Link, Typography } from '@mui/material';
+
+import { SMALL_LAYOUT_BREAKPOINT } from '@/constants';
+
+import { useDesktopOrUp } from './dolma/shared';
+
+interface DolmaInformationCardProps {
+    linkText: string;
+    linkUrl: string;
+    title: string;
+    buttonText: string;
+    buttonUrl: string;
+}
+
+export const DolmaInformationCard = ({
+    linkText,
+    linkUrl,
+    title,
+    buttonText,
+    buttonUrl,
+}: DolmaInformationCardProps) => {
+    const isDesktopOrUp = useDesktopOrUp();
+
+    return (
+        <Card
+            sx={(theme) => ({
+                background: (theme) => theme.palette.primary.dark,
+                padding: (theme) => theme.spacing(4),
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'flex-start',
+                textAlign: 'left',
+                flex: 1,
+                justifyContent: 'space-between',
+                [theme.breakpoints.down(SMALL_LAYOUT_BREAKPOINT)]: {
+                    padding: 3,
+                    borderRadius: '0px',
+                },
+            })}
+            variant={isDesktopOrUp ? 'elevation' : 'outlined'}>
+            <CardContent
+                sx={{
+                    flex: 1,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    alignItems: 'flex-start',
+                    padding: 0,
+                }}>
+                <CardActions
+                    sx={{
+                        padding: 0,
+                    }}>
+                    <Link
+                        href={linkUrl}
+                        sx={{ color: (theme) => theme.palette.primary.light }}
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        {linkText}
+                    </Link>
+                </CardActions>
+                <Typography
+                    variant="h4"
+                    sx={{
+                        color: (theme) => theme.palette.primary.contrastText,
+                        marginY: 1,
+                    }}>
+                    {title}
+                </Typography>
+                <CardActions
+                    sx={{
+                        padding: 0,
+                    }}>
+                    <Button
+                        size="small"
+                        variant="contained"
+                        component={Link}
+                        href={buttonUrl}
+                        target="_blank"
+                        rel="noopener noreferrer">
+                        {buttonText}
+                    </Button>
+                </CardActions>
+            </CardContent>
+        </Card>
+    );
+};
