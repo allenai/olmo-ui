21f575e4b865fa67d0a463a85e3058ce0c5b5af5
huytr1995 <huyt@allenai.org>
2024-06-17 20:23:17 -0700

    Remove comment

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,5 @@
 export const dateTimeFormat = 'M/D/YY h:mm a';
 
-// formattingUtils.ts
 export const formatValueAsPercentage = (value: number, totalSum: number) => {
     const percentage = (value / totalSum) * 100;
     return `${percentage.toFixed(2)}%`;
