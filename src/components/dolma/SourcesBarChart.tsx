ff9d65aeea84c30d0b5735df14c5a2bffae9b8e7
huytr1995 <huyt@allenai.org>
2024-07-08 15:09:23 -0700

    Redesign bar chart

--- src/components/dolma/SourcesBarChart.tsx
+++ src/components/dolma/SourcesBarChart.tsx
@@ -1,9 +1,8 @@
-import { Box, LinearProgress, Typography } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-import { BarCustomLayerProps, ResponsiveBar } from '@nivo/bar';
+import { Grid, LinearProgress, Typography, useTheme } from '@mui/material';
+import Divider from '@mui/material/Divider';
+import { BarCustomLayerProps, ComputedDatum, ResponsiveBar } from '@nivo/bar';
 import { useLoaderData, useNavigation } from 'react-router-dom';
 
-import { DESKTOP_LAYOUT_BREAKPOINT, SMALL_LAYOUT_BREAKPOINT } from '@/constants';
 import { formatValueAsPercentage } from '@/util';
 
 import { ResponsiveCard } from '../ResponsiveCard';
@@ -17,37 +16,78 @@ export interface BarData {
     color: string;
 }
 
-const barChartAriaLabel =
-    'A bar chart with 7 bars shows the sources of data contained in Dolma. Common Crawl makes up the majority with 80.13% or x documents in the dataset. Reddit is 7.53% with x documents. C4 is 7.27% of the dataset with x documents. Stack Dedup is 4.21% with x documents. Semantic Scholar (pes2o) makes up 0.74% of the dataset with x documents. Wikipedia is 0.12% with x documents. Gutenberg makes up less than one hundredth of a percent of the overall dataset with x documents.';
-
 export const SourcesBarChart = () => {
     const sourcesData = (useLoaderData() as DolmaResponse).barData;
     const navigation = useNavigation();
-    const theme = useTheme(); // Access the Material-UI theme
+    const theme = useTheme();
 
     const isLoading = navigation.state === 'loading';
 
     // Calculate the total sum of all data values
     const totalSum = sourcesData.reduce((acc, item) => acc + item.value, 0);
 
-    // Custom layer to draw the left axis line
-    const customLeftAxisLayer = ({ innerHeight }: BarCustomLayerProps<BarData>) => {
-        return (
-            <line
-                x1={0}
-                y1={0}
-                x2={0}
-                y2={innerHeight}
-                stroke={theme.palette.text.primary}
-                strokeWidth={1}
-            />
-        );
+    const filteredData: BarData[] = [];
+    const sourcesLessThanOnePercent: BarData[] = [];
+
+    let totalSumOfSourcesLessThanOnePercent: number = 0;
+
+    sourcesData.forEach((data) => {
+        const percentageValue = (data.value / totalSum) * 100;
+        if (percentageValue > 1) {
+            filteredData.push(data);
+        } else {
+            totalSumOfSourcesLessThanOnePercent += data.value;
+            sourcesLessThanOnePercent.push(data);
+        }
+    });
+
+    if (totalSumOfSourcesLessThanOnePercent > 0) {
+        filteredData.push({
+            id: 'other',
+            label: 'Other*',
+            value: totalSumOfSourcesLessThanOnePercent,
+            color: theme.palette.primary.main,
+        });
+    }
+
+    // sort from lowest to highest so Nivo can display from highest to lowest
+    const sortedData = filteredData.sort((a, b) => a.value - b.value);
+
+    // format label of bar data to display with comma
+    const formatNumberWithCommas = (value: number) => {
+        return value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
     };
 
-    // Calculate tick values for the left axis
-    // This creates an array with the tick values, ensuring that the tick values are evenly distributed from 0 to totalSum value of all sources.
-    // This array then will be used to display in % for the left axis value
-    const tickValues = [0, totalSum / 4, (totalSum * 2) / 4, (totalSum * 3) / 4, totalSum];
+    // Custom layer for labels
+    const CustomLabelLayer = ({ bars }: BarCustomLayerProps<BarData>) => (
+        <>
+            {bars.map((bar) => {
+                const isLongBar = bar.width > 100;
+                const labelX = isLongBar ? bar.x + bar.width / 2 : bar.x + bar.width + 5;
+                const textColor = isLongBar
+                    ? theme.palette.primary.contrastText
+                    : theme.color.B10.hex;
+
+                return (
+                    <text
+                        key={bar.key}
+                        x={labelX}
+                        y={bar.y + bar.height / 2 + 4}
+                        textAnchor={isLongBar ? 'middle' : 'start'}
+                        style={{
+                            fill: textColor,
+                            fontSize: 14,
+                        }}>
+                        {`${formatNumberWithCommas(bar.data.data.value)} Documents`}
+                    </text>
+                );
+            })}
+        </>
+    );
+
+    const generatedBarAriaLabel = (data: ComputedDatum<BarData>): string => {
+        return `${data.data.label} makes up ${formatValueAsPercentage(data.data.value, totalSum)}% of the dataset and contains ${data.data.value} documents`;
+    };
 
     if (isLoading) {
         return <LinearProgress />;
@@ -55,77 +95,107 @@ export const SourcesBarChart = () => {
 
     return (
         <ResponsiveCard>
-            <Box
-                sx={(theme) => ({
-                    width: '100%',
-                    maxWidth: '100%',
-                    margin: '0 auto',
-                    [theme.breakpoints.down(SMALL_LAYOUT_BREAKPOINT)]: {
-                        width: '100%',
-                    },
-                    [theme.breakpoints.up(SMALL_LAYOUT_BREAKPOINT)]: {
-                        width: '100%',
-                    },
-                    [theme.breakpoints.up('md')]: {
-                        width: '90%',
-                    },
-                    [theme.breakpoints.up(DESKTOP_LAYOUT_BREAKPOINT)]: {
-                        width: '55%',
-                    },
-                })}>
-                <Typography variant="h3" sx={{ marginBottom: (theme) => theme.spacing(-4) }}>
-                    Sources
-                </Typography>
-                <ChartContainerSansLegend>
-                    <ResponsiveBar
-                        data={sourcesData.map((item) => ({ ...item }))}
-                        keys={['value']}
-                        indexBy="label"
-                        label={({ data }) => formatValueAsPercentage(data.value, totalSum)}
-                        labelSkipWidth={0}
-                        labelSkipHeight={0}
-                        padding={0.1}
-                        colors={({ data }) => data.color}
-                        margin={{ top: 50, right: 30, bottom: 100, left: 110 }}
-                        groupMode="grouped"
-                        axisLeft={{
-                            tickValues,
-                            tickSize: 5,
-                            tickPadding: 5,
-                            tickRotation: 0,
-                            legend: '% of Documents in Dataset',
-                            legendPosition: 'middle',
-                            legendOffset: -70,
-                            format: (value) => formatValueAsPercentage(value as number, totalSum),
-                        }}
-                        axisBottom={{
-                            tickSize: 0,
-                            tickPadding: 15,
-                            tickRotation: 40.75,
-                            legend: 'Sources',
-                            legendPosition: 'middle',
-                            legendOffset: 80,
-                            truncateTickAt: 0,
-                            format: (value) => value,
-                        }}
-                        enableGridX={false}
-                        enableGridY={false}
-                        enableLabel={true}
-                        minValue={0}
-                        maxValue="auto"
-                        layers={[
-                            'grid',
-                            'axes',
-                            'bars',
-                            'markers',
-                            'legends',
-                            'annotations',
-                            customLeftAxisLayer,
-                        ]}
-                        ariaLabel={barChartAriaLabel}
-                    />
-                </ChartContainerSansLegend>
-            </Box>
+            <Typography variant="h3" sx={{ marginBottom: (theme) => theme.spacing(-4) }}>
+                Sources
+            </Typography>
+            <ChartContainerSansLegend>
+                <ResponsiveBar
+                    data={sortedData.map((item) => ({ ...item }))}
+                    keys={['value']}
+                    indexBy="label"
+                    layout="horizontal"
+                    labelSkipWidth={0}
+                    labelSkipHeight={0}
+                    padding={0.1}
+                    colors={theme.palette.primary.main}
+                    margin={{ top: 50, right: 30, bottom: 100, left: 110 }}
+                    groupMode="grouped"
+                    axisLeft={{
+                        tickSize: 0,
+                        tickPadding: 25,
+                        tickRotation: 0,
+                        legendPosition: 'middle',
+                        legendOffset: -70,
+                        format: (value) => value,
+                    }}
+                    axisBottom={{
+                        tickSize: 0,
+                        tickPadding: 20,
+                        tickRotation: 0,
+                        legend: 'Percentage of Dataset',
+                        legendPosition: 'middle',
+                        legendOffset: 80,
+                        truncateTickAt: 0,
+                        format: (value) => formatValueAsPercentage(value as number, totalSum),
+                    }}
+                    enableGridX={true}
+                    enableGridY={false}
+                    enableLabel={false}
+                    minValue={0}
+                    maxValue="auto"
+                    layers={[
+                        'grid',
+                        'axes',
+                        'bars',
+                        'markers',
+                        'legends',
+                        'annotations',
+                        CustomLabelLayer,
+                    ]}
+                    barAriaLabel={generatedBarAriaLabel}
+                    isFocusable={true}
+                />
+            </ChartContainerSansLegend>
+            <Divider />
+            <Grid
+                container
+                direction="column"
+                spacing={1}
+                justifyContent="flex-start"
+                alignItems="flex-start">
+                <Grid item container justifyContent="space-between" alignItems="center">
+                    <Grid item xs={6}>
+                        <Typography variant="h6" sx={{ fontWeight: 'bold', textAlign: 'left' }}>
+                            Other* includes
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={6} sx={{ textAlign: 'right', paddingRight: 2 }}>
+                        <Typography
+                            variant="body1"
+                            sx={{
+                                color: (theme) => theme.color.N9.hex,
+                                textAlign: 'right',
+                                margin: '20px 0 0',
+                            }}>
+                            Document Count
+                        </Typography>
+                    </Grid>
+                </Grid>
+                {sourcesLessThanOnePercent.map((data, index) => (
+                    <Grid
+                        item
+                        container
+                        justifyContent="space-between"
+                        alignItems="center"
+                        key={index}
+                        sx={{ marginBottom: 1, paddingLeft: 2 }}>
+                        <Grid item xs={6}>
+                            <Typography variant="body1" sx={{ textAlign: 'left' }}>
+                                {data.label}
+                            </Typography>
+                        </Grid>
+                        <Grid item xs={6} sx={{ textAlign: 'right', paddingRight: 2 }}>
+                            <Typography
+                                variant="body1"
+                                sx={{
+                                    color: (theme) => theme.palette.primary.main,
+                                }}>
+                                {formatNumberWithCommas(data.value)}
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                ))}
+            </Grid>
         </ResponsiveCard>
     );
 };
--- src/util.ts
+++ src/util.ts
@@ -2,5 +2,5 @@ export const dateTimeFormat = 'M/D/YY h:mm a';
 
 export const formatValueAsPercentage = (value: number, totalSum: number) => {
     const percentage = (value / totalSum) * 100;
-    return `${percentage.toFixed(2)}%`;
+    return percentage.toFixed(0);
 };
