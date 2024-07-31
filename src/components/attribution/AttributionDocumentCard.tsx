ec7da0ba72d800e9b1925b0ea7f763b2cb095921
Taylor Blanton <matthewtblanton@gmail.com>
2024-07-31 15:06:54 -0700

    cards are looking better

--- src/components/attribution/AttributionDocumentCard.tsx
+++ src/components/attribution/AttributionDocumentCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Stack, Typography } from '@mui/material';
+import { ButtonBase, Card, CardActionArea, CardContent, Stack, Typography } from '@mui/material';
 
 import { useAppContext } from '@/AppContext';
 
@@ -20,17 +20,36 @@ export const AttributionDocumentCard = ({
         (state) => state.attribution.selectedDocumentIndex === documentIndex
     );
 
+    const setSelectedDocument = useAppContext((state) => state.setSelectedDocument);
+
     return (
-        <Card>
-            <CardContent component={Stack} direction="column" gap={1}>
-                <Typography variant="h6" margin={0}>
-                    {title}
-                </Typography>
-                <Typography variant="body1">&quot;{text}&quot;...</Typography>
-                <Typography variant="subtitle1" fontWeight="bold" component="span">
-                    Source: {source}
-                </Typography>
-            </CardContent>
+        <Card
+            sx={{
+                borderLeft: (theme) => `${theme.spacing(0.5)} solid transparent`,
+
+                '&:hover': {
+                    borderColor: (theme) => theme.palette.primary.light,
+                },
+
+                '&[data-selected-document=true]': {
+                    borderColor: (theme) => theme.palette.primary.main,
+                },
+            }}
+            data-selected-document={isSelected}>
+            <CardActionArea
+                onClick={() => {
+                    setSelectedDocument(documentIndex);
+                }}>
+                <CardContent component={Stack} direction="column" gap={1}>
+                    <Typography variant="h6" margin={0}>
+                        {title}
+                    </Typography>
+                    <Typography variant="body1">&quot;{text}&quot;...</Typography>
+                    <Typography variant="subtitle1" fontWeight="bold" component="span">
+                        Source: {source}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
         </Card>
     );
 };
