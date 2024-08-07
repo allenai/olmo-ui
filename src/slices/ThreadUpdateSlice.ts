fb153eec9f61e7cb789bf268cbb35ea7435c7516
Taylor Blanton <matthewtblanton@gmail.com>
2024-08-07 09:53:21 -0500

    [OEUI-84] Load attributions when making a new thread and responding (#546)
    This PR makes it so that starting a new thread will load attributions
    when its finished. It also makes it so that attributions from a previous
    thread will go away when navigating to a new thread.
    
    
    https://github.com/user-attachments/assets/de162543-4e89-4957-82fa-bb7dd1aef883

--- src/slices/ThreadUpdateSlice.ts
+++ src/slices/ThreadUpdateSlice.ts
@@ -117,6 +117,8 @@ export const createThreadUpdateSlice: OlmoStateCreator<ThreadUpdateSlice> = (set
             handleFinalMessage,
             setSelectedThread,
             setMessageLimitReached,
+            getAttributionsForMessage,
+            resetAttribution,
         } = get();
         const abortController = new AbortController();
         const isCreatingNewThread = newMessage.parent == null;
@@ -133,6 +135,8 @@ export const createThreadUpdateSlice: OlmoStateCreator<ThreadUpdateSlice> = (set
             false,
             'threadUpdate/startCreateNewThread'
         );
+
+        resetAttribution();
 
         try {
             const messageChunks = postMessageGenerator(
@@ -168,6 +172,9 @@ export const createThreadUpdateSlice: OlmoStateCreator<ThreadUpdateSlice> = (set
 
                 if (isFinalMessage(message)) {
                     handleFinalMessage(parseMessage(message), isCreatingNewThread);
+                    await getAttributionsForMessage(
+                        message.children[message.children.length - 1].id
+                    );
                 }
             }
         } catch (err) {
