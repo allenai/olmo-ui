b8cb528b609d3938b1f72ac790a4faf1fa909b12
Taylor Blanton <matthewtblanton@gmail.com>
2024-08-14 14:45:18 -0500

    yagni extra regex stuff

--- src/components/thread/span-replacement-regex.ts
+++ src/components/thread/span-replacement-regex.ts
@@ -1,15 +1,13 @@
-const bannedStringsInFront = ['span="'].join('|');
-const bannedStringsBehind = ['"'].join('|');
 /**
  * @param spanToReplace
- * @returns A regex that will match spanToReplace as long as it isn't preceded by strings in bannedStringsInFront and followed by strings in bannedStringsBehind
+ * @returns A regex that will match spanToReplace as long as it isn't inside span="<value>"
  */
 export const createSpanReplacementRegex = (spanToReplace: string) => {
     // This regex uses negative lookbehind and negative lookahead
     // See the regex101 site for an explanation for this regex: https://regex101.com/r/rnbz3G
     return new RegExp(
         // String.raw is needed to prevent JS from escaping things automatically
-        String.raw`(?<![${bannedStringsInFront}])${spanToReplace}(?![ws]*[${bannedStringsBehind}])`,
+        String.raw`(?<![span="])${spanToReplace}(?![ws]*["])`,
         'g'
     );
 };
