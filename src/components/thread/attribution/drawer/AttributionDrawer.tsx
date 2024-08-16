535d7f0ef4f5d7a1cb8e63dee722d46357c2254f
Taylor Blanton <matthewtblanton@gmail.com>
2024-08-16 12:46:19 -0500

    remove unused imports from attributiondrawer

--- src/components/thread/attribution/drawer/AttributionDrawer.tsx
+++ src/components/thread/attribution/drawer/AttributionDrawer.tsx
@@ -1,5 +1,5 @@
 import CloseIcon from '@mui/icons-material/Close';
-import { Box, Button, Divider, IconButton, ListSubheader, Stack, Typography } from '@mui/material';
+import { Box, Divider, IconButton, ListSubheader, Stack, Typography } from '@mui/material';
 import { KeyboardEventHandler } from 'react';
 
 import { useAppContext } from '@/AppContext';
