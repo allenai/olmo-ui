2cfb94061911f5ffac80589c66a31ea709bba9a4
Taylor Blanton <matthewtblanton@gmail.com>
2024-08-22 17:32:07 -0500

    add a test, fix the extra paren in the regex

--- src/components/thread/attribution/span-replacement-regex.ts
+++ src/components/thread/attribution/span-replacement-regex.ts
@@ -7,5 +7,5 @@ import { escapeRegExp } from '@/utils/escape-reg-exp';
 export const createSpanReplacementRegex = (spanToReplace: string) => {
     // This regex uses negative lookbehind and negative lookahead
     // See the regex101 site for an explanation for this regex: https://regex101.com/r/rnbz3G
-    return new RegExp(`(?<!(?:span="))${escapeRegExp(spanToReplace)})`, 'g');
+    return new RegExp(`(?<!(?:span="))${escapeRegExp(spanToReplace)}`, 'g');
 };
