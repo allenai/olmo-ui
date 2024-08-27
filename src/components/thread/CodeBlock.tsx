10c64c60609d01c3c09b07b0268550678e6c0a54
Taylor Blanton <matthewtblanton@gmail.com>
2024-08-27 14:17:04 -0500

    WIP

--- src/components/thread/CodeBlock.tsx
+++ src/components/thread/CodeBlock.tsx
@@ -1,18 +1,35 @@
+import { useEffect } from 'react';
 import { PrismAsyncLight as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dracula } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 interface CodeBlockProps {
     inline?: boolean;
     className?: string;
-    children?: React.ReactNode;
+    children?: string;
 }
 
-export const CodeBlock = ({ inline, className, children, ...props }: CodeBlockProps) => {
+const attributionHighlightRegex =
+    /:attribution-highlight\[(?<spanText>.*)\]{variant=".*" span="(?<spanId>.*)"}/gm;
+
+export const CodeBlock = ({ inline, className, children = '', ...props }: CodeBlockProps) => {
     const match = /language-(\w+)/.exec(className || '');
 
+    const attributionHighlights = Array.from(children.matchAll(attributionHighlightRegex));
+    console.log(attributionHighlights);
+
+    const childrenWithoutAttributionHighlights = children.replaceAll(
+        attributionHighlightRegex,
+        '$1'
+    );
+
     return !inline && match ? (
-        <SyntaxHighlighter style={dracula} PreTag="div" language={match[1]} {...props}>
-            {String(children).replace(/\n$/, '')}
+        <SyntaxHighlighter
+            style={dracula}
+            PreTag="div"
+            language={match[1]}
+            {...props}
+            wrapLongLines>
+            {childrenWithoutAttributionHighlights.replace(/\n$/, '')}
         </SyntaxHighlighter>
     ) : (
         <code className={className} {...props}>
