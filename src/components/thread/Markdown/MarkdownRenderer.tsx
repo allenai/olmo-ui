1d538bca8b5c0eb3d65302a1e476d1e5ebea7dbe
huytr1995 <huyt@allenai.org>
2024-09-03 14:45:03 -0700

    Add reyhpe-sanitze to prevent xcss attack for user

--- src/components/thread/Markdown/MarkdownRenderer.tsx
+++ src/components/thread/Markdown/MarkdownRenderer.tsx
@@ -1,5 +1,6 @@
 import Markdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
+import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
 import remarkDirective from 'remark-directive';
 import remarkDirectiveRehype from 'remark-directive-rehype';
 import remarkGfm from 'remark-gfm';
@@ -16,7 +17,7 @@ export const MarkdownRenderer = ({ children: markdown }: MarkdownRendererProps) 
     return (
         <Markdown
             remarkPlugins={[remarkGfm, remarkDirective, remarkDirectiveRehype]}
-            rehypePlugins={[rehypeRaw]}
+            rehypePlugins={[rehypeRaw, [rehypeSanitize, defaultSchema]]}
             components={{
                 code: CodeBlock,
                 // @ts-expect-error - We add attribution-highlight as a custom element
