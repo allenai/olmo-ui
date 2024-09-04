6fca00cd28d0b6f17a4396adcc3dcea2f1c55a0a
huytr1995 <huyt@allenai.org>
2024-09-04 09:24:42 -0700

    WIP

--- src/components/thread/Markdown/MarkdownRenderer.tsx
+++ src/components/thread/Markdown/MarkdownRenderer.tsx
@@ -1,4 +1,5 @@
 import Markdown from 'react-markdown';
+import rehypeHighlight from 'rehype-highlight';
 import rehypeRaw from 'rehype-raw';
 import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
 import remarkDirective from 'remark-directive';
@@ -22,7 +23,7 @@ export const MarkdownRenderer = ({ children: markdown }: MarkdownRendererProps) 
     return (
         <Markdown
             remarkPlugins={[remarkGfm, remarkDirective, remarkDirectiveRehype]}
-            rehypePlugins={[rehypeRaw, [rehypeSanitize, extendedSchema]]}
+            rehypePlugins={[rehypeRaw, [rehypeSanitize, extendedSchema], rehypeHighlight]}
             components={{
                 code: CodeBlock,
                 // @ts-expect-error - We add attribution-highlight as a custom element
