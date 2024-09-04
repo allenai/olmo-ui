011fa1cc710bee37a835b9ec8a0546a14ffc5e9c
huytr1995 <huyt@allenai.org>
2024-09-04 10:08:37 -0700

    Make codeblock work

--- src/components/thread/Markdown/MarkdownRenderer.tsx
+++ src/components/thread/Markdown/MarkdownRenderer.tsx
@@ -1,5 +1,4 @@
 import Markdown from 'react-markdown';
-import rehypeHighlight from 'rehype-highlight';
 import rehypeRaw from 'rehype-raw';
 import rehypeSanitize, { defaultSchema } from 'rehype-sanitize';
 import remarkDirective from 'remark-directive';
@@ -17,13 +16,30 @@ interface MarkdownRendererProps {
 const extendedSchema = {
     ...defaultSchema,
     tagNames: [...(defaultSchema.tagNames || []), 'attribution-highlight'],
+    attributes: {
+        ...defaultSchema.attributes,
+        '*': [
+            ...(defaultSchema.attributes?.['*'] || []),
+            'style', // Allow inline styles on all elements
+        ],
+        span: [...(defaultSchema.attributes?.span || []), 'className'],
+        div: [
+            ...(defaultSchema.attributes?.div || []),
+            'className', // Allow className on <div>
+            'style', // Allow style attribute on <div>
+        ],
+        code: [
+            ...(defaultSchema.attributes?.code || []),
+            ['className', 'language-js', 'language-css', 'language-md'],
+        ],
+    },
 };
 
 export const MarkdownRenderer = ({ children: markdown }: MarkdownRendererProps) => {
     return (
         <Markdown
             remarkPlugins={[remarkGfm, remarkDirective, remarkDirectiveRehype]}
-            rehypePlugins={[rehypeRaw, [rehypeSanitize, extendedSchema], rehypeHighlight]}
+            rehypePlugins={[rehypeRaw, [rehypeSanitize, extendedSchema]]}
             components={{
                 code: CodeBlock,
                 // @ts-expect-error - We add attribution-highlight as a custom element
