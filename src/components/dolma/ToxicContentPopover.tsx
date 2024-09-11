bfdf2f13a66bffe6a53ca4d6cbf6b10b1b3a6a4d
Huy Tran <84343285+huytr1995@users.noreply.github.com>
2024-09-11 09:43:03 -0700

    [OEUI-114] Implement Toxicity filter (There are 2 txt files content toxic words that we want to blur please not looking at it) (#579)
    ## Description
    
    Closes: https://allenai.atlassian.net/browse/OEUI-114
    Figma:
    
    https://www.figma.com/design/lzeziiZpknJSelQF6558d3/AI2-Playground-with-Rebrand-(Sept-2024)?node-id=1162-12437&node-type=frame&t=h0gnOEV5lgxrJVUd-0
    
    ## Reviewer Instructions
    
    Based on the doc Dany put in the ticket. I followed his same logic in
    the doc. The color in the code will change once we upgrade varnish
    package .
    
    ## Testing Plan
    
    Verify UI works. If CI passes we are good. 
    
    ## Output / Screenshots
    
    
    https://github.com/user-attachments/assets/0a7c2bc4-5b5c-4b46-a05d-058d2cf9e22d
    
    ### A11y
    
    N/A

---
+++ src/components/dolma/ToxicContentPopover.tsx
@@ -0,0 +1,48 @@
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import { IconButton } from '@mui/material';
+import { useState } from 'react';
+
+import { ResponsiveTooltip } from '../thread/ResponsiveTooltip';
+
+const TOOLTIP_TITLE = 'Toxicity Filter';
+
+const TOOLTIP_CONTENT =
+    "To minimize the risk of encountering documents with disturbing content while exploring the dataset, we've implemented a simple filter that scans document previews for offensive language. You have the option to reveal the blurred text if you choose to do so. Please note that our filter only detects specific toxic or offensive words. You might still encounter potentially harmful content if it doesn't contain any bad word.";
+
+interface ToxicContentPopoverProps {
+    anchorEl: HTMLElement | undefined;
+}
+
+export const ToxicContentPopover = ({ anchorEl }: ToxicContentPopoverProps) => {
+    const [isTooltipOpen, setIsTooltipOpen] = useState(false);
+
+    const toggleTooltipOpen = () => {
+        setIsTooltipOpen(!isTooltipOpen);
+    };
+
+    const handleTooltipClose = () => {
+        setIsTooltipOpen(false);
+    };
+
+    return (
+        <>
+            {/* TODO: we need to figure out the right color for the info icon we just inherit from text for now */}
+            <ResponsiveTooltip
+                anchorEl={anchorEl}
+                dialogTitle={TOOLTIP_TITLE}
+                dialogContent={TOOLTIP_CONTENT}
+                isTooltipOpen={isTooltipOpen}
+                onTooltipClose={handleTooltipClose}
+                placement="right"
+                tooltipIdSuffix="toxic-content-description">
+                <IconButton
+                    tabIndex={0}
+                    aria-expanded={isTooltipOpen}
+                    sx={{ color: 'inherit', justifySelf: 'start' }}
+                    onClick={toggleTooltipOpen}>
+                    <InfoOutlinedIcon />
+                </IconButton>
+            </ResponsiveTooltip>
+        </>
+    );
+};
