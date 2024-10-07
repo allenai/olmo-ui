e0c75cd8a3a9ee1f690fa7b4da284418fa7107ed
Taylor Blanton <matthewtblanton@gmail.com>
2024-10-07 12:03:12 -0500

    WIP

--- api/dolma_search/api/v1.py
+++ api/dolma_search/api/v1.py
@@ -1,9 +1,17 @@
-from .. import config, index, analytics
-from werkzeug import exceptions
-from google.oauth2.service_account import Credentials
 from datetime import datetime
 
 import flask
+from google.oauth2.service_account import Credentials
+from werkzeug import exceptions
+
+from dolma_search.infini_gram_api_client.models.available_infini_gram_index_id import (
+    AvailableInfiniGramIndexId,
+)
+
+from .. import analytics, config, index
+from ..infini_gram_api_client import Client
+from ..infini_gram_api_client.api.documents import search_documents_index_documents_get
+
 
 def parse_int_from_qs(name: str, default: int, smallest: int, largest: int) -> int:
     try:
@@ -12,12 +20,17 @@ def parse_int_from_qs(name: str, default: int, smallest: int, largest: int) -> i
             return default
         i = int(v)
         if not smallest <= i <= largest:
-            raise exceptions.BadRequest(f"The \"{name}\" argument \"{i}\" must be in range [{smallest}, {largest}]")
+            raise exceptions.BadRequest(
+                f'The "{name}" argument "{i}" must be in range [{smallest}, {largest}]'
+            )
         return i
     except ValueError:
-        raise exceptions.BadRequest(f"The \"{name}\" argument must be a valid integer")
+        raise exceptions.BadRequest(f'The "{name}" argument must be a valid integer')
+
 
 class Server(flask.Blueprint):
+    infini_gram_client: Client
+
     def __init__(self):
         super().__init__("v1", __name__)
 
@@ -25,6 +38,8 @@ class Server(flask.Blueprint):
         self.get("/document/<id>")(self.document)
         self.get("/meta")(self.meta)
         self.post("/event")(self.create_event)
+
+        self.infini_gram_client = Client(base_url="https://infinigram-api.allen.ai")
 
     def es(self) -> index.Client:
         c = config.Config.load()
@@ -38,15 +53,20 @@ class Server(flask.Blueprint):
     def search_request_from_query_string(self) -> index.SearchRequest:
         query = flask.request.args.get("query", default="", type=lambda s: s.strip())
         if query == "":
-            raise exceptions.BadRequest("The \"query\" argument must not be empty")
+            raise exceptions.BadRequest('The "query" argument must not be empty')
 
         # Reject long queries. This allows 100 characters which is ~20 words.
         if len(query) > 100:
-            raise exceptions.BadRequest("The \"query\" argument must not exceed 100 characters")
+            raise exceptions.BadRequest(
+                'The "query" argument must not exceed 100 characters'
+            )
 
         try:
             filters = None
-            sources = [index.Source(s) for s in flask.request.args.getlist("source", type=lambda s: s.strip())]
+            sources = [
+                index.Source(s)
+                for s in flask.request.args.getlist("source", type=lambda s: s.strip())
+            ]
             ids = flask.request.args.getlist("id", type=lambda s: s.strip())
             filters = index.Filters(sources, ids)
         except ValueError as err:
@@ -73,20 +93,31 @@ class Server(flask.Blueprint):
             raise exceptions.BadRequest("Unable to retrieve more than 10,000 results")
 
         try:
-            sp = flask.request.args.get("snippet", default=index.SnippetType.Short.value)
+            sp = flask.request.args.get(
+                "snippet", default=index.SnippetType.Short.value
+            )
             snippet = index.SnippetType(sp)
         except ValueError as err:
             raise exceptions.BadRequest(str(err))
 
-        return index.SearchRequest(query, offset, size, filters, match, no_aggs, snippet)
+        return index.SearchRequest(
+            query, offset, size, filters, match, no_aggs, snippet
+        )
 
     def search(self):
+        return flask.jsonify(
+            search_documents_index_documents_get.sync(
+                client=self.infini_gram_client,
+                index=AvailableInfiniGramIndexId.OLMOE_MIX_0924,
+                search=self.search_request_from_query_string().query,
+            )
+        )
         return flask.jsonify(self.es().search(self.search_request_from_query_string()))
 
     def document(self, id: str):
         doc = self.es().document(id)
         if doc is None:
-            raise exceptions.NotFound(f"Document \"{id}\" not found")
+            raise exceptions.NotFound(f'Document "{id}" not found')
         return flask.jsonify(doc)
 
     def meta(self):
@@ -98,7 +129,9 @@ class Server(flask.Blueprint):
         try:
             details = flask.request.json.get("details")
             if details is not None and not isinstance(details, dict):
-                raise exceptions.BadRequest("The \"details\" argument must be a JSON object")
+                raise exceptions.BadRequest(
+                    'The "details" argument must be a JSON object'
+                )
             event = analytics.Event(
                 type=flask.request.json["type"],
                 url=flask.request.referrer,
@@ -110,4 +143,3 @@ class Server(flask.Blueprint):
 
         self.analytics().insert(event)
         return flask.Response(status=201)
-
