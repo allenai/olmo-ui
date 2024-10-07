114fa25314edc6db3414461351db6771dc568eda
Taylor Blanton <matthewtblanton@gmail.com>
2024-10-07 12:03:22 -0500

    add api stuff

--- api/dolma_search/infini_gram_api_client/models/available_infini_gram_index_id.py
+++ api/dolma_search/infini_gram_api_client/models/available_infini_gram_index_id.py
@@ -4,6 +4,7 @@ from enum import Enum
 class AvailableInfiniGramIndexId(str, Enum):
     DOLMA_1_7 = "dolma-1_7"
     PILEVAL_LLAMA = "pileval-llama"
+    OLMOE_MIX_0924 = "olmoe-mix-0924"
 
     def __str__(self) -> str:
         return str(self.value)
