22aa5df04cb05d857eab2697022d4656fab814f7
Taylor Blanton <matthewtblanton@gmail.com>
2024-10-08 18:11:29 -0500

    a little more cleanup lol

--- webpack.config.js
+++ webpack.config.js
@@ -89,7 +89,7 @@ module.exports = (env) => ({
             DOLMA_API_URL: '/api',
             ENABLE_MOCKING: false,
             IS_ATTRIBUTION_ENABLED: true,
-            AUTH0_DOMAIN: 'allenai-public-dev.us.auth0.com',
+            AUTH0_DOMAIN: 'allenai-public.us.auth0.com',
             AUTH0_CLIENT_ID: 'CmIxLlwn0miZ8kaQgDAK37pG0L6vrQEm',
             AUTH0_OLMO_API_AUDIENCE: 'https://olmo-api.allen.ai',
             IS_ATTRIBUTION_SPAN_FIRST_ENABLED: true,
