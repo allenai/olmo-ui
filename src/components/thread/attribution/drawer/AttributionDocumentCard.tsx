443be54b4721e14dad04d8816b34c96cd1ba03cc
Taylor Blanton <matthewtblanton@gmail.com>
2024-10-11 18:22:35 -0500

    [OEUI-237] Add "Open in Dataset Explorer" link to CorpusLink document cards (#637)
    ## Description
    
    Closes OEUI-237
    
    This PR adds the "Open in Dataset Explorer" button to the CorpusLink
    document cards. It links to the corresponding document in the dataset
    explorer. The user currently has to click on the button itself, but we
    can make it so clicking on any part of the card goes to the right place.
    
    It also makes us fetch 1mm tokens for a document in dataset explorer
    instead of just 10. We may want to add a way to fetch all tokens to
    infinigram-api at some point, 1mm is just arbitrarily large.
    
    Question: do we want the document to open in another tab?
    
    ## Testing Plan
    
    Tested locally. I don't think this needs a test.
    
    ## Output / Screenshots
    
    
    ![image](https://github.com/user-attachments/assets/cada2202-261b-43cc-9f46-98a54473906d)
    
    (ignore that the document isn't the same between the document card and
    the explorer, this is a mock data thing)
    
    https://github.com/user-attachments/assets/a2e61450-dfd5-4302-99a3-1aa1936fc676
    
    ### A11y
    
    The button follows proper a11y practices

--- src/components/thread/attribution/drawer/AttributionDocumentCard.tsx
+++ src/components/thread/attribution/drawer/AttributionDocumentCard.tsx
@@ -1,7 +1,8 @@
-import { Card, CardActionArea, CardContent, Skeleton, Stack, Typography } from '@mui/material';
+import { Button, Card, CardActions, CardContent, Skeleton, Stack, Typography } from '@mui/material';
 import { ReactNode } from 'react';
 
 import { useAppContext } from '@/AppContext';
+import { links } from '@/Links';
 
 import { BoldTextForDocumentAttribution } from './BoldTextForDocumentAttribution';
 
@@ -9,6 +10,7 @@ interface AttributionDocumentCardBaseProps {
     title: ReactNode;
     text: ReactNode;
     source: ReactNode;
+    datasetExplorerLink: ReactNode;
     // href: string;
     setSelectedDocument?: () => void;
     setPreviewDocument?: () => void;
@@ -18,78 +20,60 @@ interface AttributionDocumentCardBaseProps {
 }
 
 const AttributionDocumentCardBase = ({
-    setSelectedDocument,
-    setPreviewDocument,
-    unsetPreviewDocument,
     isSelected,
     isPreviewed,
     title,
     text,
     source,
+    datasetExplorerLink,
 }: AttributionDocumentCardBaseProps) => {
     return (
         <Card
             sx={{
                 bgcolor: '#F8F0E780',
+
+                borderLeft: (theme) => `${theme.spacing(1)} solid transparent`,
+
+                '&[data-previewed-document="true"]': {
+                    borderColor: (theme) => theme.palette.primary.light,
+                },
+
+                '&[data-selected-document="true"]': {
+                    borderColor: (theme) => theme.palette.primary.main,
+                },
             }}>
-            <CardActionArea
-                disabled={setSelectedDocument == null}
-                onClick={() => {
-                    setSelectedDocument?.();
-                }}
-                onMouseEnter={() => {
-                    setPreviewDocument?.();
-                }}
-                onMouseLeave={() => {
-                    unsetPreviewDocument?.();
-                }}
-                onFocus={() => {
-                    setPreviewDocument?.();
-                }}
-                onBlur={() => {
-                    unsetPreviewDocument?.();
-                }}>
-                <CardContent
-                    component={Stack}
-                    direction="column"
-                    gap={1}
+            <CardContent
+                component={Stack}
+                direction="column"
+                gap={1}
+                data-selected-document={isSelected}
+                data-previewed-document={isPreviewed}>
+                <Typography
+                    variant="body1"
+                    fontWeight="bold"
+                    component="h4"
+                    margin={0}
                     sx={{
-                        borderLeft: (theme) => `${theme.spacing(1)} solid transparent`,
-
-                        '&[data-previewed-document="true"]': {
-                            borderColor: (theme) => theme.palette.primary.light,
-                        },
-
-                        '&[data-selected-document="true"]': {
-                            borderColor: (theme) => theme.palette.primary.main,
-                        },
-                    }}
-                    data-selected-document={isSelected}
-                    data-previewed-document={isPreviewed}>
-                    <Typography
-                        variant="body1"
-                        fontWeight="bold"
-                        component="h4"
-                        margin={0}
-                        sx={{
-                            // This isn't _standard_ standard but it's widely available
-                            // It uses an older version of a Chrome flex implementation that's supported in Safari and FF
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            display: '-webkit-box',
-                            '-webkit-line-clamp': '2',
-                            lineClamp: '2',
-                            '-webkit-box-orient': 'vertical',
-                        }}>
-                        {title}
-                    </Typography>
-                    <Typography variant="body1">{text}</Typography>
-                    {/* todo: Switch this to theme.typography.fontWeightSemiBold when it's added  */}
-                    <Typography variant="body2" fontWeight={600} component="span">
-                        {source}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
+                        // This isn't _standard_ standard but it's widely available
+                        // It uses an older version of a Chrome flex implementation that's supported in Safari and FF
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        display: '-webkit-box',
+                        '-webkit-line-clamp': '2',
+                        lineClamp: '2',
+                        '-webkit-box-orient': 'vertical',
+                    }}>
+                    {title}
+                </Typography>
+                <Typography variant="body1">{text}</Typography>
+                {/* todo: Switch this to theme.typography.fontWeightSemiBold when it's added  */}
+                <Typography variant="body2" fontWeight={600} component="span">
+                    {source}
+                </Typography>
+            </CardContent>
+            <CardActions sx={{ padding: 2, paddingBlockStart: 0 }}>
+                {datasetExplorerLink != null && datasetExplorerLink}
+            </CardActions>
         </Card>
     );
 };
@@ -126,6 +110,19 @@ export const AttributionDocumentCard = ({
             title={title ?? MISSING_DOCUMENT_TITLE_TEXT}
             text={<BoldTextForDocumentAttribution correspondingSpans={spans} text={text} />}
             source={`Source: ${source}`}
+            datasetExplorerLink={
+                <Button
+                    href={links.document(documentIndex)}
+                    variant="outlined"
+                    color="inherit"
+                    size="small"
+                    fullWidth={false}
+                    sx={{
+                        width: 'fit-content',
+                    }}>
+                    Open in Dataset Explorer
+                </Button>
+            }
         />
     );
 };
@@ -144,6 +141,7 @@ export const AttributionDocumentCardSkeleton = (): JSX.Element => {
                 </>
             }
             source={<Skeleton />}
+            datasetExplorerLink={<Skeleton />}
         />
     );
 };
