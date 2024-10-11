4b666a830ebffb8e3769b653baf1a89050174b88
Taylor Blanton <matthewtblanton@gmail.com>
2024-10-11 17:23:31 -0500

    add open in dataset explorer button

--- src/components/thread/attribution/drawer/AttributionDocumentCard.tsx
+++ src/components/thread/attribution/drawer/AttributionDocumentCard.tsx
@@ -1,7 +1,8 @@
-import { Card, CardActionArea, CardContent, Skeleton, Stack, Typography } from '@mui/material';
+import { Button, Card, CardContent, Skeleton, Stack, Typography } from '@mui/material';
 import { ReactNode } from 'react';
 
 import { useAppContext } from '@/AppContext';
+import { links } from '@/Links';
 
 import { BoldTextForDocumentAttribution } from './BoldTextForDocumentAttribution';
 
@@ -9,6 +10,7 @@ interface AttributionDocumentCardBaseProps {
     title: ReactNode;
     text: ReactNode;
     source: ReactNode;
+    datasetExplorerLink: ReactNode;
     // href: string;
     setSelectedDocument?: () => void;
     setPreviewDocument?: () => void;
@@ -18,78 +20,59 @@ interface AttributionDocumentCardBaseProps {
 }
 
 const AttributionDocumentCardBase = ({
-    setSelectedDocument,
-    setPreviewDocument,
-    unsetPreviewDocument,
     isSelected,
     isPreviewed,
     title,
     text,
     source,
+    datasetExplorerLink,
 }: AttributionDocumentCardBaseProps) => {
     return (
         <Card
             sx={{
                 bgcolor: '#F8F0E780',
             }}>
-            <CardActionArea
-                disabled={setSelectedDocument == null}
-                onClick={() => {
-                    setSelectedDocument?.();
+            <CardContent
+                component={Stack}
+                direction="column"
+                gap={1}
+                sx={{
+                    borderLeft: (theme) => `${theme.spacing(1)} solid transparent`,
+
+                    '&[data-previewed-document="true"]': {
+                        borderColor: (theme) => theme.palette.primary.light,
+                    },
+
+                    '&[data-selected-document="true"]': {
+                        borderColor: (theme) => theme.palette.primary.main,
+                    },
                 }}
-                onMouseEnter={() => {
-                    setPreviewDocument?.();
-                }}
-                onMouseLeave={() => {
-                    unsetPreviewDocument?.();
-                }}
-                onFocus={() => {
-                    setPreviewDocument?.();
-                }}
-                onBlur={() => {
-                    unsetPreviewDocument?.();
-                }}>
-                <CardContent
-                    component={Stack}
-                    direction="column"
-                    gap={1}
+                data-selected-document={isSelected}
+                data-previewed-document={isPreviewed}>
+                <Typography
+                    variant="body1"
+                    fontWeight="bold"
+                    component="h4"
+                    margin={0}
                     sx={{
-                        borderLeft: (theme) => `${theme.spacing(1)} solid transparent`,
-
-                        '&[data-previewed-document="true"]': {
-                            borderColor: (theme) => theme.palette.primary.light,
-                        },
-
-                        '&[data-selected-document="true"]': {
-                            borderColor: (theme) => theme.palette.primary.main,
-                        },
-                    }}
-                    data-selected-document={isSelected}
-                    data-previewed-document={isPreviewed}>
-                    <Typography
-                        variant="body1"
-                        fontWeight="bold"
-                        component="h4"
-                        margin={0}
-                        sx={{
-                            // This isn't _standard_ standard but it's widely available
-                            // It uses an older version of a Chrome flex implementation that's supported in Safari and FF
-                            overflow: 'hidden',
-                            textOverflow: 'ellipsis',
-                            display: '-webkit-box',
-                            '-webkit-line-clamp': '2',
-                            lineClamp: '2',
-                            '-webkit-box-orient': 'vertical',
-                        }}>
-                        {title}
-                    </Typography>
-                    <Typography variant="body1">{text}</Typography>
-                    {/* todo: Switch this to theme.typography.fontWeightSemiBold when it's added  */}
-                    <Typography variant="body2" fontWeight={600} component="span">
-                        {source}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
+                        // This isn't _standard_ standard but it's widely available
+                        // It uses an older version of a Chrome flex implementation that's supported in Safari and FF
+                        overflow: 'hidden',
+                        textOverflow: 'ellipsis',
+                        display: '-webkit-box',
+                        '-webkit-line-clamp': '2',
+                        lineClamp: '2',
+                        '-webkit-box-orient': 'vertical',
+                    }}>
+                    {title}
+                </Typography>
+                <Typography variant="body1">{text}</Typography>
+                {/* todo: Switch this to theme.typography.fontWeightSemiBold when it's added  */}
+                <Typography variant="body2" fontWeight={600} component="span">
+                    {source}
+                </Typography>
+                {datasetExplorerLink != null && datasetExplorerLink}
+            </CardContent>
         </Card>
     );
 };
@@ -126,6 +109,22 @@ export const AttributionDocumentCard = ({
             title={title ?? MISSING_DOCUMENT_TITLE_TEXT}
             text={<BoldTextForDocumentAttribution correspondingSpans={spans} text={text} />}
             source={`Source: ${source}`}
+            datasetExplorerLink={
+                <Button
+                    href={links.document(documentIndex)}
+                    variant="outlined"
+                    color="inherit"
+                    size="small"
+                    fullWidth={false}
+                    sx={{
+                        width: 'fit-content',
+                        // this is slightly further from the thing above it in the design so it gets this extra margin
+                        // combined with gap this makes the total margin 2
+                        marginBlockStart: 1,
+                    }}>
+                    Open in Dataset Explorer
+                </Button>
+            }
         />
     );
 };
@@ -144,6 +143,7 @@ export const AttributionDocumentCardSkeleton = (): JSX.Element => {
                 </>
             }
             source={<Skeleton />}
+            datasetExplorerLink={<Skeleton />}
         />
     );
 };
