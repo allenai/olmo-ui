37b1f4f6ab219e4c9a762d3b88ec1550ff770b23
Byron Bischoff <byronb@gmail.com>
2024-10-17 12:19:15 -0700

    Updating the Navigation sidebar styling

--- src/components/assets/ChatIcon.tsx
+++ src/components/assets/ChatIcon.tsx
@@ -4,11 +4,9 @@ import { ComponentProps } from 'react';
 export const ChatIcon = (props: ComponentProps<typeof SvgIcon>): JSX.Element => {
     return (
         <SvgIcon {...props}>
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none">
+            {/* viewbox is a hack to visually move the svg */}
+            <svg width="20" height="20" viewBox="0 -4 24 24" fill="none">
                 <path
-                    id="Chat"
-                    fillRule="evenodd"
-                    clipRule="evenodd"
                     d="M0 0H20V14.8003H13.9168L8.80089 20L7.24396 18.4176L13.0047 12.5624H17.7982V2.23792H2.20183V12.5624H7.28084V14.8003H0V0Z"
                     fill="currentColor"
                 />
