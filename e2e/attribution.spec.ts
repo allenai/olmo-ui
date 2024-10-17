9156af1c2511bd1778f69fe5ab2aa2b8f94e36d0
Taylor Blanton <matthewtblanton@gmail.com>
2024-10-17 17:17:28 -0500

    add e2e test for attribution drawer stuff, show the highlight stress test in the thread history when using mocks

--- e2e/attribution.spec.ts
+++ e2e/attribution.spec.ts
@@ -15,3 +15,53 @@ test('should filter displayed documents when a span is selected', async ({ page 
         .click();
     await expect(page.getByTestId('attribution-drawer').getByText('Source')).toHaveCount(1);
 });
+
+test('should show the attribution drawer when navigating to a thread', async ({ page }) => {
+    await page.goto('/thread/msg_A8E5H1X2O3');
+    await page.waitForLoadState('networkidle');
+
+    await expect(page.getByRole('tab', { name: 'CorpusLink' })).toHaveAttribute(
+        'aria-selected',
+        'true'
+    );
+
+    await page.getByRole('tab', { name: 'Parameters' }).click();
+    await expect(page.getByRole('tab', { name: 'Parameters' })).toHaveAttribute(
+        'aria-selected',
+        'true'
+    );
+    await expect(page.getByRole('tab', { name: 'CorpusLink' })).toHaveAttribute(
+        'aria-selected',
+        'false'
+    );
+
+    await page.getByRole('button', { name: 'Thread history' }).click();
+    await page.getByRole('link', { name: 'Second existing message' }).click();
+    await page.getByRole('heading', { name: 'Thread History' }).press('Escape');
+
+    await expect(page.getByRole('tab', { name: 'CorpusLink' })).toHaveAttribute(
+        'aria-selected',
+        'true'
+    );
+    await expect(page.getByTestId('attribution-drawer').getByRole('listitem')).toHaveCount(2);
+
+    await page.getByRole('tab', { name: 'Parameters' }).click();
+
+    // Make sure attribution doesn't go away when you click on the same thread
+    await page.getByRole('button', { name: 'Thread history' }).click();
+    await page.getByRole('link', { name: 'Second existing message' }).click();
+    await page.getByRole('heading', { name: 'Thread History' }).press('Escape');
+
+    await expect(page.getByTestId('attribution-drawer').getByRole('listitem')).toHaveCount(2);
+
+    // Make sure new attributions show when you click another thread
+    await page.getByRole('button', { name: 'Thread history' }).click();
+    await page.getByRole('link', { name: 'Highlight stress test' }).click();
+    await page.getByRole('heading', { name: 'Thread History' }).press('Escape');
+
+    await expect(page.getByRole('tab', { name: 'CorpusLink' })).toHaveAttribute(
+        'aria-selected',
+        'true'
+    );
+    await expect(page.getByTestId('attribution-drawer').getByRole('listitem')).toHaveCount(1);
+});
