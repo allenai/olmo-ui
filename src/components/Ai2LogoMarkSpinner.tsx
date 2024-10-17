5d9fc988ac458da793884d39de4326579ebeb44d
Byron Bischoff <byronb@gmail.com>
2024-10-17 14:33:55 -0700

    Added loading spinner for new conversations

---
+++ src/components/Ai2LogoMarkSpinner.tsx
@@ -0,0 +1,29 @@
+import { Box } from '@mui/material';
+
+type Ai2LogoMarkSpinnerProps = Pick<React.ComponentProps<'img'>, 'height' | 'width' | 'alt'> & {
+    isAnimating: boolean;
+};
+
+export const Ai2LogoMarkSpinner = ({
+    width,
+    height,
+    alt = '',
+    isAnimating = true,
+}: Ai2LogoMarkSpinnerProps) => {
+    return (
+        <Box
+            sx={{
+                '@keyframes spin': {
+                    from: {
+                        transform: 'rotate(0deg)',
+                    },
+                    to: {
+                        transform: 'rotate(360deg)',
+                    },
+                },
+                animation: isAnimating ? 'spin 1.5s linear infinite' : undefined,
+            }}>
+            <img src="/ai2-monogram.svg" alt={alt} width={width} height={height} />
+        </Box>
+    );
+};
