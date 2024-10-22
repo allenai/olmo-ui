3be4ae733f043bbf2312d43a9bbc36c9c11203cc
Byron Bischoff <byronb@gmail.com>
2024-10-22 12:40:05 -0700

    Remove unused static media queries

--- src/utils/container-query-utils.tsx
+++ src/utils/container-query-utils.tsx
@@ -1,14 +1,6 @@
-import { Theme } from '@mui/material';
-
 // not smart enough to check if unit, just convert base number to `Zpx`
 const addPxIfNumber = (size: number | string): string =>
     typeof size === 'number' ? `${size}px` : size;
-
-export const biggerContainerQuery = (theme: Theme) =>
-    `@container (min-width: ${theme.breakpoints.values.md}px)`;
-
-export const smallerContainerQuery = (theme: Theme) =>
-    `@container (max-width: ${theme.breakpoints.values.md}px)`;
 
 export const minContainerQuery = (size: number | string) =>
     `@container (min-width: ${addPxIfNumber(size)})`;
