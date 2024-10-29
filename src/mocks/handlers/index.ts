b342676c5c265e7f104fa05ab3904451a5c00cee
Huy Tran <84343285+huytr1995@users.noreply.github.com>
2024-10-29 10:40:02 -0700

    [OEUI-320] Fix Navigating to a document from the dataset explorer shows an error (#675)
    ## Description
    
    Notice when navigating to a specific document it shows an error. This PR
    fixes it.
    
    ## Reviewer Instructions
    
    Notice that the error is cause if the total from meta property is > 1
    then it throws error. Previously we were using es but now we switch to
    infini_gram so this can be the case.
    
    ## Testing Plan
    
    Verify UI works. If CI passes we are good.
    
    ## Output / Screenshots
    
    
    https://github.com/user-attachments/assets/9e83de4d-2967-4ab9-b290-8052cf9e24a5
    
    
    ### A11y
    
    N/A

--- src/mocks/handlers/index.ts
+++ src/mocks/handlers/index.ts
@@ -40,16 +40,24 @@ export const handlers = [
     http.get(`${process.env.DOLMA_API_URL}/v1/search`, ({ request }) => {
         const searchParams = new URL(request.url).searchParams;
         const query = searchParams.get('query');
-        const id = searchParams.get('id');
         if (query === 'Seattle') {
-            if (id === 'a718be1486e24cbb7e0aee7d0bef8442') {
-                return HttpResponse.json(datasetDocumentResponse);
-            }
             return HttpResponse.json(datasetSearchResponse);
         }
 
         return passthrough();
     }),
+
+    http.get(
+        `${process.env.DOLMA_API_URL}/v1/document/a718be1486e24cbb7e0aee7d0bef8442`,
+        ({ request }) => {
+            const searchParams = new URL(request.url).searchParams;
+            const query = searchParams.get('query');
+            if (query === 'Seattle') {
+                return HttpResponse.json(datasetDocumentResponse);
+            }
+            return HttpResponse.json(datasetDocumentResponse);
+        }
+    ),
 ];
 
 const fakeModelsResponse: ModelList = [
