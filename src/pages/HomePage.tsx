97551bfe224202ef98183c604130722c806211d2
Taylor Blanton <matthewtblanton@gmail.com>
2024-11-05 15:35:01 -0600

    added a basic error page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,17 @@
+import { Button, Stack, Typography } from '@mui/material';
+
 import { AppLayout } from '@/components/AppLayout';
+import { links } from '@/Links';
 
 export const HomePage = () => {
-    return <AppLayout>landing page</AppLayout>;
+    return (
+        <AppLayout>
+            <Stack direction="column" gap={2}>
+                <Typography variant="h1">Showcasing truly open AI models</Typography>
+                <Button href={links.playground} variant="contained">
+                    Start testing our models now
+                </Button>
+            </Stack>
+        </AppLayout>
+    );
 };
