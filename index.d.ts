3ac125fcf1b7f1370e47b8f272a787af1a8b0526
Taylor Blanton <matthewtblanton@gmail.com>
2024-11-11 18:15:09 -0600

    make heap check to make sure it can load before it does

--- index.d.ts
+++ index.d.ts
@@ -3,3 +3,20 @@ declare module '*.svg' {
     const content: any;
     export default content;
 }
+
+interface Window {
+    // taken from https://developers.heap.io/reference/client-side-apis-overview#typescript-type-definitions
+    heap: {
+        track: (event: string, properties?: object) => void;
+        identify: (identity: string) => void;
+        resetIdentity: () => void;
+        addUserProperties: (properties: object) => void;
+        addEventProperties: (properties: object) => void;
+        removeEventProperty: (property: string) => void;
+        clearEventProperties: () => void;
+        appid: string;
+        userId: string;
+        identity: string | null;
+        config: unknown;
+    };
+}
