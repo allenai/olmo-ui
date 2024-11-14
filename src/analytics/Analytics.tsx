9b01286b3415b133cf07b96f7ae53e42799c6cba
Taylor Blanton <matthewtblanton@gmail.com>
2024-11-14 11:21:19 -0600

    remove console.log

--- src/analytics/Analytics.tsx
+++ src/analytics/Analytics.tsx
@@ -18,7 +18,6 @@ const HeapSetup = (): ReactNode => (
     <Helmet>
         <script type="text/javascript" key="heap-setup">
             {`
-            console.log(${process.env.HEAP_ANALYTICS_ID})
             window.heapReadyCb=window.heapReadyCb||[],window.heap=window.heap||[],heap.load=function(e,t){window.heap.envId=e,window.heap.clientConfig=t=t||{},window.heap.clientConfig.shouldFetchServerConfig=!1;var a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src="https://cdn.us.heap-api.com/config/"+e+"/heap_config.js";var r=document.getElementsByTagName("script")[0];r.parentNode.insertBefore(a,r);var n=["init","startTracking","stopTracking","track","resetIdentity","identify","getSessionId","getUserId","getIdentity","addUserProperties","addEventProperties","removeEventProperty","clearEventProperties","addAccountProperties","addAdapter","addTransformer","addTransformerFn","onReady","addPageviewProperties","removePageviewProperty","clearPageviewProperties","trackPageview"],i=function(e){return function(){var t=Array.prototype.slice.call(arguments,0);window.heapReadyCb.push({name:e,fn:function(){heap[e]&&heap[e].apply(heap,t)}})}};for(var p=0;p<n.length;p++)heap[n[p]]=i(n[p])};
             window.Osano('onAnalytics', () => {
                 // Osano doesn't automatically handle Heap's scripts so we need to handle it manually
