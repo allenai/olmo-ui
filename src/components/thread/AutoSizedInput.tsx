e29cfba0a23536276960733d9108ca71195c8677
Taylor Blanton <matthewtblanton@gmail.com>
2024-11-19 14:59:36 -0600

    it's working without MUI

--- src/components/thread/AutoSizedInput.tsx
+++ src/components/thread/AutoSizedInput.tsx
@@ -1,11 +1,11 @@
-import { Box, Input, useTheme } from '@mui/material';
+import { Box, Input } from '@mui/material';
 import clsx from 'clsx';
 import { useState } from 'react';
 
 // taken from https://css-tricks.com/the-cleanest-trick-for-autogrowing-textareas/
 export const AutoSizedInput = () => {
     const [value, setValue] = useState('');
-    const theme = useTheme();
+    // const theme = useTheme();
 
     return (
         <Box
@@ -14,12 +14,16 @@ export const AutoSizedInput = () => {
                 display: 'grid',
             }}>
             <textarea
+                rows={1}
+                className={clsx('MuiInput-input', 'MuiInput-multiline')}
                 style={{
                     gridArea: '1 / 1 / 2 / 2',
                     height: 'unset',
                     resize: 'none',
                     maxWidth: '100%',
                     maxHeight: '6lh',
+                    lineHeight: 'inherit',
+                    fontSize: 'inherit',
                 }}
                 value={value}
                 onChange={(e) => {
