a31b797542a3905a745e61487361f74ff249b0e2
Taylor Blanton <matthewtblanton@gmail.com>
2024-11-20 14:18:13 -0600

    pass all the menu item props (esp autoFocus) to the custom menuitem

--- src/components/OlmoAppBar/ColorModeSelection.tsx
+++ src/components/OlmoAppBar/ColorModeSelection.tsx
@@ -1,29 +1,33 @@
 import { Check } from '@mui/icons-material';
 import TVOutlinedIcon from '@mui/icons-material/TvOutlined';
-import { Box, ListItemIcon, Menu, MenuItem } from '@mui/material';
-import { MouseEventHandler, ReactNode, useState } from 'react';
+import { Box, ListItemIcon, Menu, MenuItem, MenuItemProps } from '@mui/material';
+import { ReactNode, useState } from 'react';
 
 import { ColorPreference, useColorMode } from '../ColorModeProvider';
 import { NavigationLink } from './NavigationLink';
 
-interface ColorModeSelectionMenuItemProps {
+// We need the autoFocus prop from MenuItemProps so the MenuList can automatically focus the right item
+// Including all the props just in case we need something else
+interface ColorModeSelectionMenuItemProps extends MenuItemProps {
     title: string;
     mode: ColorPreference;
-    onClick: MouseEventHandler;
-    selected?: boolean;
 }
 
 const ColorModeSelectionMenuItem = ({
     title,
     mode,
+    onClick,
+    ...menuItemProps
 }: ColorModeSelectionMenuItemProps): ReactNode => {
     const [colorMode, setColorMode] = useColorMode();
     const isSelected = mode === colorMode;
 
     return (
         <MenuItem
-            onClick={() => {
+            {...menuItemProps}
+            onClick={(e) => {
                 setColorMode(mode);
+                onClick?.(e);
             }}>
             <Box flexGrow={1}>{title}</Box>
             {isSelected ? (
@@ -38,6 +42,8 @@ const ColorModeSelectionMenuItem = ({
         </MenuItem>
     );
 };
+
+ColorModeSelectionMenuItem.displayName = 'ColorModeSelectionMenuItem';
 
 export const ColorModeSelection = () => {
     const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
@@ -96,9 +102,7 @@ export const ColorModeSelection = () => {
                         },
                     },
                 }}>
-                <MenuItem onClick={() => {}}>foo</MenuItem>
-                <MenuItem>bar</MenuItem>
-                {/* <ColorModeSelectionMenuItem
+                <ColorModeSelectionMenuItem
                     title="System"
                     mode="system"
                     onClick={handleMenuItemClick}
@@ -112,7 +116,7 @@ export const ColorModeSelection = () => {
                     title="Dark"
                     mode="dark"
                     onClick={handleMenuItemClick}
-                /> */}
+                />
             </Menu>
         </>
     );
