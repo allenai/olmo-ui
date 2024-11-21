dba9dab114113ed884213ac36f080dfc048e6f23
Taylor Blanton <matthewtblanton@gmail.com>
2024-11-20 19:17:56 -0600

    fix e2e tests, make the buttons only show if we're on a selected thread page

--- e2e/olmo.spec.ts
+++ e2e/olmo.spec.ts
@@ -17,9 +17,9 @@ test('can send prompt in Olmo Playground', async ({ page, isAnonymousTest }) => 
     }).toBeVisible();
 
     if (isAnonymousTest) {
-        await expect(page.getByRole('button', { name: 'Share' })).toHaveCount(0);
+        await expect(page.getByRole('button', { name: 'Share this thread' })).toBeDisabled();
     } else {
-        await expect(page.getByRole('button', { name: 'Share' })).toBeVisible();
+        await expect(page.getByRole('button', { name: 'Share this thread' })).toBeEnabled();
     }
 
     await expect(
@@ -43,9 +43,9 @@ test('can send prompt in Olmo Playground', async ({ page, isAnonymousTest }) => 
     }).toBeVisible();
 
     if (isAnonymousTest) {
-        await expect(page.getByRole('button', { name: 'Share' })).toHaveCount(0);
+        await expect(page.getByRole('button', { name: 'Share this thread' })).toBeDisabled();
     } else {
-        await expect(page.getByRole('button', { name: 'Share' })).toBeVisible();
+        await expect(page.getByRole('button', { name: 'Share this thread' })).toBeEnabled();
     }
 
     await expect(
--- src/components/thread/ThreadPageControls/ThreadPageControls.tsx
+++ src/components/thread/ThreadPageControls/ThreadPageControls.tsx
@@ -1,22 +1,12 @@
-import { useMatch } from 'react-router-dom';
-
-import { links } from '@/Links';
-
 import { NewThreadIconButton } from './NewThreadIconButton';
 import { ParameterIconButton } from './ParameterIconButton';
 import { ShareThreadIconButton } from './ShareThreadIconButton';
 
 export const ThreadPageControls = (): React.ReactNode => {
-    const isOnThreadPage = useMatch({ path: links.thread(':id') });
-
     return (
         <>
-            {isOnThreadPage && (
-                <>
-                    <ShareThreadIconButton />
-                    <ParameterIconButton />
-                </>
-            )}
+            <ShareThreadIconButton />
+            <ParameterIconButton />
             <NewThreadIconButton />
         </>
     );
