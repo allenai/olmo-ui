c2f3e02df0befb6827b59acd11dfbf2d49383f24
Taylor Blanton <matthewtblanton@gmail.com>
2024-11-21 15:17:37 -0600

    add a missing model test

--- src/pages/playgroundLoader.spec.ts
+++ src/pages/playgroundLoader.spec.ts
@@ -13,4 +13,15 @@ describe('root playground loader', () => {
 
         expect(appContext.getState().selectedModel?.id).toEqual('OLMo-peteish-dpo-preview');
     });
+
+    it("should not set a model if the model query param doesn't match a real model", async () => {
+        expect(appContext.getState().selectedModel?.id).not.toEqual('OLMo-peteish-dpo-preview');
+
+        await playgroundLoader({
+            params: { id: undefined },
+            request: new Request(new URL('http://localhost:8080/?model=fake-model')),
+        });
+
+        expect(appContext.getState().selectedModel?.id).toEqual('tulu2');
+    });
 });
