b60579e7c95439ac308f311a3f761b7812759ee1
Taylor Blanton <matthewtblanton@gmail.com>
2024-11-21 14:26:49 -0600

    add a not condition to make sure we're not fake-passing the test

--- src/pages/playgroundLoader.spec.ts
+++ src/pages/playgroundLoader.spec.ts
@@ -4,6 +4,8 @@ import { playgroundLoader } from './UIRefreshThreadPage';
 
 describe('root playground loader', () => {
     it('should set the model from a model query param', async () => {
+        expect(appContext.getState().selectedModel?.id).not.toEqual('OLMo-peteish-dpo-preview');
+
         await playgroundLoader({
             params: { id: undefined },
             request: new Request(new URL('http://localhost:8080/?model=OLMo-peteish-dpo-preview')),
