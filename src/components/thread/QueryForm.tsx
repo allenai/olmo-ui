c1e246840b0500f9d58196a429142fcdbd0720a7
Byron Bischoff <byronb@gmail.com>
2024-11-21 08:39:38 -0800

    Tulu3 placeholder text

--- src/components/thread/QueryForm.tsx
+++ src/components/thread/QueryForm.tsx
@@ -216,6 +216,8 @@ export const QueryForm = (): JSX.Element => {
         }
     };
 
+    const placeholderText = lastMessageId ? 'Reply to Tülu3' : 'Message Tülu3';
+
     return (
         <Box marginBlockStart="auto" width={1} paddingInline={2}>
             <FormContainer formContext={formContext} onSuccess={handleSubmit}>
@@ -252,8 +254,8 @@ export const QueryForm = (): JSX.Element => {
                                 value={value}
                                 ref={ref}
                                 onKeyDown={handleKeyDown}
-                                aria-label="Prompt"
-                                placeholder="Prompt"
+                                aria-label={placeholderText}
+                                placeholder={placeholderText}
                                 endAdornment={
                                     <SubmitPauseAdornment
                                         canPause={canPauseThread}
