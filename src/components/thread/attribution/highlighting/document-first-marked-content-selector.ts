769820dc63c1a56780bb06d6e6c0298186e8b9cf
Taylor Blanton <matthewtblanton@gmail.com>
2024-12-16 13:23:33 -0600

    made highlights work properly in document-first selection

--- src/components/thread/attribution/highlighting/document-first-marked-content-selector.ts
+++ src/components/thread/attribution/highlighting/document-first-marked-content-selector.ts
@@ -1,34 +1,49 @@
+import { TopLevelAttributionSpan } from '@/api/AttributionClient';
 import { AppContextState } from '@/AppContext';
 import { messageAttributionsSelector } from '@/slices/attribution/attribution-selectors';
 
 import { createSpanReplacementRegex } from '../span-replacement-regex';
+import { escapeBraces } from './escape-braces';
+import { removeMarkdownCharactersFromStartAndEndOfSpan } from './escape-markdown-in-span';
 import { getAttributionHighlightString } from './get-attribution-highlight-string';
 
+const correspondingSpansSelector =
+    (documentIndex?: string | null) =>
+    (state: AppContextState): [string, TopLevelAttributionSpan | undefined][] => {
+        const message = messageAttributionsSelector(state);
+        if (documentIndex == null || message == null) {
+            return [];
+        }
+
+        const correspondingSpanIds = message.documents[documentIndex]?.corresponding_spans ?? [];
+
+        return correspondingSpanIds.map((correspondingSpanId) => [
+            correspondingSpanId.toString(),
+            message.spans[correspondingSpanId],
+        ]);
+    };
+
 const selectedCorrespondingSpansSelector = (state: AppContextState) => {
-    if (state.attribution.selectedDocumentIndex == null) {
-        return [];
-    }
-    const message = messageAttributionsSelector(state);
-
-    if (message == null) {
-        return [];
-    }
-
-    const correspondingSpans =
-        message.documents[state.attribution.selectedDocumentIndex]?.corresponding_spans ?? [];
-
-    return correspondingSpans.map((correspondingSpan) => message.spans[correspondingSpan]);
+    return correspondingSpansSelector(state.attribution.selectedDocumentIndex)(state);
 };
+
 const previewCorrespondingSpansSelector = (state: AppContextState) => {
-    const message = messageAttributionsSelector(state);
-    if (state.attribution.previewDocumentIndex == null || message == null) {
-        return [];
+    return correspondingSpansSelector(state.attribution.previewDocumentIndex)(state);
+};
+
+const formatSpan = (variant: 'selected' | 'preview', spanKey: string, spanText?: string) => {
+    if (!spanText) {
+        return {};
     }
 
-    const correspondingSpans =
-        message.documents[state.attribution.previewDocumentIndex]?.corresponding_spans ?? [];
+    const escapedSpanText = removeMarkdownCharactersFromStartAndEndOfSpan(spanText);
 
-    return correspondingSpans.map((correspondingSpan) => message.spans[correspondingSpan]);
+    const spanReplacementRegex = createSpanReplacementRegex(escapedSpanText);
+
+    const spanDisplayText = escapeBraces(escapedSpanText);
+    const attributionHighlight = getAttributionHighlightString(spanKey, spanDisplayText, variant);
+
+    return { spanReplacementRegex, attributionHighlight };
 };
 
 export const documentFirstMarkedContentSelector =
@@ -40,11 +55,17 @@ export const documentFirstMarkedContentSelector =
 
         const selectedSpans = selectedCorrespondingSpansSelector(state);
 
-        Object.entries(selectedSpans).forEach(([spanKey, span]) => {
-            if (span?.text) {
+        selectedSpans.forEach(([spanKey, span]) => {
+            const { spanReplacementRegex, attributionHighlight } = formatSpan(
+                'selected',
+                spanKey,
+                span?.text
+            );
+
+            if (spanReplacementRegex != null && attributionHighlight != null) {
                 contentWithMarks = contentWithMarks.replaceAll(
-                    createSpanReplacementRegex(span.text),
-                    getAttributionHighlightString(spanKey, span.text, 'selected')
+                    spanReplacementRegex,
+                    attributionHighlight
                 );
             }
         });
@@ -54,11 +75,17 @@ export const documentFirstMarkedContentSelector =
             (previewSpan) => !selectedSpans.includes(previewSpan)
         );
 
-        Object.entries(previewSpansThatArentSelected).forEach(([spanKey, span]) => {
-            if (span?.text) {
+        previewSpansThatArentSelected.forEach(([spanKey, span]) => {
+            const { spanReplacementRegex, attributionHighlight } = formatSpan(
+                'preview',
+                spanKey,
+                span?.text
+            );
+
+            if (spanReplacementRegex != null && attributionHighlight != null) {
                 contentWithMarks = contentWithMarks.replaceAll(
-                    createSpanReplacementRegex(span.text),
-                    getAttributionHighlightString(spanKey, span.text, 'preview')
+                    spanReplacementRegex,
+                    attributionHighlight
                 );
             }
         });
