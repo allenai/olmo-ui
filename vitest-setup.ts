8c793c45ecf895c75df2c162a735619fd9e51d13
Taylor Blanton <matthewtblanton@gmail.com>
2025-01-15 12:32:14 -0600

    tell vitest to load variables in from .env.test

--- vitest-setup.ts
+++ vitest-setup.ts
@@ -9,8 +9,6 @@ vi.stubEnv('LLMX_API_URL', 'http://localhost:8080');
 vi.stubEnv('DOLMA_API_URL', '/api');
 vi.stubEnv('AUTH0_CLIENT_ID', 'client_id');
 vi.stubEnv('AUTH0_DOMAIN', 'domain');
-vi.stubEnv('NODE_ENV', 'test');
-vi.stubEnv('IS_DATASET_EXPLORER_ENABLED', 'true');
 
 let server: SetupServerApi;
 
