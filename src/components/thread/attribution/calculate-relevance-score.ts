23ee1a4ef86e8ef5e39ca13fbac5d91c9ed3aee6
yensung <george129.chen@gmail.com>
2025-01-17 15:51:46 -0800

    Update comment

--- src/components/thread/attribution/calculate-relevance-score.ts
+++ src/components/thread/attribution/calculate-relevance-score.ts
@@ -3,7 +3,7 @@ export const calculateRelevanceScore = (relevanceScore: number, messageLength: n
         return 0.0;
     }
 
-    // 0.125 is a hyperparam heuristically determined by running distrib_of_score_span.py in infinigram-api
+    // 0.18 is a hyperparam heuristically determined by running distrib_of_score_span.py in infinigram-api
     const score = relevanceScore / (messageLength * 0.18);
     return Math.min(Math.max(score, 0.0), 1.0);
 };
