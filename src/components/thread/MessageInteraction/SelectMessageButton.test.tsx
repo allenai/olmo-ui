abccecbba5d1e53ca50b40cfee34b20db116bbfe
Taylor Blanton <matthewtblanton@gmail.com>
2025-01-23 15:05:56 -0600

    add tests for selectmessagebutton states

---
+++ src/components/thread/MessageInteraction/SelectMessageButton.test.tsx
@@ -0,0 +1,64 @@
+import { render, screen } from '@test-utils';
+import { ComponentProps } from 'react';
+
+import * as AppContext from '@/AppContext';
+import { FakeAppContextProvider, useFakeAppContext } from '@/utils/FakeAppContext';
+
+import { SelectMessageButton } from './SelectMessageButton';
+
+describe('SelectMessageButton', () => {
+    it('should show "Match training text" when selected and the drawer is closed', () => {
+        vi.spyOn(AppContext, 'useAppContext').mockImplementation(useFakeAppContext);
+
+        const initialState = {
+            currentOpenDrawer: null,
+            attribution: {
+                selectedMessageId: 'message-1',
+            },
+        } satisfies ComponentProps<typeof FakeAppContextProvider>['initialState'];
+
+        render(
+            <FakeAppContextProvider initialState={initialState}>
+                <SelectMessageButton messageId="message-1" />
+            </FakeAppContextProvider>
+        );
+
+        expect(screen.getByRole('button', { name: 'Match training text' })).toBeInTheDocument();
+    });
+
+    it('should show "Hide training text" when selected and the drawer is open', () => {
+        vi.spyOn(AppContext, 'useAppContext').mockImplementation(useFakeAppContext);
+
+        const initialState = {
+            currentOpenDrawer: 'attribution',
+            attribution: {
+                selectedMessageId: 'message-1',
+            },
+        } satisfies ComponentProps<typeof FakeAppContextProvider>['initialState'];
+
+        render(
+            <FakeAppContextProvider initialState={initialState}>
+                <SelectMessageButton messageId="message-1" />
+            </FakeAppContextProvider>
+        );
+
+        expect(screen.getByRole('button', { name: 'Hide training text' })).toBeInTheDocument();
+    });
+
+    it('should show "Match training text" when not selected and the drawer is open', () => {
+        vi.spyOn(AppContext, 'useAppContext').mockImplementation(useFakeAppContext);
+
+        const initialState = {
+            currentOpenDrawer: 'attribution',
+            attribution: {},
+        } satisfies ComponentProps<typeof FakeAppContextProvider>['initialState'];
+
+        render(
+            <FakeAppContextProvider initialState={initialState}>
+                <SelectMessageButton messageId="message-1" />
+            </FakeAppContextProvider>
+        );
+
+        expect(screen.getByRole('button', { name: 'Match training text' })).toBeInTheDocument();
+    });
+});
