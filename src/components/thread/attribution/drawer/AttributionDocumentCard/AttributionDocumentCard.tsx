fcad2f27d4361bc670eefe6577e60452deb90150
Jiacheng Liu <liujch1998@gmail.com>
2025-01-30 14:00:30 -0800

    Update the source field of doc cards to match design (#868)
    ## Description
    
    Closes https://github.com/allenai/playground-issues-repo/issues/150
    
    ## Reviewer Instructions
    
    Anything a review needs to know about this PR to help them give better
    feedback. Are there specific questions you have? Are there areas you are
    unsure of? Does this come from or relate to another part of the
    codebase?
    
    ## Testing Plan
    
    Describe what you did to test the change.
    
    ## Output / Screenshots
    
    ### Before
    
    What did it look like before?
    
    ### After
    
    Light mode:
    <img width="1396" alt="Screenshot 2025-01-29 at 15 49 55"
    src="https://github.com/user-attachments/assets/dd6ef4eb-72d6-46c4-a213-9a327769fdfe"
    />
    
    Dark mode:
    <img width="1396" alt="Screenshot 2025-01-29 at 15 49 44"
    src="https://github.com/user-attachments/assets/3b32df20-fa7d-458d-b4e1-326cffaf60c1"
    />
    
    
    ### A11y
    
    How do keyboard navigation and screen readers work with this PR?

--- src/components/thread/attribution/drawer/AttributionDocumentCard/AttributionDocumentCard.tsx
+++ src/components/thread/attribution/drawer/AttributionDocumentCard/AttributionDocumentCard.tsx
@@ -10,6 +10,89 @@ import { links } from '@/Links';
 import { AttributionBucket } from '../../calculate-relevance-score';
 import { AttributionDocumentCardSnippets } from './AttributionDocumentCardSnippets';
 
+const deduceUsageFromSource = (source: string): string => {
+    switch (source) {
+        case 'dclm-hero-run-fasttext_for_HF':
+        case 'dclm':
+        case 'arxiv':
+        case 'algebraic-stack':
+        case 'open-web-math':
+        case 'pes2o':
+        case 'starcoder':
+        case 'wiki':
+            return 'Pre-training';
+        case 'dolmino':
+            return 'Mid-training';
+        case 'tulu-3-sft-olmo-2-mixture':
+            return 'Post-training (SFT)';
+        case 'olmo-2-1124-13b-preference-mix':
+            return 'Post-training (DPO)';
+        case 'RLVR-GSM-MATH-IF-Mixed-Constraints':
+            return 'Post-training (RLVR)';
+        default:
+            return '';
+    }
+};
+
+const prettifySource = (source: string): ReactNode => {
+    let displayName = '';
+    let url = '';
+    let secondaryName = '';
+    switch (source) {
+        case 'dclm-hero-run-fasttext_for_HF':
+        case 'dclm':
+            displayName = 'olmo-mix-1124';
+            url = 'https://huggingface.co/datasets/allenai/olmo-mix-1124';
+            secondaryName = 'web corpus (DCLM)';
+            break;
+        case 'arxiv':
+        case 'algebraic-stack':
+        case 'open-web-math':
+        case 'pes2o':
+        case 'starcoder':
+        case 'wiki':
+            displayName = 'olmo-mix-1124';
+            url = 'https://huggingface.co/datasets/allenai/olmo-mix-1124';
+            secondaryName = source;
+            break;
+        case 'dolmino':
+            displayName = 'dolmino-mix-1124';
+            url = 'https://huggingface.co/datasets/allenai/dolmino-mix-1124';
+            break;
+        case 'tulu-3-sft-olmo-2-mixture':
+            displayName = 'tulu-3-sft-olmo-2-mixture';
+            url = 'https://huggingface.co/datasets/allenai/tulu-3-sft-olmo-2-mixture';
+            break;
+        case 'olmo-2-1124-13b-preference-mix':
+            displayName = 'olmo-2-1124-13b-preference-mix';
+            url = 'https://huggingface.co/datasets/allenai/olmo-2-1124-13b-preference-mix';
+            break;
+        case 'RLVR-GSM-MATH-IF-Mixed-Constraints':
+            displayName = 'RLVR-GSM-MATH-IF-Mixed-Constraints';
+            url = 'https://huggingface.co/datasets/allenai/RLVR-GSM-MATH-IF-Mixed-Constraints';
+            break;
+        default:
+            return <></>;
+    }
+
+    return (
+        <>
+            <Link href={url} target="_blank" fontWeight={600} color="primary" underline="always">
+                {displayName}
+            </Link>
+            {secondaryName !== '' && (
+                <Typography
+                    variant="body2"
+                    component="span"
+                    sx={{ color: (theme) => theme.palette.text.secondary }}>
+                    {' > '}
+                    {secondaryName}
+                </Typography>
+            )}
+        </>
+    );
+};
+
 interface AttributionDocumentCardActionWrapperProps extends PropsWithChildren {}
 
 interface AttributionDocumentCardBaseProps extends AttributionDocumentCardActionWrapperProps {
@@ -66,9 +149,12 @@ const AttributionDocumentCardBase = ({
             <CardContent component={Stack} direction="column" gap={1}>
                 <div>
                     <Typography variant="body2" fontWeight={600} component="span">
-                        Source:{' '}
+                        {deduceUsageFromSource(source)} document from:
+                        <br />
                     </Typography>
-                    {source}
+                    <Typography variant="body2" component="span">
+                        {prettifySource(source)}
+                    </Typography>
                 </div>
                 <Typography variant="body1" component="span">
                     {snippets}
