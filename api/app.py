5900497ca4dfb2868b48d2149e53fd3384d8bde7
Taylor Blanton <matthewtblanton@gmail.com>
2025-01-31 19:00:05 -0600

    lets do it this way actually

--- api/app.py
+++ api/app.py
@@ -1,11 +1,12 @@
-from dolma_search import glog, error, custom
-from dolma_search.api import v1
-from werkzeug import exceptions
+import logging
+import os
+
+import flask
 from werkzeug.middleware import proxy_fix
 
-import os
-import logging
-import flask
+from dolma_search import custom, error, glog
+from dolma_search.api import v1
+
 
 def create_app() -> proxy_fix.ProxyFix:
     # If LOG_FORMAT is "google:json" emit log message as JSON in a format Google Cloud can parse.
@@ -14,14 +15,13 @@ def create_app() -> proxy_fix.ProxyFix:
     level = os.environ.get("LOG_LEVEL", default=logging.INFO)
     logging.basicConfig(level=level, handlers=handlers)
 
-    app = flask.Flask("dolma_search", static_url_path="/api/static")
+    app = flask.Flask("dolma_search", static_url_path="/static")
     app.json = custom.JSONProvider(app)
-    app.get("/api/health")(lambda: ("", 204))
-    app.register_blueprint(v1.Server(), url_prefix="/api/v1")
+    app.get("/health")(lambda: ("", 204))
+    app.register_blueprint(v1.Server(), url_prefix="/v1")
     app.register_error_handler(Exception, error.handle)
 
     # Use the X-Forwarded-* headers to set the request IP, host and port. Technically there
     # are two reverse proxies in deployed environments, but we "hide" the reverse proxy deployed
     # as a sibling of the API by forwarding the X-Forwarded-* headers rather than chaining them.
     return proxy_fix.ProxyFix(app, x_for=1, x_proto=1, x_host=1, x_port=1)
-
