3bc37c81c93d69e83a6bcde7dc27f56d3e78ffb0
yensung <george129.chen@gmail.com>
2025-02-10 10:47:59 -0800

    Move download link

--- src/components/thread/LegalNotice/OlmoELegalNotice.tsx
+++ src/components/thread/LegalNotice/OlmoELegalNotice.tsx
@@ -1,12 +1,13 @@
 import { ReactNode } from 'react';
 
 import { TermAndConditionsLink } from '@/components/TermsAndConditionsLink';
+import { links } from '@/Links';
 
-export const OlmoELegalNotice = (): ReactNode => {
+export const OlmoeLegalNotice = (): ReactNode => {
     return (
         <>
             Run OLMoE locally and privately on your iOS device!{' '}
-            <TermAndConditionsLink link="https://apps.apple.com/app/id6738533815">
+            <TermAndConditionsLink link={links.olmoeAppStoreDownload}>
                 Download Ai2 OLMoE on the App Store
             </TermAndConditionsLink>{' '}
             to try it out.{' '}
