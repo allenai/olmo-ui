10fcf671b411df5a1bafd9d3af503b9c050217d1
yensung <george129.chen@gmail.com>
2025-03-03 10:08:35 -0800

    Add image modal

---
+++ src/components/thread/PointResponseMessage/PointPictureModal.tsx
@@ -0,0 +1,38 @@
+import { DialogContent } from '@mui/material';
+
+import {
+    StandardDialogCloseButton,
+    StandardDialogTitle,
+    StandardModal,
+} from '@/components/StandardModal';
+
+interface PointPictureModalProps {
+    open: boolean;
+    closeModal: () => void;
+    children?: React.ReactNode;
+}
+
+export const PointPictureModal = ({
+    open,
+    closeModal: handleClose,
+    children,
+}: PointPictureModalProps) => {
+    return (
+        <StandardModal open={open} onClose={handleClose} data-testid="about-attribution-modal">
+            <StandardDialogTitle variant="h4">
+                <StandardDialogCloseButton onClick={handleClose} />
+            </StandardDialogTitle>
+            <DialogContent
+                sx={{
+                    display: 'grid',
+                    gridTemplate: 'auto / auto',
+                    gridTemplateAreas: '"combined"',
+                    width: 'fit-content',
+                    height: 'fit-content',
+                    maxWidth: '100%',
+                }}>
+                {children}
+            </DialogContent>
+        </StandardModal>
+    );
+};
