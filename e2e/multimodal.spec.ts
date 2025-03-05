4e0e15c98b80272b0800912212ddfa69e9653211
Taylor Blanton <matthewtblanton@gmail.com>
2025-03-05 11:51:02 -0600

    add e2e test for files going away when changing to a non-mm model

--- e2e/multimodal.spec.ts
+++ e2e/multimodal.spec.ts
@@ -2,10 +2,13 @@ import path from 'path';
 
 import { expect, test } from './playwright-utils';
 
+test.describe.configure({ mode: 'parallel' });
+
 test('can send a multimodal prompt', async ({ page }) => {
     await page.goto('/');
     await page.getByRole('combobox', { name: 'Model:' }).click();
     await page.getByRole('option', { name: 'Molmo' }).click();
+
     await page
         .getByLabel('Upload file')
         .setInputFiles(path.join(__dirname, 'test-files', 'molmo-boats.png'));
@@ -13,4 +16,22 @@ test('can send a multimodal prompt', async ({ page }) => {
     await page.getByRole('textbox', { name: /^Message*/ }).press('Enter');
 
     await expect(page.getByText('Counting the the boats shows a total of 37.')).toBeVisible();
+});
+
+test("removes files when switching to a modal that doesn't allow them", async ({ page }) => {
+    await page.goto('/');
+
+    await page.getByRole('combobox', { name: 'Model:' }).click();
+    await page.getByRole('option', { name: 'Molmo' }).click();
+
+    await page
+        .getByLabel('Upload file')
+        .setInputFiles(path.join(__dirname, 'test-files', 'molmo-boats.png'));
+
+    await expect(page.getByRole('img', { name: 'User file molmo-boats.png' })).toBeVisible();
+
+    await page.getByRole('combobox', { name: 'Model:' }).click();
+    await page.getByRole('option', { name: 'Tulu2.5' }).click();
+
+    await expect(page.getByRole('img', { name: 'User file molmo-boats.png' })).not.toBeAttached();
 });
