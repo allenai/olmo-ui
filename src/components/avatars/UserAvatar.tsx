5a93e9df6f4f986803c264ab252104a0b6c72dcc
yensung <george129.chen@gmail.com>
2025-03-17 15:03:37 -0700

    Show profile picture in avatar

--- src/components/avatars/UserAvatar.tsx
+++ src/components/avatars/UserAvatar.tsx
@@ -1,9 +1,31 @@
 import { Theme } from '@mui/material';
 
+import { useUserAuthInfo } from '@/api/auth/auth-loaders';
+import { useAppContext } from '@/AppContext';
+
 import defaultUserAvatarURL from '../assets/user.svg';
 import { ChatAvatar } from './ChatAvatar';
 
 export const UserAvatar = () => {
+    const { isAuthenticated } = useUserAuthInfo();
+    const userEmail = useAppContext((state) => state.userInfo?.email);
+    const userPicture = useAppContext((state) => state.userInfo?.pictureLink);
+
+    if (isAuthenticated && userPicture) {
+        return (
+            <ChatAvatar
+                alt={userEmail}
+                src={userPicture}
+                sx={{
+                    padding: 0,
+                    border: 'none',
+                    '> img': {
+                        borderRadius: '50%',
+                    },
+                }}
+            />
+        );
+    }
     return (
         <ChatAvatar
             src={defaultUserAvatarURL}
