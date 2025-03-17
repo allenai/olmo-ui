0ca2ad77ba5b05dcec57112f24021d81e381574e
yensung <george129.chen@gmail.com>
2025-03-17 16:40:41 -0700

    Add box shadow

--- src/components/thread/ThreadPageControls/AvatarMenu.tsx
+++ src/components/thread/ThreadPageControls/AvatarMenu.tsx
@@ -25,12 +25,12 @@ export const AvatarMenu = ({ anchorEl, placement, sx }: AvatarMenuProps) => {
                 <Box
                     sx={(theme) => ({
                         p: 2.5,
-                        borderRadius: '16px',
+                        borderRadius: 4,
                         bgcolor:
                             theme.palette.mode === 'light'
                                 ? theme.palette.background.default
                                 : theme.palette.background.drawer.primary,
-                        boxShadow: `0px 4px 60px 0px alpha(${(theme.palette.common.black, 0.15)})`,
+                        boxShadow: `0px 4px 60px 0px rgba(0, 0, 0, 0.15)`,
                         display: 'flex',
                         flexDirection: 'column',
                         gap: 1,
