944ff3d465781de8636bdb5812dea459911dea5f
yensung <george129.chen@gmail.com>
2025-03-19 14:12:55 -0700

    Fix test

--- src/utils/FakeAuthLoaders.ts
+++ src/utils/FakeAuthLoaders.ts
@@ -1,10 +1,17 @@
-import { User } from '@auth0/auth0-spa-js';
+import { User as Auth0User } from '@auth0/auth0-spa-js';
+
+import { User as ApiUser } from '@/api/User';
 
 export const getFakeUseUserAuthInfo =
-    ({ userInfo = {} as Partial<User>, isAuthenticated = false } = {}) =>
+    ({
+        userInfo = {} as ApiUser,
+        userAuthInfo = {} as Partial<Auth0User>,
+        isAuthenticated = false,
+    } = {}) =>
     () => {
         return {
             userInfo,
+            userAuthInfo,
             isAuthenticated,
         };
     };
