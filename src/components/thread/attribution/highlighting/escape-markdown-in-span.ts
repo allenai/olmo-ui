fac40a42fdbe65c4affca6dd27034f6eec30bc64
Taylor Blanton <matthewtblanton@gmail.com>
2025-03-31 11:54:29 -0500

    update the regex comment to match what it is now

--- src/components/thread/attribution/highlighting/escape-markdown-in-span.ts
+++ src/components/thread/attribution/highlighting/escape-markdown-in-span.ts
@@ -2,9 +2,7 @@ export const removeMarkdownCharactersFromStartAndEndOfSpan = (spanText: string):
     /**
      * https://regex101.com/r/5S3uuh
      * This regex checks for markdown characters inside of spans
-     * At the start of a string, it looks for things like headings (any number of #) and list symbols (-, +, *, or 1.).
-     * At the start and end of a string it looks for markdown symbols that can wrap something. This includes things like emphasis (any number of *, any number of _) and code blocks (`)
-     * We also trim after doing the regex matching to make sure there's spaces before and after the highlight as needed
+     * At the start of a string, it looks for things like headings (any number of #) and list symbols (-, +, *, or 1.) with a space after them.
      */
     return spanText.trim().replaceAll(/^(?:[+\->`]|#+|\d\.|\*+\s)+\s*|(?<!\s)(?:`)$/gm, '');
 };
