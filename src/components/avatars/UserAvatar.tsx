5d5d384efddb74ec5a46afd8e895cf4d17161849
Taylor Blanton <matthewtblanton@gmail.com>
2025-03-31 14:45:01 -0500

    always use our default avatar icon in threads

--- src/components/avatars/UserAvatar.tsx
+++ src/components/avatars/UserAvatar.tsx
@@ -1,28 +1,9 @@
 import { Theme } from '@mui/material';
 
-import { useUserAuthInfo } from '@/api/auth/auth-loaders';
-
 import defaultUserAvatarURL from '../assets/user.svg';
 import { ChatAvatar } from './ChatAvatar';
 
 export const UserAvatar = () => {
-    const { userAuthInfo, isAuthenticated } = useUserAuthInfo();
-
-    if (isAuthenticated && userAuthInfo?.picture) {
-        return (
-            <ChatAvatar
-                alt={userAuthInfo.email}
-                src={userAuthInfo.picture}
-                sx={{
-                    padding: 0,
-                    border: 'none',
-                    '> img': {
-                        borderRadius: '50%',
-                    },
-                }}
-            />
-        );
-    }
     return (
         <ChatAvatar
             src={defaultUserAvatarURL}
