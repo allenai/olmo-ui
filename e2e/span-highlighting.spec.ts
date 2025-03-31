9eff594b2c73c4144c3dd73b1a52d0e2e1735ff0
Taylor Blanton <matthewtblanton@gmail.com>
2025-03-31 11:50:42 -0500

    add e2e test for span highlighting structure

--- e2e/span-highlighting.spec.ts
+++ e2e/span-highlighting.spec.ts
@@ -2,84 +2,169 @@
 
 import { expect, test } from './playwright-utils';
 
-// const checkList = async (page: Page, listLocator: Locator, listItemTexts: string[]) => {
-//     const firstListItem = page
-//         .getByRole('listitem', { name: 'list item with * in the span' })
-//         .filter({
-//             has: page.getByRole('button', { name: 'list item with * in the span' }),
-//         });
-
-//     const list = page.getByRole('list').filter({
-//         has: firstListItem,
-//     });
-
-//     // Make sure the list kept all the children it's supposed to have
-//     await expect(list.locator('listitem')).toHaveCount(3);
-//     await expect(firstListItem).toBeVisible();
-
-//     await expect(
-//         list.getByRole('listitem', { name: 'list item with a space after *' }).filter({
-//             has: page.getByRole('button', { name: 'list item with a space after *' }),
-//         })
-//     ).toBeVisible();
-// };
-
 test('span highlighting', async ({ page }) => {
     await page.goto('/thread/highlightstresstest');
     await page.getByRole('button', { name: 'Show OLMoTrace' }).click();
-    await expect(
-        page.getByRole('heading', { name: 'H1 with # inside the span', level: 1 }).filter({
-            has: page.getByRole('button', { name: 'H1 with # inside the span' }),
-        })
-    ).toBeVisible();
-
-    await expect(
-        page.getByRole('heading', { name: 'H1 with a space after #', level: 1 }).filter({
-            has: page.getByRole('button', { name: 'H1 with a space after #' }),
-        })
-    ).toBeVisible();
-
-    await expect(
-        page.getByRole('heading', { name: 'H5 with # inside the span', level: 5 }).filter({
-            has: page.getByRole('button', { name: 'H5 with # inside the span' }),
-        })
-    ).toBeVisible();
-
-    await expect(
-        page.getByRole('heading', { name: 'H5 with a space after #', level: 5 }).filter({
-            has: page.getByRole('button', { name: 'H5 with a space after #' }),
-        })
-    ).toBeVisible();
-
-    await expect(
-        page
-            .getByRole('listitem')
-            .filter({ hasText: 'link with link stuff inside the span' })
-            .filter({
-                has: page.getByRole('link', { name: 'link with link stuff inside the span' }),
+    await expect
+        .soft(
+            page.getByRole('heading', { name: 'H1 with # inside the span', level: 1 }).filter({
+                has: page.getByRole('button', { name: 'H1 with # inside the span' }),
             })
-    ).toBeVisible();
+        )
+        .toBeVisible();
 
-    await expect(
-        page.getByRole('button', {
-            name: 'Show documents related to this span list item with * in the span',
-        })
-    ).toBeVisible();
+    await expect
+        .soft(
+            page.getByRole('heading', { name: 'H1 with a space after #', level: 1 }).filter({
+                has: page.getByRole('button', { name: 'H1 with a space after #' }),
+            })
+        )
+        .toBeVisible();
 
-    const starList = page.getByRole('list').filter({
-        has: page.getByRole('listitem', { name: 'list item with * in the span' }).filter({
-            has: page.getByRole('button', { name: 'list item with * in the span' }),
+    await expect
+        .soft(
+            page.getByRole('heading', { name: 'H5 with # inside the span', level: 5 }).filter({
+                has: page.getByRole('button', { name: 'H5 with # inside the span' }),
+            })
+        )
+        .toBeVisible();
+
+    await expect
+        .soft(
+            page.getByRole('heading', { name: 'H5 with a space after #', level: 5 }).filter({
+                has: page.getByRole('button', { name: 'H5 with a space after #' }),
+            })
+        )
+        .toBeVisible();
+
+    // #region check star list
+    const firstStarListItem = page.getByRole('listitem').filter({
+        has: page.getByRole('button', {
+            name: 'list item with * in the span',
         }),
     });
-    await expect(starList).toBeVisible();
+
+    const starList = page.getByRole('list').filter({
+        has: firstStarListItem,
+    });
+    await expect.soft(starList).toBeVisible();
 
     // Make sure the list kept all the children it's supposed to have
-    await expect(starList.getByRole('listitem')).toHaveCount(3);
-    await expect(firstStarListItem).toBeVisible();
+    await expect.soft(starList.getByRole('listitem')).toHaveCount(3);
+    await expect.soft(firstStarListItem).toBeVisible();
 
-    await expect(
-        starList.getByRole('listitem', { name: 'list item with a space after *' }).filter({
-            has: page.getByRole('button', { name: 'list item with a space after *' }),
-        })
-    ).toBeVisible();
+    await expect
+        .soft(
+            starList.getByRole('listitem').filter({
+                has: page.getByRole('button', { name: 'list item with a space after *' }),
+            })
+        )
+        .toBeVisible();
+    // #endregion
+
+    // #region check dash list
+    const firstDashListItem = page.getByRole('listitem').filter({
+        has: page.getByRole('button', {
+            name: 'list item with - in the span',
+        }),
+    });
+
+    const dashList = page.getByRole('list').filter({
+        has: firstDashListItem,
+    });
+    await expect.soft(dashList).toBeVisible();
+
+    // Make sure the list kept all the children it's supposed to have
+    await expect.soft(dashList.getByRole('listitem')).toHaveCount(3);
+    await expect.soft(firstDashListItem).toBeVisible();
+
+    await expect
+        .soft(
+            dashList.getByRole('listitem').filter({
+                has: page.getByRole('button', { name: 'list item with a space after -' }),
+            })
+        )
+        .toBeVisible();
+    // #endregion
+
+    // #region check plus list
+    const firstPlusListItem = page.getByRole('listitem').filter({
+        has: page.getByRole('button', {
+            name: 'list item with + in the span',
+        }),
+    });
+
+    const plusList = page.getByRole('list').filter({
+        has: firstPlusListItem,
+    });
+    await expect.soft(plusList).toBeVisible();
+
+    // Make sure the list kept all the children it's supposed to have
+    await expect.soft(plusList.getByRole('listitem')).toHaveCount(3);
+    await expect.soft(firstPlusListItem).toBeVisible();
+
+    await expect
+        .soft(
+            plusList.getByRole('listitem').filter({
+                has: page.getByRole('button', { name: 'list item with a space after +' }),
+            })
+        )
+        .toBeVisible();
+    // #endregion
+
+    await expect
+        .soft(
+            page
+                .getByRole('blockquote')
+                .filter({ has: page.getByRole('button', { name: 'quote with > in the span' }) })
+        )
+        .toBeVisible();
+
+    await expect
+        .soft(
+            page
+                .getByRole('blockquote')
+                .filter({ has: page.getByRole('button', { name: 'quote with a space after >' }) })
+        )
+        .toBeVisible();
+
+    // #region check ordered list
+    const firstOrderedListItem = page.getByRole('listitem').filter({
+        has: page.getByRole('button', {
+            name: 'numbered list with 1. in the span',
+        }),
+    });
+
+    const orderedList = page.getByRole('list').filter({
+        has: firstOrderedListItem,
+    });
+    await expect.soft(orderedList).toBeVisible();
+
+    // Make sure the list kept all the children it's supposed to have
+    await expect.soft(orderedList.getByRole('listitem')).toHaveCount(5);
+    await expect.soft(firstOrderedListItem).toBeVisible();
+
+    await expect
+        .soft(
+            orderedList.getByRole('listitem').filter({
+                has: page.getByRole('button', { name: 'list item with a space after 1.' }),
+            })
+        )
+        .toBeVisible();
+
+    await expect
+        .soft(
+            orderedList
+                .getByRole('listitem')
+                .filter({ has: page.getByText('span that contains bolding') })
+        )
+        .toBeVisible();
+    // #endregion
+
+    // I'm not sure how to check that the code block is right before this!
+    await expect
+        .soft(page.getByRole('button', { name: 'View code dataset matches' }))
+        .toBeVisible();
+
+    // There's a bunch of italics and bold checks that I'm not sure how to check after this one
 });
