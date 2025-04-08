4f06f57faa17726560d9b5aa9ba0d65eae750902
Taylor Blanton <matthewtblanton@gmail.com>
2025-04-07 19:32:40 -0500

    enable OLMoTrace by default

--- webpack.config.js
+++ webpack.config.js
@@ -97,7 +97,7 @@ module.exports = (env) => ({
             LLMX_API_URL: 'http://localhost:8080',
             DOLMA_API_URL: '/api',
             ENABLE_MOCKING: false,
-            IS_CORPUS_LINK_ENABLED: false,
+            IS_CORPUS_LINK_ENABLED: true,
             AUTH0_DOMAIN: 'allenai-public.us.auth0.com',
             AUTH0_CLIENT_ID: 'CmIxLlwn0miZ8kaQgDAK37pG0L6vrQEm',
             AUTH0_OLMO_API_AUDIENCE: 'https://olmo-api.allen.ai',
