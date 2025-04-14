fc30cf79a848bc3df0113c96aceb8071f0610ee8
yensung <george129.chen@gmail.com>
2025-04-14 13:50:21 +0800

    Add user profile icon to menu

--- src/components/avatars/UserAvatar.tsx
+++ src/components/avatars/UserAvatar.tsx
@@ -1,9 +1,33 @@
 import { Theme } from '@mui/material';
 
+import { useUserAuthInfo } from '@/api/auth/auth-loaders';
+
 import defaultUserAvatarURL from '../assets/user.svg';
 import { ChatAvatar } from './ChatAvatar';
 
-export const UserAvatar = () => {
+interface UserAvatarProps {
+    useDefault?: boolean;
+}
+
+export const UserAvatar = ({ useDefault = false }: UserAvatarProps) => {
+    const { userAuthInfo, isAuthenticated } = useUserAuthInfo();
+
+    if (!useDefault && isAuthenticated && userAuthInfo?.picture) {
+        return (
+            <ChatAvatar
+                alt={userAuthInfo.email}
+                src={userAuthInfo.picture}
+                sx={{
+                    padding: 0,
+                    border: 'none',
+                    '> img': {
+                        borderRadius: '50%',
+                    },
+                }}
+            />
+        );
+    }
+
     return (
         <ChatAvatar
             src={defaultUserAvatarURL}
