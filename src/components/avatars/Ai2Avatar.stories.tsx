8547663d2cedc517b408301d855499a4612329cf
Eric Marsh <ericm@allenai.org>
2025-04-24 11:35:02 -0700

    add storybook + setup tests

---
+++ src/components/avatars/Ai2Avatar.stories.tsx
@@ -0,0 +1,28 @@
+import { getTheme } from '@allenai/varnish2/theme';
+import { ThemeProvider } from '@mui/material/styles';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { uiRefreshOlmoTheme } from '../../olmoTheme';
+import { Ai2Avatar } from './Ai2Avatar';
+
+const theme = getTheme(uiRefreshOlmoTheme);
+
+const meta: Meta<typeof Ai2Avatar> = {
+    component: Ai2Avatar,
+    title: 'Components/Ai2Avatar',
+    decorators: [
+        (Story) => (
+            <ThemeProvider theme={theme}>
+                <Story />
+            </ThemeProvider>
+        ),
+    ],
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Ai2Avatar>;
+
+export const Primary: Story = {
+    args: {},
+};
