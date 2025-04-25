6049ae8c923dd9fd8daac32f9bf2d671928e6bcf
Huy Tran <84343285+huytr1995@users.noreply.github.com>
2025-04-25 11:53:01 -0700

    Setup Pandacss

---
+++ panda.config.ts
@@ -0,0 +1,12 @@
+// panda.config.ts
+import { defineConfig } from '@pandacss/dev';
+
+export default defineConfig({
+    preflight: true,
+    include: ['./src/**/*.{js,jsx,ts,tsx}'],
+    exclude: [],
+    outdir: 'styled-system',
+    theme: {
+        extend: {},
+    },
+});
