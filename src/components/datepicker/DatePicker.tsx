42d5bc5986e09f663858c2ec805ce061d506f1d2
Eric Marsh <108751850+ericmarsh995@users.noreply.github.com>
2025-05-06 12:21:39 -0700

    Add DatePicker to Playground (#969)
    [RAC Datepicker design
    doc](https://docs.google.com/document/d/1UyOnkdOJsd3BOSl_Gg40ed-9-Mxkg9ejPPOyV-IN_II/edit?tab=t.0)
    
    Closes: https://github.com/allenai/playground-issues-repo/issues/333
    
    ## Description
    
    Add Date Picker to storybook so we can start using it in Add Model Page.
    
    ### Screenshot
    
    
    https://github.com/user-attachments/assets/7854a9e1-4fe4-4ef0-85d8-5d04fefdc583
    
    ---------
    
    Co-authored-by: Huy Tran <84343285+huytr1995@users.noreply.github.com>

---
+++ src/components/datepicker/DatePicker.tsx
@@ -0,0 +1,68 @@
+import { cx } from '@allenai/varnish-panda-runtime/css';
+import { Button, Dialog, Label, Popover } from '@allenai/varnish-ui';
+import {
+    DateInput as AriaDateInput,
+    DatePicker as AriaDatePicker,
+    DatePickerProps as AriaDatePickerProps,
+    DateSegment as AriaDateSegment,
+    DateValue,
+    Group as AriaGroup,
+} from 'react-aria-components';
+
+import Calendar from './Calendar';
+import datePickerRecipe, { DatePickerRecipeProps } from './datePicker.styles';
+
+type DatePickerProps<T extends DateValue = DateValue> = {
+    value?: T;
+    placeHolder?: T;
+    children?: React.ReactNode;
+    className?: string;
+    groupClassName?: string;
+    labelText?: string;
+} & DatePickerRecipeProps &
+    AriaDatePickerProps<T>;
+
+const DatePicker = ({
+    value,
+    placeHolder,
+    children,
+    className,
+    groupClassName,
+    labelText,
+    ...rest
+}: DatePickerProps) => {
+    const [variantProps, localProps] = datePickerRecipe.splitVariantProps(rest);
+    const recipeClassNames = datePickerRecipe(variantProps);
+
+    return (
+        <AriaDatePicker
+            value={value}
+            placeholderValue={placeHolder}
+            className={cx(recipeClassNames.root, className)}
+            {...localProps}>
+            <Label className={cx(recipeClassNames.label)}>{labelText}</Label>
+            <AriaGroup className={cx(recipeClassNames.group, groupClassName)}>
+                <AriaDateInput className={cx(recipeClassNames.dateInput)}>
+                    {(segment) => <AriaDateSegment segment={segment} />}
+                </AriaDateInput>
+                <Button className={cx(recipeClassNames.button)}>▼</Button>
+            </AriaGroup>
+            <Popover className={cx(recipeClassNames.popover)}>
+                <Dialog className={cx(recipeClassNames.dialog)}>
+                    <Calendar
+                        classNames={{
+                            calendar: recipeClassNames.calendar,
+                            calendarHeader: recipeClassNames.calendarHeader,
+                            navButton: recipeClassNames.navButton,
+                            calendarGrid: recipeClassNames.calendarGrid,
+                            calendarCell: recipeClassNames.calendarCell,
+                        }}
+                    />
+                </Dialog>
+            </Popover>
+        </AriaDatePicker>
+    );
+};
+
+export { DatePicker };
+export type { DatePickerProps };
---
+++ src/components/datepicker/datePicker.styles.ts
@@ -0,0 +1,93 @@
+import { sva } from '@allenai/varnish-panda-runtime/css';
+import type { RecipeVariantProps } from '@allenai/varnish-panda-runtime/types';
+
+const datePickerRecipe = sva({
+    slots: [
+        'root',
+        'label',
+        'group',
+        'popover',
+        'calendar',
+        'dateInput',
+        'button',
+        'calendarHeader',
+        'navButton',
+        'dialog',
+        'calendarGrid',
+        'calendarCell',
+    ],
+    base: {
+        root: {
+            color: 'text',
+        },
+        label: {
+            color: 'text',
+        },
+        group: {
+            display: 'flex',
+            width: '[fit-content]',
+            alignItems: 'center',
+            border: '1px solid',
+            borderColor: 'gray.30',
+            borderRadius: 'sm',
+            padding: '1',
+        },
+        calendar: {
+            display: 'flex',
+            flexDir: 'column',
+            '& button': {
+                height: '[fit-content]',
+            },
+            width: '[16rem]',
+        },
+        dateInput: {
+            padding: '2',
+            display: 'inline',
+            width: '[fit-content]',
+        },
+        button: {
+            border: '2px solid',
+            borderColor: 'gray.100',
+            borderRadius: 'sm',
+            padding: '[0]',
+            color: 'text',
+        },
+        calendarHeader: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            margin: '[0 0.25rem 0.5rem]',
+        },
+        navButton: {
+            width: '[2rem]',
+            height: '[2rem]',
+            padding: '0',
+            backgroundColor: '[transparent]',
+        },
+        dialog: {},
+        popover: {},
+        calendarCell: {
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            '&[data-selected]': {
+                background: 'accent.primary',
+                _dark: {
+                    background: 'accent.secondary',
+                },
+                border: '1px solid',
+                borderColor: 'gray.30',
+            },
+        },
+        calendarGrid: {
+            width: '[100%]',
+            borderCollapse: 'separate',
+            borderSpacing: '1',
+        },
+    },
+});
+
+type DatePickerRecipeProps = RecipeVariantProps<typeof datePickerRecipe>;
+
+export { datePickerRecipe, datePickerRecipe as default };
+export type { DatePickerRecipeProps };
