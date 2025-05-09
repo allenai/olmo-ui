789d77a26ead23682ecff106abd1391184db950d
Huy Tran <84343285+huytr1995@users.noreply.github.com>
2025-05-09 09:20:18 -0700

    Minor change

--- src/pages/admin/modelConfig/CreateModelPage/CreateModelPage.tsx
+++ src/pages/admin/modelConfig/CreateModelPage/CreateModelPage.tsx
@@ -1,72 +1,30 @@
-import { parseAbsoluteToLocal } from '@internationalized/date';
 import { FormProvider, useForm } from 'react-hook-form';
-import { useLocation, useSubmit } from 'react-router-dom';
+import { useSubmit } from 'react-router-dom';
 
 import { MetaTags } from '@/components/MetaTags';
-import { links } from '@/Links';
 
 import { ModelConfigForm, type ModelConfigFormValues } from '../components/ModelConfigForm';
 import { mapConfigFormDataToRequest } from '../mapConfigFormDataToRequest';
 
-const mapModelEditFormData = (model: ModelConfigFormValues) => {
-    const { availableTime, deprecationTime, internal } = model;
-
-    const modifiedAvailableTime = availableTime
-        ? parseAbsoluteToLocal(availableTime.toString())
-        : undefined;
-
-    const modifiedDeprecationTime = deprecationTime
-        ? parseAbsoluteToLocal(deprecationTime.toString())
-        : undefined;
-
-    const availability: 'public' | 'internal' | 'prerelease' = internal
-        ? 'internal'
-        : modifiedAvailableTime || modifiedDeprecationTime
-          ? 'prerelease'
-          : 'public';
-
-    return {
-        ...model,
-        availableTime: modifiedAvailableTime,
-        deprecationTime: modifiedDeprecationTime,
-        availability,
-    };
-};
-
 export const CreateModelPage = () => {
-    const location = useLocation();
-    const model = location.state?.modelToEdit as ModelConfigFormValues;
     const formContext = useForm<ModelConfigFormValues>({
-        defaultValues: model
-            ? mapModelEditFormData(model)
-            : {
-                  promptType: 'text_only',
-                  host: 'modal',
-                  availability: 'internal',
-                  familyId: 'no_family',
-                  modelType: 'chat',
-              },
+        defaultValues: {
+            promptType: 'text_only',
+            host: 'modal',
+            availability: 'internal',
+            familyId: 'no_family',
+            modelType: 'chat',
+        },
         mode: 'onChange',
     });
-
-    const isEditMode = !!model;
 
     const submit = useSubmit();
 
     const handleSubmit = (formData: ModelConfigFormValues) => {
-        if (isEditMode) {
-            const path = links.editModel(model.id);
-            submit(mapConfigFormDataToRequest(formData), {
-                method: 'put',
-                action: path,
-                encType: 'application/json',
-            });
-        } else {
-            submit(mapConfigFormDataToRequest(formData), {
-                method: 'post',
-                encType: 'application/json',
-            });
-        }
+        submit(mapConfigFormDataToRequest(formData), {
+            method: 'post',
+            encType: 'application/json',
+        });
     };
     return (
         <>
