dd70590a7723c70efba64b9327c48ac193b277dc
Taylor Blanton <matthewtblanton@gmail.com>
2025-05-14 16:43:51 -0500

    refactor FileSizeInput, add stories for the new parsing

--- src/pages/admin/modelConfig/components/FileSizeInput/FileSizeInput.stories.tsx
+++ src/pages/admin/modelConfig/components/FileSizeInput/FileSizeInput.stories.tsx
@@ -1,31 +1,52 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { FormProvider, useForm } from 'react-hook-form';
+import { expect, fn, userEvent, waitFor, within } from '@storybook/test';
 
 import { FileSizeInput } from './FileSizeInput';
 
 const meta: Meta<typeof FileSizeInput> = {
     title: 'Components/FileSizeInput',
     component: FileSizeInput,
+    args: { onChange: fn(), name: 'test-file-size' },
 };
 
 export default meta;
 
 type Story = StoryObj<typeof FileSizeInput>;
 
-export const Default: Story = {
-    render: () => {
-        const methods = useForm({
-            defaultValues: {
-                fileSize: { amount: 512, unit: 'kib' },
-            },
-        });
+export const NoDefaultValue: Story = {};
 
-        return (
-            <FormProvider {...methods}>
-                <form>
-                    <FileSizeInput />
-                </form>
-            </FormProvider>
-        );
+export const FifteenKBytes: Story = {
+    args: {
+        value: 15360,
+    },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+
+        await expect(canvas.getByLabelText('File Size')).toHaveValue(15);
+        await expect(canvas.getByLabelText('Unit')).toHaveTextContent('KiB');
+    },
+};
+
+export const Default20MiB: Story = {
+    args: {
+        value: '20 MiB',
+    },
+    play: async ({ canvasElement }) => {
+        const canvas = within(canvasElement);
+
+        await expect(canvas.getByLabelText('File Size')).toHaveValue(20);
+        await expect(canvas.getByLabelText('Unit')).toHaveTextContent('MiB');
+    },
+};
+
+export const FillsIn15KiB: Story = {
+    play: async ({ canvasElement, args }) => {
+        const canvas = within(canvasElement);
+
+        await userEvent.type(canvas.getByLabelText('File Size'), '15');
+
+        await waitFor(async () => {
+            await expect(args.onChange).toHaveBeenCalledWith('15 KiB');
+        });
     },
 };
