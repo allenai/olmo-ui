13e3b12d155204e9dcd9936eb73f49c3b2a09177
Taylor Blanton <matthewtblanton@gmail.com>
2025-05-14 13:22:46 -0500

    put the custommenuitems back into ModelSelect (#997)
    MUI's Select doesn't like having the MenuItems be in their own
    component. This broke model selection. Putting them back fixes it!

--- src/components/thread/ModelSelect/ModelSelect.tsx
+++ src/components/thread/ModelSelect/ModelSelect.tsx
@@ -1,14 +1,21 @@
+import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
+import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
 import {
+    alpha,
     Box,
     FormControl,
     InputBase,
     inputBaseClasses,
     InputBaseProps,
+    MenuItem,
+    menuItemClasses,
     Select,
     selectClasses,
+    Stack,
     styled,
     SxProps,
     Theme,
+    Typography,
 } from '@mui/material';
 import { useEffect, useId, useRef } from 'react';
 import { useShallow } from 'zustand/react/shallow';
@@ -17,7 +24,6 @@ import { Model } from '@/api/playgroundApi/additionalTypes';
 import { AppContextState, useAppContext } from '@/AppContext';
 
 import { selectMessagesToShow } from '../ThreadDisplay/selectMessagesToShow';
-import { ModelSelectItem } from './ModelSelectItem';
 import { useHandleChangeModel } from './useHandleChangeModel';
 
 // HACK: This is here because we don't always have the models loaded when the selectedThreadPageLoader is called (if first load is a thread, for example)
@@ -131,13 +137,30 @@ export const ModelSelect = ({ sx }: ModelSelectionDisplayProps) => {
                     }}>
                     {models.map((model) => {
                         return (
-                            <ModelSelectItem
-                                key={model.id}
-                                name={model.name}
-                                id={model.id}
-                                acceptsFiles={model.accepts_files}
-                                internal={model.internal}
-                            />
+                            <CustomMenuItem key={model.id} value={model.id}>
+                                <Stack direction="row">
+                                    <Box
+                                        sx={{
+                                            padding: 1,
+                                            '>svg': {
+                                                verticalAlign: 'middle',
+                                            },
+                                        }}>
+                                        {model.accepts_files ? (
+                                            <ImageOutlinedIcon />
+                                        ) : (
+                                            <ChatOutlinedIcon />
+                                        )}
+                                    </Box>
+                                    <Stack direction="column">
+                                        <Typography>{model.name}</Typography>
+                                        <Typography sx={{ opacity: 0.5 }}>
+                                            {model.accepts_files ? 'Multimodal' : 'Text-only'}
+                                            {model.internal && ' (Internal)'}
+                                        </Typography>
+                                    </Stack>
+                                </Stack>
+                            </CustomMenuItem>
                         );
                     })}
                 </Select>
@@ -176,5 +199,28 @@ const CustomInput = styled((props: InputBaseProps) => <InputBase {...props} />)(
         marginInlineEnd: theme.spacing(2),
         transform: 'scale(1.2) translateY(0px)',
         fill: theme.palette.primary.main,
+    },
+}));
+
+const CustomMenuItem = styled(MenuItem)(({ theme }) => ({
+    paddingInline: theme.spacing(1.5),
+
+    background: 'transparent',
+
+    [`&.${menuItemClasses.focusVisible}`]: {
+        backgroundColor: alpha(theme.palette.common.black, 0.12),
+    },
+    ':hover': {
+        backgroundColor: alpha(theme.palette.common.black, 0.04),
+    },
+    [`&.${menuItemClasses.selected}`]: {
+        background: alpha(theme.palette.background.paper, 0.6),
+        color: theme.palette.text.primary,
+        [`&.${menuItemClasses.focusVisible}`]: {
+            backgroundColor: alpha(theme.palette.common.black, 0.12),
+        },
+        ':hover': {
+            backgroundColor: alpha(theme.palette.common.black, 0.04),
+        },
     },
 }));
