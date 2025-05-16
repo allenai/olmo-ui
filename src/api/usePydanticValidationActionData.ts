6d50b2755c2d37e91b883f4a107cd99356cc3438
Huy Tran <84343285+huytr1995@users.noreply.github.com>
2025-05-16 12:10:52 -0700

    Errors when a model with a duplicate ID is added don't show on the form

--- src/api/usePydanticValidationActionData.ts
+++ src/api/usePydanticValidationActionData.ts
@@ -1,8 +1,13 @@
 import { useEffect } from 'react';
-import { type FieldValues, type UseFormSetError } from 'react-hook-form';
+import { type FieldValues, Path, type UseFormSetError } from 'react-hook-form';
 import { useActionData } from 'react-router-dom';
 
 import { error } from '@/api/error';
+
+interface ConflictErrorPayload {
+    code: number;
+    message: string;
+}
 
 export const usePydanticValidationActionData = <TFieldValues extends FieldValues = FieldValues>(
     setError: UseFormSetError<TFieldValues>
@@ -15,6 +20,7 @@ export const usePydanticValidationActionData = <TFieldValues extends FieldValues
             typeof actionData === 'object' &&
             error.isValidationErrorPayload(actionData)
         ) {
+            console.log('Validation error detected, setting form errors');
             actionData.validation_errors.forEach((error) => {
                 // Since the response models can be polymorphic Pydantic sometimes puts the type in the first loc element
                 // Getting the last element gets us the right form field
@@ -22,6 +28,17 @@ export const usePydanticValidationActionData = <TFieldValues extends FieldValues
                 const errorLoc = error.loc.at(-1);
                 // @ts-expect-error - There's probably some mapping we need to do here to make this perfect
                 setError(errorLoc, { type: error.type, message: error.msg });
+            });
+        }
+        if (
+            actionData != null &&
+            typeof actionData === 'object' &&
+            'code' in actionData &&
+            (actionData as ConflictErrorPayload).code === 409
+        ) {
+            setError('id' as Path<TFieldValues>, {
+                type: 'conflict',
+                message: (actionData as ConflictErrorPayload).message,
             });
         }
     }, [actionData, setError]);
