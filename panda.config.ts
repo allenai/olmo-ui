670a52b45721fb598fa894b96f0315f93ad70c13
Byron Bischoff <byronb@gmail.com>
2025-05-22 13:11:09 -0700

    All rem values

--- panda.config.ts
+++ panda.config.ts
@@ -29,15 +29,15 @@ export default defineConfig({
     theme: {
         extend: {
             breakpoints: {
-                sm: '600px',
-                md: '900px',
-                lg: '1200px',
+                sm: '37.5rem', // 600px
+                md: '56.25rem', // 900px
+                lg: '75rem', // 1200px
                 // compat:
-                desktop: '1200px',
+                desktop: '75rem',
             },
             containerSizes: {
-                small: '31.25rem',
-                large: '43.75rem',
+                small: '31.25rem', // 500px
+                large: '43.75rem', // 700px
             },
         },
     },
