b78b5305a82bb463de2d42aa240b34baf6a0d2ca
Byron <bronzle@users.noreply.github.com>
2025-06-05 09:28:26 -0700

    Use react context for threadId (#1021)
    ## Description
    
    Sets the thread in context for use below. This sort of necessitated
    adding a correct placeholder so that I could render the provider only if
    we had a threadId.
    
    Only actual new code is the ThreadProvider and the placeholder component
    that renders and empty ThreadDisplayView

---
+++ src/pages/comparison/SingleThreadContainer.tsx
@@ -0,0 +1,81 @@
+import { css } from '@allenai/varnish-panda-runtime/css';
+
+import { Model } from '@/api/playgroundApi/additionalTypes';
+import { Thread, useThread } from '@/api/playgroundApi/thread';
+import { ThreadDisplay, ThreadDisplayView } from '@/components/thread/ThreadDisplay/ThreadDisplay';
+
+import { CompareModelSelect } from './CompareModelSelect';
+import { ThreadProvider } from './ThreadContext';
+
+const singleThreadClasses = css({
+    display: 'flex',
+    flexDirection: 'column',
+});
+
+interface SingleThreadContainerProps {
+    threadViewIdx: string;
+    models: Model[];
+    threadRootId?: Thread['id'];
+}
+
+export const SingleThreadContainer = ({
+    threadViewIdx,
+    models,
+    threadRootId,
+}: SingleThreadContainerProps) => {
+    if (!threadRootId) {
+        return <ThreadPlaceholder threadViewIdx={threadViewIdx} models={models} />;
+    }
+
+    return (
+        <div className={singleThreadClasses}>
+            <ThreadProvider threadId={threadRootId}>
+                <CompareModelSelect threadViewId={threadViewIdx} models={models} />
+                <SingleThread threadRootId={threadRootId} />
+            </ThreadProvider>
+        </div>
+    );
+};
+
+interface SingleThreadProps {
+    threadRootId: Thread['id'];
+}
+
+const SingleThread = ({ threadRootId }: SingleThreadProps) => {
+    const shouldShowAttributionHighlightDescription = false;
+    const streamingMessageId = null;
+    const isUpdatingMessageContent = false;
+    const selectedMessageId = undefined;
+
+    const { data, error: _ } = useThread(threadRootId);
+
+    const { messages } = data;
+    const childMessageIds = messages.map((message) => {
+        return message.id;
+    });
+
+    return (
+        <ThreadDisplay
+            childMessageIds={childMessageIds}
+            shouldShowAttributionHighlightDescription={shouldShowAttributionHighlightDescription}
+            streamingMessageId={streamingMessageId}
+            isUpdatingMessageContent={isUpdatingMessageContent}
+            selectedMessageId={selectedMessageId}
+        />
+    );
+};
+
+type ThreadPlaceholderProps = Omit<SingleThreadContainerProps, 'threadRootId'>;
+
+const ThreadPlaceholder = ({ threadViewIdx, models }: ThreadPlaceholderProps) => {
+    return (
+        <div className={singleThreadClasses}>
+            <CompareModelSelect threadViewId={threadViewIdx} models={models} />
+            <ThreadDisplayView
+                shouldShowAttributionHighlightDescription={false}
+                streamingMessageId={null}
+                isUpdatingMessageContent={false}
+            />
+        </div>
+    );
+};
