49cd6f6328a1efb34f9748587087245461710d9c
jonathan m borchardt <jonathanb@allenai.org>
2025-06-25 16:21:08 -0700

    add trtansition for audio processing (#1055)

--- src/slices/TranscriptionSlice.ts
+++ src/slices/TranscriptionSlice.ts
@@ -3,9 +3,12 @@ import { OlmoStateCreator } from '@/AppContext';
 export interface TranscriptionSlice {
     isTranscribing: boolean;
     setIsTranscribing: (isTranscribing: boolean) => void;
+    isProcessingAudio: boolean;
+    setIsProcessingAudio: (isProcessingAudio: boolean) => void;
 }
 
 export const createTranscriptionSlice: OlmoStateCreator<TranscriptionSlice> = (set) => ({
+    // recording audio on client
     isTranscribing: false,
     setIsTranscribing: (isTranscribing: boolean) => {
         set(
@@ -14,6 +17,17 @@ export const createTranscriptionSlice: OlmoStateCreator<TranscriptionSlice> = (s
             },
             undefined,
             'transcription/setIsTranscribing'
+        );
+    },
+    // converting audio to text on server
+    isProcessingAudio: false,
+    setIsProcessingAudio: (isProcessingAudio: boolean) => {
+        set(
+            (state) => {
+                state.isProcessingAudio = isProcessingAudio;
+            },
+            undefined,
+            'transcription/setIsProcessingAudio'
         );
     },
 });
