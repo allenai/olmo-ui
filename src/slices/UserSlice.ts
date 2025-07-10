278c6dbc8e84acae195eb55a0a6020515e7fee92
Taylor Blanton <matthewtblanton@gmail.com>
2025-07-10 12:30:51 -0500

    switch the default a0 domain to our custom domain (#1064)
    This PR changes the default auth0 domain and handles the case of getting
    unauth errors after we switch the API to use the new domain. Some users
    would have a token signed under the old domain which the APi would
    correctly reject.

--- src/slices/UserSlice.ts
+++ src/slices/UserSlice.ts
@@ -1,5 +1,7 @@
 import { QueryClient } from '@tanstack/react-query';
 
+import { createLoginRedirectURL } from '@/api/auth/auth-utils';
+import { auth0Client } from '@/api/auth/auth0Client';
 import { getUserModel } from '@/api/getWhoAmIModel';
 import { OlmoStateCreator } from '@/AppContext';
 
@@ -10,17 +12,29 @@ import { errorToAlert } from './SnackMessageSlice';
 export interface UserSlice {
     userRemoteState?: RemoteState;
     userInfo: User | null;
-    getUserInfo: (queryClient: QueryClient) => Promise<User>;
+    getUserInfo: (queryClient: QueryClient, retryCount?: number) => Promise<User>;
     resetTermsAndConditionsAcceptance: () => void;
     acceptTermsAndConditions: () => Promise<void>;
 }
 
 const userClient = new UserClient();
 
+function isAuthError(error: unknown): error is { error: { code: 401 } } {
+    return (
+        typeof error === 'object' &&
+        error != null &&
+        'error' in error &&
+        typeof error.error === 'object' &&
+        error.error !== null &&
+        'code' in error.error &&
+        error.error.code === 401
+    );
+}
+
 export const createUserSlice: OlmoStateCreator<UserSlice> = (set, get) => ({
     userRemoteState: undefined,
     userInfo: null,
-    getUserInfo: async (queryClient: QueryClient) => {
+    getUserInfo: async (queryClient: QueryClient, retryCount = 0) => {
         const { addSnackMessage } = get();
         set({ userRemoteState: RemoteState.Loading });
 
@@ -34,6 +48,16 @@ export const createUserSlice: OlmoStateCreator<UserSlice> = (set, get) => ({
 
             return user;
         } catch (err) {
+            if (isAuthError(err) && retryCount < 3) {
+                const isAuthenticated = await auth0Client.isAuthenticated();
+                const { getUserInfo } = get();
+                if (isAuthenticated) {
+                    document.location = createLoginRedirectURL(window.location.href);
+                }
+
+                return await getUserInfo(queryClient, retryCount + 1);
+            }
+
             addSnackMessage(
                 errorToAlert(
                     `fetch-${WhoamiApiUrl}-${new Date().getTime()}`.toLowerCase(),
