e978c29c5fe5f3be3b15935a9348b608049098cd
Taylor Blanton <matthewtblanton@gmail.com>
2025-07-25 17:04:39 -0500

    pull the streaming test out to another file, ignore it on webkit

--- e2e/olmo.spec.ts
+++ e2e/olmo.spec.ts
@@ -80,30 +80,6 @@ test('should scroll to the new user prompt message when its submitted', async ({
     expect(page.url()).toContain(selectedThreadId);
 });
 
-test('should stop thread from streaming', async ({ page }) => {
-    await page.goto('/');
-    await page.waitForLoadState('networkidle');
-    await page.getByRole('textbox', { name: /^Message*/ }).focus();
-    await page.getByRole('textbox', { name: /^Message*/ }).fill('User message');
-    await page.getByRole('button', { name: 'Submit prompt' }).click();
-
-    // Wait for the stop button, then check streaming indicator
-    await expect(page.getByRole('button', { name: 'Stop response generation' })).toBeVisible();
-    await expect(
-        page.locator('[data-testid="thread-display"] [data-is-streaming="true"]')
-    ).toBeVisible();
-
-    await page.getByRole('button', { name: 'Stop response generation' }).click();
-
-    // Check for stop message
-    await expect(
-        page.getByText('You stopped OLMo from generating answers to your query')
-    ).toBeVisible();
-
-    // Streaming indicator should disapear when stopped
-    await expect(page.locator('[data-is-streaming="true"]')).not.toBeVisible();
-});
-
 test('can load threads from history drawer', async ({ page }) => {
     // Check the first existing thread
     await page.goto('/');
