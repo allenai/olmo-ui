854defab77a1fb02586bbd2e0e9108466f8c61af
Taylor Blanton <matthewtblanton@gmail.com>
2025-07-31 15:57:52 -0500

    have desktop and mobile share link styles

--- src/components/OlmoAppBar/Footer/NavigationFooter.tsx
+++ src/components/OlmoAppBar/Footer/NavigationFooter.tsx
@@ -1,9 +1,5 @@
-import FindInPageOutlined from '@mui/icons-material/FindInPageOutlined';
-import LaunchOutlinedIcon from '@mui/icons-material/LaunchOutlined';
 import { Stack } from '@mui/material';
 
-import Ai2Icon from '@/components/assets/ai2.svg?react';
-import DiscordIcon from '@/components/assets/discord.svg?react';
 import { useDesktopOrUp } from '@/components/dolma/shared';
 import { links } from '@/Links';
 
@@ -15,32 +11,10 @@ export const NavigationFooter = () => {
     const isDesktop = useDesktopOrUp();
 
     return (
-        <Stack
-            marginBlockStart="auto"
-            id="nav-footer"
-            gap={isDesktop ? 1 : 2}
-            component={isDesktop ? 'div' : 'ul'}
-            padding="0"
-            marginBottom="0">
-            <FooterLink
-                startIcon={<Ai2Icon height={20} width={20} viewBox="0 0 72 72" />}
-                href={links.ai2}
-                endIcon={LaunchOutlinedIcon}>
-                Ai2
-            </FooterLink>
-            <FooterLink
-                startIcon={<DiscordIcon />}
-                href={links.discord}
-                endIcon={LaunchOutlinedIcon}>
-                Discord
-            </FooterLink>
-            <FooterLink
-                startIcon={<FindInPageOutlined />}
-                href={links.documentation}
-                endIcon={LaunchOutlinedIcon}>
-                Documentation
-            </FooterLink>
-
+        <Stack marginBlockStart="auto" id="nav-footer" gap={1} padding="0" marginBottom="0">
+            <FooterLink href={links.ai2}>Ai2</FooterLink>
+            <FooterLink href={links.discord}>Discord</FooterLink>
+            <FooterLink href={links.documentation}>Documentation</FooterLink>
             {isDesktop && <LoginLink />}
             <AvatarMenuLink />
         </Stack>
--- src/components/OlmoAppBar/FooterLink.tsx
+++ src/components/OlmoAppBar/FooterLink.tsx
@@ -1,21 +1,13 @@
-import type { SvgIconComponent } from '@mui/icons-material';
 import ArrowOutwardOutlined from '@mui/icons-material/ArrowOutwardOutlined';
 import { Link } from '@mui/material';
-import type { ReactNode } from 'react';
-
-import { DESKTOP_LAYOUT_BREAKPOINT } from '@/constants';
-
-import { NavigationLink } from './NavigationLink';
 
 interface FooterLinkProps {
     href: string;
     children: string;
     isExternal?: boolean;
-    startIcon?: ReactNode;
-    endIcon?: SvgIconComponent;
 }
 
-export const FooterLink = ({ href, children, isExternal, startIcon, endIcon }: FooterLinkProps) => {
+export const FooterLink = ({ href, children, isExternal }: FooterLinkProps) => {
     return (
         <>
             <Link
@@ -23,25 +15,12 @@ export const FooterLink = ({ href, children, isExternal, startIcon, endIcon }: F
                 target={isExternal ? '_blank' : '_self'}
                 rel="noreferrer"
                 fontWeight={600}
-                sx={(theme) => ({
+                sx={{
                     paddingInline: 4,
                     paddingBlock: 1,
-                    [theme.breakpoints.down(DESKTOP_LAYOUT_BREAKPOINT)]: { display: 'none' },
-                })}>
+                }}>
                 {children} <ArrowOutwardOutlined sx={{ height: '1rem', width: '1rem' }} />
             </Link>
-            <NavigationLink
-                icon={startIcon}
-                href={href}
-                DisclosureIcon={endIcon}
-                sx={(theme) => ({
-                    [theme.breakpoints.up(DESKTOP_LAYOUT_BREAKPOINT)]: { display: 'none' },
-                })}
-                textSx={(theme) => ({
-                    color: theme.palette.primary.main,
-                })}>
-                {children}
-            </NavigationLink>
         </>
     );
 };
