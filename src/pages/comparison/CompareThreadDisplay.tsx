d08974cf7bd49face2e148b7e05fcc30a137ca15
Byron <bronzle@users.noreply.github.com>
2025-08-04 13:55:36 -0700

    Comparison mobile UI (#1102)
    ## Description
    
    Rebased and did some minor updates to the [old mobile PR
    (#1040)](https://github.com/allenai/olmo-ui/pull/1040)
    
    ## Testing Plan
    
    Tested this in chrome as "mobile" but I'd like to test it native (maybe
    I can commandeer `comparison-page` ? and test on-device
    
    ## Output / Screenshots
    
    
    https://github.com/user-attachments/assets/9ac17dbb-d0f1-4fdc-a9b4-4b52779c6b5e
    
    ### A11y
    
    How do keyboard navigation and screen readers work with this PR?

--- src/pages/comparison/CompareThreadDisplay.tsx
+++ src/pages/comparison/CompareThreadDisplay.tsx
@@ -1,23 +1,39 @@
 import { css } from '@allenai/varnish-panda-runtime/css';
+import { styled, Tab, Tabs } from '@mui/material';
+import { useEffect, useRef, useState } from 'react';
 
 import { isModelVisible, useModels } from '@/components/thread/ModelSelect/useModels';
+import { DESKTOP_LAYOUT_BREAKPOINT } from '@/constants';
 import { useQueryContext } from '@/contexts/QueryContext';
 
 import { SingleThreadContainer } from './SingleThreadContainer';
+import { ComparisonTabPanel } from './tabView/ComparisonTabPanel';
+import { dataViewId, viewIdAttr, viewPanelId, viewTabId } from './tabView/tabAttributes';
 
 const containerStyle = css({
     gridArea: 'content',
-    paddingInline: '5',
-    paddingBlockEnd: '2',
     minWidth: '[0]',
     minHeight: '[0]',
     height: '[100%]',
-    gap: '2',
+    gap: '4',
     justifyContent: 'center',
     display: 'flex',
+    flexDirection: 'column',
 });
 
-// TODO Implement (columns degrade to tabs)
+const threadContainerClassNames = css({
+    display: 'flex',
+    height: '[100%]',
+
+    overflowY: 'hidden',
+    overflowX: {
+        base: 'auto',
+        lg: 'hidden',
+    },
+
+    scrollSnapType: '[x mandatory]',
+});
+
 export const CompareThreadDisplay = () => {
     const models = useModels({
         // add back including model from state/params
@@ -27,14 +43,93 @@ export const CompareThreadDisplay = () => {
 
     const queryContext = useQueryContext();
 
-    const containers = queryContext.transform((threadViewId, _model, threadId) => (
-        <SingleThreadContainer
+    // Get the first threadViewId from context
+    const [firstViewId] = queryContext.transform((threadViewId) => threadViewId);
+
+    const [selectedThreadViewIdx, setSelectedThreadViewIdx] = useState(firstViewId);
+    const threadContainerRef = useRef<HTMLDivElement>(null);
+
+    const handleViewChange = (_e: React.SyntheticEvent, viewId: string) => {
+        const threadView = threadContainerRef.current?.querySelector(dataViewId(viewId));
+        threadView?.scrollIntoView({
+            inline: 'center',
+            behavior: 'smooth',
+        });
+        setSelectedThreadViewIdx(viewId);
+    };
+
+    useEffect(() => {
+        let observer: IntersectionObserver | undefined;
+        if (threadContainerRef.current) {
+            observer = new IntersectionObserver(
+                (entries) => {
+                    const intersectingEntry = entries.find((entry) => {
+                        return entry.isIntersecting;
+                    });
+                    const viewId = intersectingEntry?.target.getAttribute(viewIdAttr);
+
+                    if (viewId && viewId !== selectedThreadViewIdx) {
+                        setSelectedThreadViewIdx(viewId);
+                    }
+                },
+                {
+                    root: threadContainerRef.current,
+                    rootMargin: '0px',
+                    threshold: 0.51,
+                }
+            );
+            threadContainerRef.current.querySelectorAll('[role="tabpanel"]').forEach((element) => {
+                observer?.observe(element);
+            });
+        }
+        return () => {
+            observer?.disconnect();
+        };
+    }, [selectedThreadViewIdx]);
+
+    const tabs = queryContext.transform((threadViewId, model) => (
+        <Tab
             key={threadViewId}
-            threadViewIdx={threadViewId}
-            threadRootId={threadId}
-            models={models}
+            label={model?.name}
+            id={viewTabId(threadViewId)}
+            aria-controls={viewPanelId(threadViewId)}
+            value={threadViewId}
         />
     ));
 
-    return <div className={containerStyle}>{containers}</div>;
+    const containers = queryContext.transform((threadViewId, _model, threadId) => {
+        const isSelected = threadViewId === selectedThreadViewIdx;
+        return (
+            <ComparisonTabPanel
+                key={threadViewId}
+                threadViewId={threadViewId}
+                isSelected={isSelected}>
+                <SingleThreadContainer
+                    threadViewIdx={threadViewId}
+                    threadRootId={threadId}
+                    models={models}
+                />
+            </ComparisonTabPanel>
+        );
+    });
+
+    return (
+        <div className={containerStyle}>
+            <MobileTabs value={selectedThreadViewIdx} onChange={handleViewChange}>
+                {tabs}
+            </MobileTabs>
+            <div className={threadContainerClassNames} ref={threadContainerRef}>
+                {containers}
+            </div>
+        </div>
+    );
 };
+
+// I would prefer to use panda for this, but MUI's styles override it
+const MobileTabs = styled(Tabs)(({ theme }) => ({
+    paddingInline: theme.spacing(2),
+    display: 'flex',
+    [theme.breakpoints.up(DESKTOP_LAYOUT_BREAKPOINT)]: {
+        display: 'none',
+    },
+}));
