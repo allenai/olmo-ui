6a4b859b4b90f6ed0b6de2e8dfb4f5bec234d393
Byron Bischoff <byronb@gmail.com>
2025-08-07 21:10:52 -0700

    working with the current system

--- src/components/thread/QueryForm/AudioTranscription/AudioInputButton.tsx
+++ src/components/thread/QueryForm/AudioTranscription/AudioInputButton.tsx
@@ -1,9 +1,13 @@
 import { css } from '@allenai/varnish-panda-runtime/css';
 import { MicRounded, StopCircleOutlined } from '@mui/icons-material';
-import { IconButton, Tooltip } from '@mui/material';
+import CheckIcon from '@mui/icons-material/Check';
+import CloseIcon from '@mui/icons-material/Close';
+import { IconButton, IconButtonProps, Stack, styled, Tooltip } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
+import { useState } from 'react';
 
 import { useAppContext } from '@/AppContext';
+import { StyledTooltip } from '@/components/StyledTooltip';
 import { useFeatureToggles } from '@/FeatureToggleContext';
 import { AlertMessageSeverity, errorToAlert, SnackMessageType } from '@/slices/SnackMessageSlice';
 
@@ -20,6 +24,7 @@ interface AudioInputButtonProps {
     onRecordingEnd?: () => void;
     onTranscriptionBegin?: () => void;
     onTranscriptionComplete?: (content: string) => void;
+    onComplete?: (content?: string | null) => void;
 }
 
 export const AudioInputButton = ({
@@ -27,11 +32,16 @@ export const AudioInputButton = ({
     onRecordingEnd,
     onTranscriptionBegin,
     onTranscriptionComplete,
+    onComplete,
 }: AudioInputButtonProps) => {
     const { isOLMoASREnabled } = useFeatureToggles();
     const { isTranscribing, addSnackMessage, isProcessingAudio, setIsProcessingAudio } =
         useAppContext();
     const { startRecording, stopRecording } = useAudioRecording();
+
+    const cancelRecording = () => {
+        stopRecording('userCancel');
+    };
 
     if (!isOLMoASREnabled) {
         return null;
@@ -43,6 +53,7 @@ export const AudioInputButton = ({
     const handleAudioClick = async () => {
         if (isTranscribing) {
             stopRecording();
+            onComplete?.(null);
         } else {
             try {
                 onRecordingBegin?.();
@@ -50,6 +61,9 @@ export const AudioInputButton = ({
                     pollLength,
                     maxLength,
                     onStop: async (data, reason) => {
+                        if (reason === 'userCancel') {
+                            return;
+                        }
                         setIsProcessingAudio(true);
                         if (reason === 'maxLength') {
                             addSnackMessage({
@@ -65,6 +79,7 @@ export const AudioInputButton = ({
                             onTranscriptionBegin?.();
                             const { text } = await handleTranscribe(data);
                             onTranscriptionComplete?.(text);
+                            onComplete?.(text);
                         } catch (error: unknown) {
                             addSnackMessage(
                                 errorToAlert(
@@ -73,6 +88,7 @@ export const AudioInputButton = ({
                                     error
                                 )
                             );
+                            onComplete?.(null);
                         } finally {
                             setIsProcessingAudio(false);
                         }
@@ -86,37 +102,80 @@ export const AudioInputButton = ({
                         error
                     )
                 );
+                onComplete?.(null);
             }
         }
     };
 
+    const cancelButton = (
+        <PromptButton
+            onClick={() => {
+                cancelRecording();
+            }}
+            disableRipple={true}
+            color="default"
+            size="medium"
+            sx={{
+                color: 'var(--palette-light-text-default)',
+                ':hover': {
+                    color: 'var(--palette-light-accent-secondary)',
+                },
+            }}>
+            <CloseIcon fontSize="small" />
+        </PromptButton>
+    );
+    const acceptButton = (
+        <PromptButton
+            onClick={() => {
+                stopRecording();
+            }}
+            disableRipple={true}
+            color="secondary"
+            size="medium">
+            <CheckIcon fontSize="small" />
+        </PromptButton>
+    );
+
+    if (isTranscribing) {
+        return (
+            <>
+                {cancelButton}
+                {acceptButton}
+            </>
+        );
+    }
+
     return (
-        <IconButton
+        <PromptButton
             onClick={handleAudioClick}
             disableRipple={true}
+            color="secondary"
             sx={{
-                // style like the FileUploadButton
-                padding: 0.5,
-                color: 'var(--palette-light-accent-secondary)',
-                ':hover': {
-                    backgroundColor: 'transparent',
-                    color: 'var(--color-teal-100)',
-                },
-                ':has(:focus-visible)': {
-                    outline: '1px solid',
-                    borderRadius: 'var(--radii-full, 9999px)',
-                },
                 cursor: isProcessingAudio ? 'default' : 'hand',
             }}>
             {isProcessingAudio ? (
-                <Tooltip title="Transcribing Audio. Please wait.">
+                <StyledTooltip title="Transcribing Audio. Please wait." placement="top">
                     <CircularProgress size="1.5rem" color="secondary" />
-                </Tooltip>
+                </StyledTooltip>
             ) : isTranscribing ? (
                 <StopCircleOutlined className={iconClassName} />
             ) : (
                 <MicRounded className={iconClassName} />
             )}
-        </IconButton>
+        </PromptButton>
     );
 };
+
+const PromptButton = styled(IconButton)({
+    padding: 0.5,
+    width: '32px',
+    color: 'var(--palette-light-accent-secondary)',
+    ':hover': {
+        backgroundColor: 'transparent',
+        color: 'var(--color-teal-100)',
+    },
+    ':has(:focus-visible)': {
+        outline: '1px solid',
+        borderRadius: 'var(--radii-full, 9999px)',
+    },
+});
