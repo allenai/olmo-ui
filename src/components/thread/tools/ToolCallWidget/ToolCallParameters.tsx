bd9b65a69b0d60f925d79323c5bb4a7c3e46216b
Taylor Blanton <matthewtblanton@gmail.com>
2025-08-19 16:43:28 -0500

    move the copy button to the top right of the params

---
+++ src/components/thread/tools/ToolCallWidget/ToolCallParameters.tsx
@@ -0,0 +1,43 @@
+import { ContentCopy } from '@mui/icons-material';
+
+import { useAppContext } from '@/AppContext';
+import { IconButtonWithTooltip } from '@/components/IconButtonWithTooltip';
+import { ThemeSyntaxHighlighter } from '@/components/ThemeSyntaxHighlighter';
+import { CollapsibleWidgetPanelContent } from '@/components/widgets/CollapsibleWidget/CollapsibleWidgetPanel';
+import { SnackMessageType } from '@/slices/SnackMessageSlice';
+
+interface ToolCallParametersProps {
+    // Not using the optional ? here ensures that children are passed
+    children: string | undefined;
+}
+
+export const ToolCallParameters = ({ children = '' }: ToolCallParametersProps) => {
+    const addSnackMessage = useAppContext((state) => state.addSnackMessage);
+    const copyParameters = async () => {
+        if (children) {
+            await navigator.clipboard.writeText(children);
+            addSnackMessage({
+                type: SnackMessageType.Brief,
+                id: 'tool-call-answer-copied',
+                message: 'Tool call parameters copied',
+            });
+        }
+    };
+
+    return (
+        <CollapsibleWidgetPanelContent>
+            <IconButtonWithTooltip
+                label="Copy tool call parameters"
+                color="default"
+                placement="top"
+                onClick={copyParameters}
+                sx={{ position: 'absolute', top: 0, right: 0 }}>
+                <ContentCopy />
+            </IconButtonWithTooltip>
+            <ThemeSyntaxHighlighter
+                customStyle={{ margin: 0, padding: 0, backgroundColor: 'transparent' }}>
+                {children}
+            </ThemeSyntaxHighlighter>
+        </CollapsibleWidgetPanelContent>
+    );
+};
--- src/components/thread/tools/ToolCallWidget/ToolCallWidget.tsx
+++ src/components/thread/tools/ToolCallWidget/ToolCallWidget.tsx
@@ -1,20 +1,17 @@
-import { ContentCopy, DataObject } from '@mui/icons-material';
+import { DataObject } from '@mui/icons-material';
 
 import type { SchemaToolCall } from '@/api/playgroundApi/playgroundApiSchema';
-import { useAppContext } from '@/AppContext';
-import { IconButtonWithTooltip } from '@/components/IconButtonWithTooltip';
 import { CollapsibleWidgetBase } from '@/components/widgets/CollapsibleWidget/CollapsibleWidgetBase';
 import { CollapsibleWidgetFooter } from '@/components/widgets/CollapsibleWidget/CollapsibleWidgetFooter';
 import { CollapsibleWidgetHeading } from '@/components/widgets/CollapsibleWidget/CollapsibleWidgetHeading';
 import { CollapsibleWidgetPanel } from '@/components/widgets/CollapsibleWidget/CollapsibleWidgetPanel';
 import { ExpandArrow } from '@/components/widgets/CollapsibleWidget/ExpandArrow';
 import { FadeOverflowContent } from '@/components/widgets/FadeOverflowContent';
-import { SnackMessageType } from '@/slices/SnackMessageSlice';
 import { css } from '@/styled-system/css';
 
 import { mapToolCallArgs } from '../mapToolCallArgs';
 import { ToolCallAnswer } from './ToolCallAnswer';
-import { ToolCallParameters } from './ToolCallContent';
+import { ToolCallParameters } from './ToolCallParameters';
 
 interface ToolCallWidgetProps {
     toolCall: SchemaToolCall;
@@ -25,16 +22,6 @@ export const ToolCallWidget = ({ toolCall, answer }: ToolCallWidgetProps) => {
     const mappedArgs = mapToolCallArgs(toolCall);
     const stringArgs = JSON.stringify(mappedArgs, undefined, 2);
 
-    const addSnackMessage = useAppContext((state) => state.addSnackMessage);
-
-    const copyParameters = async () => {
-        await navigator.clipboard.writeText(stringArgs);
-        addSnackMessage({
-            type: SnackMessageType.Brief,
-            id: 'tool-call-args-copied',
-            message: 'Tool call parameters copied',
-        });
-    };
     return (
         <CollapsibleWidgetBase defaultExpanded>
             <CollapsibleWidgetHeading
@@ -46,16 +33,7 @@ export const ToolCallWidget = ({ toolCall, answer }: ToolCallWidgetProps) => {
             <CollapsibleWidgetPanel>
                 <FadeOverflowContent className={css({ width: '[100%]' })}>
                     <ToolCallParameters>{stringArgs}</ToolCallParameters>
-                    <CollapsibleWidgetFooter>
-                        <IconButtonWithTooltip
-                            label="Copy tool call parameters"
-                            onClick={copyParameters}
-                            color="default"
-                            placement="top"
-                            edge="start">
-                            <ContentCopy />
-                        </IconButtonWithTooltip>
-                    </CollapsibleWidgetFooter>
+                    <CollapsibleWidgetFooter></CollapsibleWidgetFooter>
                     <ToolCallAnswer>{answer}</ToolCallAnswer>
                 </FadeOverflowContent>
             </CollapsibleWidgetPanel>
