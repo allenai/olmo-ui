9a4dae20f8fa51d60fb55613a97ac3399ef6a5b6
Taylor Blanton <matthewtblanton@gmail.com>
2025-08-25 14:19:17 -0500

    add a paper display to the storybook root so font styles are right

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,10 +1,10 @@
-import { ReactRenderer, type Preview } from '@storybook/react'
-import '../styled-system/styles.css'
-import { withThemeByClassName, withThemeFromJSXProvider } from '@storybook/addon-themes'
-import { withRouter } from 'storybook-addon-remix-react-router';
-import { ThemeProvider } from '@mui/material';
 import { olmoThemePaletteMode, uiRefreshOlmoTheme } from '@/olmoTheme';
 import { getTheme } from '@allenai/varnish2/theme';
+import { Paper, ThemeProvider } from '@mui/material';
+import { withThemeByClassName, withThemeFromJSXProvider } from '@storybook/addon-themes';
+import { ReactRenderer, type Preview } from '@storybook/react';
+import { withRouter } from 'storybook-addon-remix-react-router';
+import '../styled-system/styles.css';
 
 const preview: Preview = {
   parameters: {
@@ -33,13 +33,15 @@ const preview: Preview = {
       defaultTheme: 'light'
     }),
     withRouter,
+    // This is needed to get typography to inherit the right colors when using MUI
+    (Story) => <Paper><Story /></Paper>,
     withThemeFromJSXProvider({
       themes: {
         light: olmoThemePaletteMode(getTheme(uiRefreshOlmoTheme), 'light'),
         dark: olmoThemePaletteMode(getTheme(uiRefreshOlmoTheme), 'dark')
       },
       Provider: ThemeProvider
-    })
+    }),
   ]
 };
 
