d05cb366a5ac8e143e48b0333efc2368426bcdc7
Taylor Blanton <matthewtblanton@gmail.com>
2025-08-25 11:25:51 -0500

    pull react-query decorator into a shared decorator

--- src/components/thread/tools/AllToolCalls.stories.tsx
+++ src/components/thread/tools/AllToolCalls.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import { threadOptions } from '@/api/playgroundApi/thread';
+import { withMockReactQuery } from '@/utils/storybook/withReactQuery';
 import { createMockMessage, createMockThread } from '@/utils/test/createMockModel';
 
 import AllToolCalls from './AllToolCalls';
@@ -14,8 +14,72 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-const storyQueryClient = new QueryClient({
-    defaultOptions: { queries: { enabled: false } },
+const mockThread = createMockThread({
+    id: 'thread',
+    messages: [
+        createMockMessage({
+            id: 'tool-call-response-1',
+            role: 'tool_call_result',
+            content: '74yl0r bl4n70n',
+            toolCalls: [
+                {
+                    toolCallId: 'tool-call-1',
+                    args: { firstName: 'Taylor', lastName: 'Blanton' },
+                    toolName: 'leetName',
+                },
+            ],
+        }),
+        createMockMessage({
+            id: 'tool-call-response-2',
+            role: 'tool_call_result',
+
+            toolCalls: [
+                {
+                    args: { firstName: 'Caleb', lastName: 'Ouellette' },
+                    toolCallId: 'tool-call-2',
+                    toolName: 'leetName',
+                },
+            ],
+            content: 'c4l3b 0u3ll3773',
+        }),
+        createMockMessage({
+            id: 'tool-call-response-3',
+            role: 'tool_call_result',
+
+            toolCalls: [
+                {
+                    args: '{ "firstName": "Paul", "lastName": "Laskowski" }',
+                    toolCallId: 'tool-call-3',
+                    toolName: 'leetName',
+                },
+            ],
+            content: 'p4ul l4sk0wski',
+        }),
+        createMockMessage({
+            id: 'tool-call-response-4',
+            role: 'tool_call_result',
+            toolCalls: [
+                {
+                    args: { firstName: 'Byron', lastName: 'Bischoff' },
+                    toolCallId: 'tool-call-4',
+                    toolName: 'leetName',
+                },
+            ],
+            content: 'byr0n bisch0ff',
+        }),
+        createMockMessage({
+            id: 'tool-call-response-5',
+            role: 'tool_call_result',
+            toolCalls: [
+                {
+                    args: { firstName: 'Jon', lastName: 'Borchardt' },
+                    toolCallId: 'tool-call-5',
+                    toolName: 'leetName',
+                },
+            ],
+            content: 'j0n b0rch4rd7',
+        }),
+    ],
 });
 
 export const Default: Story = {
@@ -49,83 +113,8 @@ export const Default: Story = {
             },
         ],
     },
-    decorators: [
-        (Story) => {
-            const mockThread = createMockThread({
-                id: 'thread',
-                messages: [
-                    createMockMessage({
-                        id: 'tool-call-response-1',
-                        role: 'tool_call_result',
-                        content: '74yl0r bl4n70n',
-                        toolCalls: [
-                            {
-                                toolCallId: 'tool-call-1',
-                                args: { firstName: 'Taylor', lastName: 'Blanton' },
-                                toolName: 'leetName',
-                            },
-                        ],
-                    }),
-                    createMockMessage({
-                        id: 'tool-call-response-2',
-                        role: 'tool_call_result',
-
-                        toolCalls: [
-                            {
-                                args: { firstName: 'Caleb', lastName: 'Ouellette' },
-                                toolCallId: 'tool-call-2',
-                                toolName: 'leetName',
-                            },
-                        ],
-                        content: 'c4l3b 0u3ll3773',
-                    }),
-                    createMockMessage({
-                        id: 'tool-call-response-3',
-                        role: 'tool_call_result',
-
-                        toolCalls: [
-                            {
-                                args: '{ "firstName": "Paul", "lastName": "Laskowski" }',
-                                toolCallId: 'tool-call-3',
-                                toolName: 'leetName',
-                            },
-                        ],
-                        content: 'p4ul l4sk0wski',
-                    }),
-                    createMockMessage({
-                        id: 'tool-call-response-4',
-                        role: 'tool_call_result',
-                        toolCalls: [
-                            {
-                                args: { firstName: 'Byron', lastName: 'Bischoff' },
-                                toolCallId: 'tool-call-4',
-                                toolName: 'leetName',
-                            },
-                        ],
-                        content: 'byr0n bisch0ff',
-                    }),
-                    createMockMessage({
-                        id: 'tool-call-response-5',
-                        role: 'tool_call_result',
-                        toolCalls: [
-                            {
-                                args: { firstName: 'Jon', lastName: 'Borchardt' },
-                                toolCallId: 'tool-call-5',
-                                toolName: 'leetName',
-                            },
-                        ],
-                        content: 'j0n b0rch4rd7',
-                    }),
-                ],
-            });
-
-            storyQueryClient.setQueryData(threadOptions(mockThread.id).queryKey, mockThread);
-
-            return (
-                <QueryClientProvider client={storyQueryClient}>
-                    <Story />
-                </QueryClientProvider>
-            );
-        },
-    ],
+    parameters: {
+        mockData: [{ queryKey: threadOptions(mockThread.id).queryKey, data: mockThread }],
+    },
+    decorators: [withMockReactQuery],
 };
---
+++ src/utils/storybook/withReactQuery.tsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import type { DecoratorFunction } from 'storybook/internal/types';
+
+const storyQueryClient = new QueryClient({
+    defaultOptions: { queries: { enabled: false } },
+});
+
+export const withMockReactQuery: DecoratorFunction = (Story, { parameters: { mockData = [] } }) => {
+    for (const { queryKey, data } of mockData) {
+        storyQueryClient.setQueryData(queryKey, data);
+    }
+
+    return (
+        <QueryClientProvider client={storyQueryClient}>
+            <Story />
+        </QueryClientProvider>
+    );
+};
