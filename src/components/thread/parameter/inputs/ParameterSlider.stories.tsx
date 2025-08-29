9d264348cc80ed69caa14a6f6ecb4a737c1bb222
Taylor Blanton <matthewtblanton@gmail.com>
2025-08-29 16:18:43 -0500

    update the parameter slider story to be more accurate, add a `play` test

--- src/components/thread/parameter/inputs/ParameterSlider.stories.tsx
+++ src/components/thread/parameter/inputs/ParameterSlider.stories.tsx
@@ -1,10 +1,26 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
+import { expect, fn, waitFor } from 'storybook/test';
 
 import { ParameterSlider } from './ParameterSlider';
 
 const meta = {
-    title: 'molecule/ParameterSlider',
+    title: 'parameters/ParameterSlider',
     component: ParameterSlider,
+    args: {
+        label: 'Slider',
+        dialogContent: 'In the dialog',
+        dialogTitle: 'Dialog title',
+        id: 'slider',
+        onChange: fn(),
+    },
+    decorators: [
+        // The parameters drawer has a special grid setup. Make sure we stay up to date with it if we change it.
+        (Story) => (
+            <div style={{ display: 'grid', gridTemplateColumns: '1fr auto' }}>
+                <Story />
+            </div>
+        ),
+    ],
 } satisfies Meta<typeof ParameterSlider>;
 
 export default meta;
@@ -12,10 +28,15 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {
-        label: 'Slider',
-        dialogContent: 'In the dialog',
-        dialogTitle: 'Dialog title',
-        id: 'slider',
+    play: async ({ canvas, userEvent, args }) => {
+        const numberInput = canvas.getByLabelText('Slider', {
+            selector: 'input[type="number"]',
+        });
+
+        await userEvent.type(numberInput, '42');
+        await waitFor(() => expect(args.onChange).toHaveBeenCalled(), {
+            // The value is debounced to 800ms in this component
+            timeout: 900,
+        });
     },
 };
