0f114688b77e3768f998a4cbab235cb5ebb6f08e
Paul Laskowski <paull@allenai.org>
2025-09-12 11:26:40 -0700

    add e2e test

--- e2e/model-switching.spec.ts
+++ e2e/model-switching.spec.ts
@@ -6,10 +6,16 @@ test('model switching warning', async ({ page }) => {
     const modelSelectLocator = page.getByRole('combobox', { name: 'Model' });
     await expect(modelSelectLocator).toContainText('Tulu2.5');
 
+    const LinkLocator = page.getByRole('link', { name: /^Read more about/i });
+    await expect(LinkLocator).toBeVisible();
+    await expect(LinkLocator).toContainText('Tulu2.5');
+    await expect(LinkLocator).toHaveAttribute('href', 'https://allenai.org');
+
     await modelSelectLocator.click();
     await page.getByRole('option', { name: 'Molmo' }).dispatchEvent('click');
 
     await expect(page.getByText('Change model and start a new thread?')).not.toBeAttached();
+    await expect(LinkLocator).not.toBeVisible();
 
     await page.goto('/thread/msg_A8E5H1X2O3');
     await expect(modelSelectLocator).toContainText('OLMo-peteish-dpo-preview');
