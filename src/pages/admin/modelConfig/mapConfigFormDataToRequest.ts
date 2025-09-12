1a204acc665e2fb05d0f2368649868c1e00e9064
Paul Laskowski <paull@allenai.org>
2025-09-12 15:00:39 -0700

    make sure to send undefined if empty input (empty string)

--- src/pages/admin/modelConfig/mapConfigFormDataToRequest.ts
+++ src/pages/admin/modelConfig/mapConfigFormDataToRequest.ts
@@ -6,14 +6,17 @@ import { ModelConfigFormValues } from './components/ModelConfigForm/ModelConfigF
 export const mapConfigFormDataToRequest = (
     formData: ModelConfigFormValues
 ): SchemaRootCreateModelConfigRequest => {
-    const { availability, availableTime, deprecationTime, familyId, ...rest } = formData;
+    const { availability, availableTime, deprecationTime, familyId, informationUrl, ...rest } =
+        formData;
 
     const internal = availability === 'internal';
     const mappedFamilyId = familyId === 'no_family' ? undefined : familyId;
+    const mappedInformationUrl = informationUrl?.trim() === '' ? undefined : informationUrl;
     const request = {
         ...rest,
         internal,
         familyId: mappedFamilyId,
+        informationUrl: mappedInformationUrl,
         availableTime: availableTime?.toAbsoluteString(),
         deprecationTime: deprecationTime?.toAbsoluteString(),
     } as Mutable<SchemaRootCreateModelConfigRequest>;
