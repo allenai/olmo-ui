9524db9cb8abffeacf8163187c9e06b44192ceba
Taylor Blanton <matthewtblanton@gmail.com>
2025-09-22 16:43:20 -0500

    make it look closer to the figma design

--- src/components/thread/ModelSelect/ModelSelectInput/ModelSelectMenuItem/ModelSelectMenuItem.tsx
+++ src/components/thread/ModelSelect/ModelSelectInput/ModelSelectMenuItem/ModelSelectMenuItem.tsx
@@ -1,7 +1,7 @@
 import { DataObject } from '@mui/icons-material';
 import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
 import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
-import { alpha, Box, MenuItem, menuItemClasses, Stack, styled, Typography } from '@mui/material';
+import { alpha, MenuItem, menuItemClasses, styled, Typography } from '@mui/material';
 
 import type { Model } from '@/api/playgroundApi/additionalTypes';
 import ThinkingSvg from '@/components/assets/thinking.svg?react';
@@ -9,6 +9,15 @@ import ThinkingSvg from '@/components/assets/thinking.svg?react';
 import { ModelSelectFeatureIndicator } from './ModelSelectFeatureIndicator';
 
 const CustomMenuItem = styled(MenuItem)(({ theme }) => ({
+    display: 'grid',
+    gridTemplateColumns: 'auto 1fr',
+    gridTemplateAreas: `
+        'icon title'
+        'icon features'
+    `,
+    columnGap: '0.5rem',
+    rowGap: '0.25rem',
+
     paddingInline: theme.spacing(1.5),
 
     background: 'transparent',
@@ -40,38 +49,20 @@ export const ModelSelectMenuItem = ({ model }: ModelSelectMenuItemProps) => {
 
     return (
         <CustomMenuItem key={model.id} value={model.id}>
-            <Stack direction="row">
-                <Box
-                    sx={{
-                        padding: 1,
-                        '> svg': {
-                            verticalAlign: 'middle',
-                        },
-                    }}>
-                    <MenuItemIcon />
-                </Box>
-                <Stack direction="column">
-                    <Typography>{model.name}</Typography>
-                    <Typography sx={{ opacity: 0.55 }} component="div">
-                        {model.accepts_files ? 'Multimodal' : 'Text-only'}
-                        {model.internal && ' (Internal)'}
-                        <ul>
-                            {model.can_think && (
-                                <ModelSelectFeatureIndicator
-                                    Icon={ThinkingSvg}
-                                    feature="Thinking"
-                                />
-                            )}
-                            {model.can_call_tools && (
-                                <ModelSelectFeatureIndicator
-                                    Icon={DataObject}
-                                    feature="Tool calling"
-                                />
-                            )}
-                        </ul>
-                    </Typography>
-                </Stack>
-            </Stack>
+            <MenuItemIcon sx={{ gridArea: 'icon', alignSelf: 'start' }} />
+            <Typography sx={{ gridArea: 'title', lineHeight: 1 }}>{model.name}</Typography>
+            <Typography sx={{ opacity: 0.55, gridArea: 'features' }} component="div">
+                {model.accepts_files ? 'Multimodal' : 'Text-only'}
+                {model.internal && ' (Internal)'}
+                <ul>
+                    {model.can_think && (
+                        <ModelSelectFeatureIndicator Icon={ThinkingSvg} feature="Thinking" />
+                    )}
+                    {model.can_call_tools && (
+                        <ModelSelectFeatureIndicator Icon={DataObject} feature="Tool calling" />
+                    )}
+                </ul>
+            </Typography>
         </CustomMenuItem>
     );
 };
