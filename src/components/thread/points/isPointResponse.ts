5f7b1bfd7072c93352d502759005c10d56b3bd04
Byron <bronzle@users.noreply.github.com>
2025-10-06 10:52:46 -0700

    Fix hasPoints to use search vs test (#1194)
    ## Description
    
    When using Molmo and pointing, switching back and forth between raw
    loses the reply image with points.
    
    hasPoint is using a global (`/g`) regex and `RegExp.test()`, which will
    search from the last match (when global).
    
    This causes a second call to hasPoints to fail when the first succeeded.
    
    Changing this to `String.search(regex) !== -1` fixes this behavior
    
    ## Reviewer Instructions
    
    ## Testing Plan
    
    Describe what you did to test the change.
    
    ## Output / Screenshots
    
    ### Before
    
    
    https://github.com/user-attachments/assets/1459a0f5-ab90-4c19-a2ef-c0ffb0df09b4
    
    
    
    ### After
    
    
    
    https://github.com/user-attachments/assets/85e4479c-6174-4472-8911-f447cf7e6f83
    
    
    
    ### A11y
    
    How do keyboard navigation and screen readers work with this PR?

--- src/components/thread/points/isPointResponse.ts
+++ src/components/thread/points/isPointResponse.ts
@@ -1,3 +1,6 @@
 import { pointRegex } from './pointRegex';
-
-export const hasPoints = (response: string): boolean => pointRegex.test(response);
+// Because this regex is used for String.replaceAll, it needs to be global.
+// we need to use match here, because pointRegex is global, which means two tests in a row
+// will produce different results, as the second one will test starting from the end of the previous
+// match. String.search doesn't have this behavior.
+export const hasPoints = (response: string): boolean => response.search(pointRegex) !== -1;
--- src/components/thread/points/pointRegex.ts
+++ src/components/thread/points/pointRegex.ts
@@ -1,2 +1,5 @@
+// This RegEx is global for String.replaceAll, when using it to match a single
+// message contents, use String.search(pointRegex) !== -1 instead of pointRegex.test(string)
+// as test will start at the last match for testing when using a global regex.
 export const pointRegex =
     /<points?(?:\s+x\d?="\d+\.?\d*"\s+y\d?="\d+\.?\d*")+.*>(?<text>.*)<\/points?>/g;
