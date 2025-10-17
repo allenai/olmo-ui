3836effc80e868b3b960a7d8973f20bee7601865
Byron Bischoff <byronb@gmail.com>
2025-10-17 14:43:05 -0700

    make the expand arrow button disclosure state aware

--- src/components/widgets/CollapsibleWidget/ExpandArrow.tsx
+++ src/components/widgets/CollapsibleWidget/ExpandArrow.tsx
@@ -1,32 +1,44 @@
-import { css, cx } from '@allenai/varnish-panda-runtime/css';
+import { cva, cx } from '@allenai/varnish-panda-runtime/css';
 import { KeyboardArrowDown } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
+import { useContext } from 'react';
+import { DisclosureStateContext } from 'react-aria-components';
 
-const expandArrowRecipe = css({
-    // MUI's transition has higher precidence :(
-    // TODO: find a better way
-    transition: '[transform! 150ms ease-in-out]',
-    _groupExpanded: {
-        transform: '[rotate(180deg)]',
+const expandArrowRecipe = cva({
+    base: {
+        // MUI's transition has higher precidence :(
+        // TODO: find a better way
+        transition: '[transform! 150ms ease-in-out]',
+    },
+    variants: {
+        expanded: {
+            true: {
+                transform: '[rotate(180deg)]',
+            },
+        },
     },
 });
 
 interface ExpandArrowProps {
     className?: string;
+    expanded?: boolean;
 }
 
-const ExpandArrow = ({ className }: ExpandArrowProps) => (
-    <KeyboardArrowDown className={cx(expandArrowRecipe, className)} />
+const ExpandArrow = ({ className, expanded = false }: ExpandArrowProps) => (
+    <KeyboardArrowDown className={cx(expandArrowRecipe({ expanded }), className)} />
 );
 
 interface ExpandArrowButtonProps extends ExpandArrowProps {
     iconClassName?: string;
 }
 
-const ExpandArrowButton = ({ className, iconClassName }: ExpandArrowButtonProps) => {
+const ExpandArrowButton = ({ className, iconClassName, expanded }: ExpandArrowButtonProps) => {
+    const disclosureState = useContext(DisclosureStateContext);
+    const isExpanded = disclosureState?.isExpanded ?? false;
+
     return (
         <IconButton tabIndex={-1} component="span" size="small" className={className}>
-            <ExpandArrow className={iconClassName} />
+            <ExpandArrow className={iconClassName} expanded={expanded ?? isExpanded} />
         </IconButton>
     );
 };
