1edddf159c7369cae3b7ed069a0c4b39defc310c
Taylor Blanton <matthewtblanton@gmail.com>
2025-10-22 18:31:37 -0500

    even more cleanup

--- src/utils/storybook/withReactHookForm.tsx
+++ src/utils/storybook/withReactHookForm.tsx
@@ -4,8 +4,6 @@ import type { DecoratorFunction } from 'storybook/internal/types';
 
 export const withReactHookForm = <T,>(defaultValues?: DefaultValues<T>): DecoratorFunction =>
     function ReactHookFormDecorator(Story) {
-        // Storybook decorators start with lower case letters, rules of hooks doesn't like that!
-        // eslint-disable-next-line react-hooks/rules-of-hooks
         const formContext = useForm({ defaultValues });
 
         return (
