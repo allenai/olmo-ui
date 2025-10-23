5c44d9225aae1e6df06a0c8d771673d74801c286
Taylor Blanton <matthewtblanton@gmail.com>
2025-10-23 15:29:10 -0500

    add a mock for /v4/agent/chat

---
+++ src/mocks/handlers/agentHandlers.ts
@@ -0,0 +1,12 @@
+import { http, HttpResponse } from 'msw';
+
+import { createStreamFromResponse } from '../mockUtils';
+import { thinkingAndToolCallsStreamResponse } from './responses/v4/stream/thinkingAndToolCalls';
+
+export const agentHandlers = [
+    http.post('/v4/agent/chat', () => {
+        const stream = createStreamFromResponse(thinkingAndToolCallsStreamResponse, 10);
+
+        return new HttpResponse(stream);
+    }),
+];
