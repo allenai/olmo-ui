befb7acb8dfaa0d652c26e9e1a23ca6229ac8ca3
Byron Bischoff <byronb@gmail.com>
2025-10-27 14:16:15 -0700

    add e2e test to validate url param when model selection changes

--- e2e/model-selection.spec.ts
+++ e2e/model-selection.spec.ts
@@ -17,3 +17,45 @@ test('model selection', async ({ page }) => {
     await page.goto('/?model=OLMo-peteish-dpo-preview');
     await expect(modelSelectLocator).toContainText('OLMo-peteish-dpo-preview');
 });
+
+test('model selection searchParam', async ({ page }) => {
+    await page.goto('/');
+
+    const modelSelect = page.getByRole('combobox', { name: 'Model' });
+    await expect(modelSelect).toHaveText('Tulu2.5');
+
+    await modelSelect.click();
+    await page.getByRole('option', { name: 'OLMo-peteish-dpo-preview' }).click();
+
+    await expect(page).toHaveURL(
+        (url) => url.searchParams.get('model') === 'OLMo-peteish-dpo-preview'
+    );
+});
+
+test('multiple thread model selection searchParam', async ({ page }) => {
+    await page.goto('/comparison');
+
+    // Select two compatible models (both text models)
+    const model1Select = page.getByRole('combobox', { name: 'Model' }).first();
+    const model2Select = page.getByRole('combobox', { name: 'Model' }).nth(1);
+
+    await model1Select.click();
+    await page.getByRole('option', { name: 'OLMo-peteish-dpo-preview' }).click();
+
+    await expect(page).toHaveURL(
+        (
+            url // should changing one, add both modes to searchParams
+        ) => url.searchParams.get('model-1') === 'OLMo-peteish-dpo-preview'
+        // && url.searchParams.get('model-2') === 'tulu2'
+    );
+
+    await model2Select.click();
+    await page.getByRole('option', { name: 'Tulu2.5' }).click();
+
+    // both should be in URL searchParams now
+    await expect(page).toHaveURL(
+        (url) =>
+            url.searchParams.get('model-1') === 'OLMo-peteish-dpo-preview' &&
+            url.searchParams.get('model-2') === 'tulu2'
+    );
+});
