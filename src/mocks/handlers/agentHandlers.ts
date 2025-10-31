e128695384a2910431fb0a4ddc8b435673fbcba6
Taylor Blanton <matthewtblanton@gmail.com>
2025-10-31 17:40:47 -0500

    Agent updates after making the server changes (#1222)
    Needed to make some small tweaks after I actually built out the API.
    This PR has those tweaks!

--- src/mocks/handlers/agentHandlers.ts
+++ src/mocks/handlers/agentHandlers.ts
@@ -4,7 +4,7 @@ import { createStreamFromResponse } from '../mockUtils';
 import { thinkingAndToolCallsStreamResponse } from './responses/v4/stream/thinkingAndToolCalls';
 
 export const agentHandlers = [
-    http.post('/v4/agent/chat', () => {
+    http.post('/v4/agents/chat', () => {
         const stream = createStreamFromResponse(thinkingAndToolCallsStreamResponse, 10);
 
         return new HttpResponse(stream);
