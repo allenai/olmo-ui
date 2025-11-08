aab3f0f8136f124750946e328be55e32c8cf15d1
Byron Bischoff <byronb@gmail.com>
2025-11-07 17:19:45 -0800

    remove katex (not used)

--- src/components/markdownMath/micromark-extension-math/index.js
+++ src/components/markdownMath/micromark-extension-math/index.js
@@ -1,4 +1,1 @@
-// From: https://github.com/ofk/micromark-extension-llm-math/blob/7fd4384be83b73fcc4490d22ba8c6f6ac4a40886/dev/index.js
-// Note: types exported from `index.d.ts`.
-export { mathHtml } from './lib/html.js';
 export { math } from './lib/syntax.js';
