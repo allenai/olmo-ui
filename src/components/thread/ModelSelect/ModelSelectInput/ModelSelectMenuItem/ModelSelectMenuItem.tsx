987a136a14fc118e9f08f0cafd1bb53de82499d6
Paul Laskowski <warmbowski@users.noreply.github.com>
2025-11-18 13:27:52 -0800

    Update ModelSelect to show New icon for list of model IDs (#1239)
    ## Description
    
    Adds hardcoded list of substrings of model ids to be marked as "New"
    * add "New" feature with icon 
    * update "Thinking" feature icon svg
    
    closes https://github.com/allenai/playground-issues-repo/issues/782
    
    ## Reviewer Instructions
    
    Design:
    https://www.figma.com/design/eQRLxoGjUCnk6DLThrm8Ii/Playground-2025-Q3---4?node-id=2802-1888&m=dev
    
    ## Testing Plan
    
    Manual testing until the hardcoded list is replaced with a model config
    property
    
    ## Output / Screenshots
    
    ### Before
    
    <img width="452" height="881" alt="Screenshot 2025-11-17 at 1 13 33 PM"
    src="https://github.com/user-attachments/assets/add73b50-6cd5-4bc0-8813-73c36eb45748"
    />
    
    
    ### After
    
    See last two models in list:
    <img width="456" height="930" alt="Screenshot 2025-11-18 at 10 36 28 AM"
    src="https://github.com/user-attachments/assets/cd25905a-e9fd-4133-8581-b279cfc81c2c"
    />
    
    
    
    ### A11y

--- src/components/thread/ModelSelect/ModelSelectInput/ModelSelectMenuItem/ModelSelectMenuItem.tsx
+++ src/components/thread/ModelSelect/ModelSelectInput/ModelSelectMenuItem/ModelSelectMenuItem.tsx
@@ -1,3 +1,4 @@
+import { AutoAwesome } from '@mui/icons-material';
 import ChatOutlinedIcon from '@mui/icons-material/ChatOutlined';
 import ImageOutlinedIcon from '@mui/icons-material/ImageOutlined';
 import {
@@ -54,9 +55,15 @@ const CustomMenuItem = styled(MenuItem)(({ theme }) => ({
 
 interface ModelSelectMenuItemProps extends MenuItemProps {
     model: Model;
+    isNewModel?: boolean;
 }
 
-export const ModelSelectMenuItem = ({ model, value, ...rest }: ModelSelectMenuItemProps) => {
+export const ModelSelectMenuItem = ({
+    model,
+    value,
+    isNewModel = false,
+    ...rest
+}: ModelSelectMenuItemProps) => {
     const MenuItemIcon = model.prompt_type === 'multi_modal' ? ImageOutlinedIcon : ChatOutlinedIcon;
 
     return (
@@ -64,11 +71,19 @@ export const ModelSelectMenuItem = ({ model, value, ...rest }: ModelSelectMenuIt
         // https://github.com/mui/material-ui/issues/31006#issuecomment-1035549630
         <CustomMenuItem value={value} {...rest}>
             <MenuItemIcon sx={{ gridArea: 'icon', alignSelf: 'start' }} />
-            <Typography sx={{ gridArea: 'title', lineHeight: 1 }}>{model.name}</Typography>
+            <Typography
+                sx={{
+                    gridArea: 'title',
+                    lineHeight: 1,
+                    fontWeight: isNewModel ? 'bold' : undefined,
+                }}>
+                {model.name}
+            </Typography>
             <Typography sx={{ opacity: 0.55, gridArea: 'features' }} component="div">
                 {model.accepts_files ? 'Multimodal' : 'Text-only'}
                 {model.internal && ' (Internal)'}
                 <ul>
+                    {isNewModel && <ModelSelectFeatureIndicator Icon={AutoAwesome} feature="New" />}
                     {model.can_think && (
                         <ModelSelectFeatureIndicator Icon={ThinkingSvg} feature="Thinking" />
                     )}
