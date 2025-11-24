2640673e1327cd4efc0fe440d249487454645062
Caleb Ouellette <calebo@allenai.org>
2025-11-24 12:34:57 -0800

    wip

---
+++ src/components/video/videoProduction/test-one.tsx
@@ -0,0 +1,9 @@
+import { registerRoot } from 'remotion';
+
+const Video = () => {
+    return <div />;
+};
+
+registerRoot(Video);
+
+// npx remotion studio ./src/components/video/videoProduction/test-one.tsx
