e62080cd4dd30eecb165028161c75bc36e6bb190
Byron Bischoff <byronb@gmail.com>
2025-11-26 22:38:09 -0800

    Don't wrap menu in FileTrigger

--- src/components/thread/QueryForm/FileUploadButton/FileUploadButton.tsx
+++ src/components/thread/QueryForm/FileUploadButton/FileUploadButton.tsx
@@ -6,15 +6,14 @@ import {
     useImperativeHandle,
     useRef,
 } from 'react';
-import { FileTrigger, Focusable } from 'react-aria-components';
+import { Focusable } from 'react-aria-components';
 
 import { StyledTooltip } from '@/components/StyledTooltip';
 import { useFeatureToggles } from '@/FeatureToggleContext';
 
-import { AddMediaButton } from './AddMediaButton';
 import { fileTypesToArray, mediaTypeMatches } from './fileTypeHelpers';
-import { type MediaType, mediaTypeList } from './fileUploadMediaConsts';
-import { FileUploadMenu } from './FileUploadMenu';
+import { mediaTypeList } from './fileUploadMediaConsts';
+import { MediaTrigger } from './MediaTrigger';
 import { useFileInputTrigger } from './useFileInputTrigger';
 
 export interface FileuploadPropsBase {
@@ -29,11 +28,15 @@ export interface FileuploadPropsBase {
     requiredFileOption?: string; // this should be the union: SchemaMultiModalModel['require_file_to_prompt'] =/
 }
 
+type InheritedInputProps = Omit<
+    DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>,
+    'type' | 'onSelect'
+>;
+
 export type FileUploadButtonProps = FileuploadPropsBase &
-    Omit<
-        DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>,
-        'type' | 'onSelect'
-    >;
+    InheritedInputProps & {
+        onSelect?: (files: FileList | undefined) => void;
+    };
 
 export const FileUploadButton = forwardRef(function FileUploadButton(
     {
@@ -45,8 +48,7 @@ export const FileUploadButton = forwardRef(function FileUploadButton(
         allowFilesInFollowups: _allowFilesInFollowups,
         maxFilesPerMessage,
         onSelect,
-        // ...props
-    }: FileUploadButtonProps & { onSelect?: (files: FileList | undefined) => void },
+    }: FileUploadButtonProps,
     ref: ForwardedRef<HTMLInputElement>
 ) {
     const inputRef = useRef<HTMLInputElement>(null);
@@ -83,43 +85,19 @@ export const FileUploadButton = forwardRef(function FileUploadButton(
         <StyledTooltip isDisabled={!tooltipContent} content={tooltipContent} placement="top">
             <Focusable>
                 <span>
-                    <FileTrigger
-                        ref={inputRef}
-                        allowsMultiple={acceptsMultiple}
+                    <MediaTrigger
+                        inputRef={inputRef}
+                        acceptsMultiple={acceptsMultiple}
+                        isDisabled={isFileUploadDisabled || isSendingPrompt}
+                        mediaTypes={mediaTypes}
+                        triggerFileInput={triggerFileInput}
                         acceptedFileTypes={fileTypes}
                         onSelect={(files) => {
                             onSelect?.(files ?? undefined);
-                        }}>
-                        <AddMediaComponent
-                            isDisabled={isFileUploadDisabled || isSendingPrompt}
-                            mediaTypes={mediaTypes}
-                            triggerFileInput={triggerFileInput}
-                        />
-                    </FileTrigger>
+                        }}
+                    />
                 </span>
             </Focusable>
         </StyledTooltip>
     );
 });
-
-const AddMediaComponent = ({
-    mediaTypes,
-    isDisabled,
-    triggerFileInput,
-}: {
-    triggerFileInput: (mediaType: string | number) => void;
-    isDisabled?: boolean;
-    mediaTypes: MediaType[];
-}) => {
-    if (mediaTypes.length > 1) {
-        return (
-            <FileUploadMenu
-                isDisabled={isDisabled}
-                mediaTypes={mediaTypes}
-                triggerFileInput={triggerFileInput}
-            />
-        );
-    }
-
-    return <AddMediaButton isDisabled={isDisabled} />;
-};
