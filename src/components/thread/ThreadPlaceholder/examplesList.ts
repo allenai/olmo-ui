5565c7cf242b918653492c69c77f843a3115dcf6
Paul Laskowski <warmbowski@users.noreply.github.com>
2025-12-04 18:02:26 -0800

    update examples (#1291)
    ## Description
    
    Describe the change and its purpose. Include a ticket if applicable.
    
    ## Reviewer Instructions
    
    Anything a review needs to know about this PR to help them give better
    feedback. Are there specific questions you have? Are there areas you are
    unsure of? Does this come from or relate to another part of the
    codebase?
    
    ## Testing Plan
    
    Describe what you did to test the change.
    
    ## Output / Screenshots
    
    ### Before
    
    What did it look like before?
    
    ### After
    
    What will it look like after merging?
    
    ### A11y
    
    How do keyboard navigation and screen readers work with this PR?

--- src/components/thread/ThreadPlaceholder/examplesList.ts
+++ src/components/thread/ThreadPlaceholder/examplesList.ts
@@ -3,37 +3,37 @@ export const examplesList = [
         prefix: 'molmo2-4b-track-count-v4-point-pretrain',
         introText:
             'Start with one of these sample prompts, or upload an image and ask a question below.',
-        templateIds: ['point_boats_12345', 'video_counting_1', 'video_tracking_2'],
+        templateIds: ['images_counting_1', 'video_tracking_5', 'video_demse_captioning_5'],
     },
     {
         prefix: 'molmo-2-4b-general-checkpoint',
         introText:
             'Start with one of these sample prompts, or upload an image and ask a question below.',
-        templateIds: ['point_boats_12345', 'video_counting_1', 'video_tracking_2'],
+        templateIds: ['images_counting_1', 'video_tracking_5', 'video_demse_captioning_5'],
     },
     {
         prefix: 'molmo2-4b-point-sft-v5',
         introText:
             'Start with one of these sample prompts, or upload an image and ask a question below.',
-        templateIds: ['point_boats_12345', 'video_counting_1', 'video_tracking_2'],
+        templateIds: ['images_counting_1', 'video_tracking_5', 'video_demse_captioning_5'],
     },
     {
         prefix: 'molmo2-4b-joint-sft-v5',
         introText:
             'Start with one of these sample prompts, or upload an image and ask a question below.',
-        templateIds: ['point_boats_12345', 'video_counting_1', 'video_tracking_2'],
+        templateIds: ['images_counting_1', 'video_tracking_5', 'video_demse_captioning_5'],
     },
     {
         prefix: 'molmo2-8b-joint-sft-v5',
         introText:
             'Start with one of these sample prompts, or upload an image and ask a question below.',
-        templateIds: ['point_boats_12345', 'video_counting_1', 'video_tracking_2'],
+        templateIds: ['images_counting_1', 'video_tracking_5', 'video_demse_captioning_5'],
     },
     // add entries like the one below to test in localdev
     // {
     //     prefix: 'molmo2-',
     //     introText:
     //         'Start with one of these sample prompts, or upload an image and ask a question below.',
-    //     templateIds: ['point_boats_12345', 'video_tracking_1', 'video_counting_1'],
+    //     templateIds: ['images_counting_1', 'video_tracking_4', 'video_tracking_5'],
     // },
 ];
