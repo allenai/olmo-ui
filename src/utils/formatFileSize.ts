6153b0784d4b1a3810d6d325cb3c58f962e2eae4
Byron Bischoff <byronb@gmail.com>
2025-12-08 07:26:25 -0800

    MiB => MB

--- src/utils/formatFileSize.ts
+++ src/utils/formatFileSize.ts
@@ -5,12 +5,12 @@ export const formatFileSize = (fileSize: number): string => {
     const unitScale =
         fileSize === 0
             ? 0
-            : Math.min(Math.floor(Math.log(fileSize) / Math.log(1024)), FILE_SIZE_UNITS.length - 1);
+            : Math.min(Math.floor(Math.log(fileSize) / Math.log(1000)), FILE_SIZE_UNITS.length - 1);
 
     // find the decimal value of the fileSize in the denominator (scale)
     const scaledFileSize = String(
         // parseFloat will convert 3.00 to 3, but leave the decimals if they are significant
-        parseFloat(Number(fileSize / Math.pow(1024, unitScale)).toFixed(2))
+        parseFloat(Number(fileSize / Math.pow(1000, unitScale)).toFixed(2))
     );
 
     return `${scaledFileSize} ${FILE_SIZE_UNITS[unitScale]}`;
