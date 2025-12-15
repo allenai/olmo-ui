72f72078ba40c036ffdb6cf03164e2f353f50da1
Paul Laskowski <paull@allenai.org>
2025-12-15 15:05:35 -0800

    updates to intro text

--- src/components/thread/ThreadPlaceholder/examplesList.ts
+++ src/components/thread/ThreadPlaceholder/examplesList.ts
@@ -2,25 +2,25 @@ export const examplesList = [
     {
         modelId: 'molmo2-8b-track',
         introText:
-            'Start with one of these sample prompts, or upload a video and ask Molmo to track it below. 8fps works best for video tracking - to try all other capabilities, choose 2fps from the dropdown',
+            'Start with one of these sample prompts, or upload a video and ask Molmo to track it below. 8fps interpolation works best for video tracking - to try all other capabilities, choose the standard option from the model dropdown',
         templateIds: ['video_tracking_7', 'video_tracking_8', 'skier_track'],
     },
     {
         modelId: 'molmo2-4b-track',
         introText:
-            'Start with one of these sample prompts, or upload a video and ask Molmo to track it below. 8fps works best for video tracking - to try all other capabilities, choose 2fps from the dropdown',
+            'Start with one of these sample prompts, or upload a video and ask Molmo to track it below. 8fps interpolation works best for video tracking - to try all other capabilities, choose the standard option from the model dropdown',
         templateIds: ['video_tracking_7', 'video_tracking_8', 'skier_track'],
     },
     {
         modelId: 'molmo2-8b',
         introText:
-            'Start with one of these sample prompts, or upload an image/ video and ask a question below. 2fps is best for all features other than video tracking. To try video tracking, choose 8fps from the dropdown',
+            'Start with one of these sample prompts, or upload an image/ video and ask a question below. 8fps interpolation works best for video tracking. To try video tracking, choose the faster frame rate from the model dropdown',
         templateIds: ['images_counting_1', 'backflip_count', 'video_demse_captioning_5'],
     },
     {
         modelId: 'molmo2-4b',
         introText:
-            'Start with one of these sample prompts, or upload an image/ video and ask a question below. 2fps is best for all features other than video tracking. To try video tracking, choose 8fps from the dropdown',
+            'Start with one of these sample prompts, or upload an image/ video and ask a question below. 8fps interpolation works best for video tracking. To try video tracking, choose the faster frame rate from the model dropdown',
         templateIds: ['images_counting_1', 'backflip_count', 'video_demse_captioning_5'],
     },
 ];
