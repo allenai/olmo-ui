c57a5118b413eee918b7c9e967c2ca5f836712a5
Paul Laskowski <paull@allenai.org>
2025-12-15 14:23:50 -0800

    rename property

--- src/components/thread/ThreadPlaceholder/examplesList.ts
+++ src/components/thread/ThreadPlaceholder/examplesList.ts
@@ -1,24 +1,24 @@
 export const examplesList = [
     {
-        prefix: 'molmo2-8b-track',
+        modelId: 'molmo2-8b-track',
         introText:
             'Start with one of these sample prompts, or upload a video and ask Molmo to track it below. 8fps works best for video tracking - to try all other capabilities, choose 2fps from the dropdown',
         templateIds: ['video_tracking_7', 'video_tracking_8', 'skier_track'],
     },
     {
-        prefix: 'molmo2-4b-track',
+        modelId: 'molmo2-4b-track',
         introText:
             'Start with one of these sample prompts, or upload a video and ask Molmo to track it below. 8fps works best for video tracking - to try all other capabilities, choose 2fps from the dropdown',
         templateIds: ['video_tracking_7', 'video_tracking_8', 'skier_track'],
     },
     {
-        prefix: 'molmo2-8b',
+        modelId: 'molmo2-8b',
         introText:
             'Start with one of these sample prompts, or upload an image/ video and ask a question below. 2fps is best for all features other than video tracking. To try video tracking, choose 8fps from the dropdown',
         templateIds: ['images_counting_1', 'backflip_count', 'video_demse_captioning_5'],
     },
     {
-        prefix: 'molmo2-4b',
+        modelId: 'molmo2-4b',
         introText:
             'Start with one of these sample prompts, or upload an image/ video and ask a question below. 2fps is best for all features other than video tracking. To try video tracking, choose 8fps from the dropdown',
         templateIds: ['images_counting_1', 'backflip_count', 'video_demse_captioning_5'],
