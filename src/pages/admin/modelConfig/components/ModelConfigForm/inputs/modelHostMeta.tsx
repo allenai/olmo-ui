8e6f6a4176b2f1b0e9b8a2e65f3ca99e93c1c648
Byron Bischoff <byronb@allenai.org>
2025-12-17 09:27:15 -0800

    reorder items

--- src/pages/admin/modelConfig/components/ModelConfigForm/inputs/modelHostMeta.tsx
+++ src/pages/admin/modelConfig/components/ModelConfigForm/inputs/modelHostMeta.tsx
@@ -14,42 +14,10 @@ interface ModelHostMeta {
 }
 
 export const modelHostMeta: Record<SchemaModelHost, ModelHostMeta> = {
-    modal: {
-        friendlyName: 'Modal',
-        hostIdMeta: {
-            label: 'App ID',
-            description: (
-                <Link
-                    href="https://github.com/allenai/reviz-modal/blob/main/docs/self-serve-hosting.md"
-                    target="_blank"
-                    rel="noopener">
-                    View Modal hosting docs
-                </Link>
-            ),
-        },
-    },
-    inferd: {
-        friendlyName: 'InferD',
-        hostIdMeta: {
-            label: 'Compute Source ID',
-        },
-    },
     beaker_queues: {
         friendlyName: 'Beaker Queues',
         hostIdMeta: {
             label: 'Queue ID',
-        },
-    },
-    cirrascale_backend: {
-        friendlyName: 'Cirrascale (Backend)',
-        hostIdMeta: {
-            label: 'Backend API Port',
-            description: (
-                <span>
-                    The port this model runs on. E.g.{' '}
-                    <code>https://ai2models.cirrascalecloud.services:{'<PORT>'}/v1/models</code>
-                </span>
-            ),
         },
     },
     cirrascale: {
@@ -79,6 +47,38 @@ export const modelHostMeta: Record<SchemaModelHost, ModelHostMeta> = {
                     </Link>
                 </>
             ),
+        },
+    },
+    modal: {
+        friendlyName: 'Modal',
+        hostIdMeta: {
+            label: 'App ID',
+            description: (
+                <Link
+                    href="https://github.com/allenai/reviz-modal/blob/main/docs/self-serve-hosting.md"
+                    target="_blank"
+                    rel="noopener">
+                    View Modal hosting docs
+                </Link>
+            ),
+        },
+    },
+    cirrascale_backend: {
+        friendlyName: 'Cirrascale (Backend)',
+        hostIdMeta: {
+            label: 'Backend API Port',
+            description: (
+                <span>
+                    The port this model runs on. E.g.{' '}
+                    <code>https://ai2models.cirrascalecloud.services:{'<PORT>'}/v1/models</code>
+                </span>
+            ),
+        },
+    },
+    inferd: {
+        friendlyName: 'InferD',
+        hostIdMeta: {
+            label: 'Compute Source ID',
         },
     },
     test_backend: {
